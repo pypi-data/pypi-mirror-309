fc9faf32ff58b3eff234c8347af4d7aa02c21569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:59 +0000

    Add package bbot/2.1.2.5232rc0/bbot-2.1.2.5232rc0-py3-none-any.whl

---
+++ packages/bbot/bbot-2.1.2.5232rc0-py3-none-any.whl/bbot/modules/passivetotal.py
@@ -0,0 +1,46 @@
+from bbot.modules.templates.subdomain_enum import subdomain_enum_apikey
+
+
+class passivetotal(subdomain_enum_apikey):
+    watched_events = ["DNS_NAME"]
+    produced_events = ["DNS_NAME"]
+    flags = ["subdomain-enum", "passive", "safe"]
+    meta = {
+        "description": "Query the PassiveTotal API for subdomains",
+        "created_date": "2022-08-08",
+        "author": "@TheTechromancer",
+        "auth_required": True,
+    }
+    options = {"username": "", "api_key": ""}
+    options_desc = {"username": "RiskIQ Username", "api_key": "RiskIQ API Key"}
+
+    base_url = "https://api.passivetotal.org/v2"
+
+    async def setup(self):
+        self.username = self.config.get("username", "")
+        self.api_key = self.config.get("api_key", "")
+        self.auth = (self.username, self.api_key)
+        return await super().setup()
+
+    async def ping(self):
+        url = f"{self.base_url}/account/quota"
+        j = (await self.api_request(url, auth=self.auth)).json()
+        limit = j["user"]["limits"]["search_api"]
+        used = j["user"]["counts"]["search_api"]
+        assert used < limit, "No quota remaining"
+
+    async def abort_if(self, event):
+        # RiskIQ is famous for their junk data
+        return await super().abort_if(event) or "unresolved" in event.tags
+
+    async def request_url(self, query):
+        url = f"{self.base_url}/enrichment/subdomains?query={self.helpers.quote(query)}"
+        return await self.api_request(url, auth=self.auth)
+
+    def parse_results(self, r, query):
+        for subdomain in r.json().get("subdomains", []):
+            yield f"{subdomain}.{query}"
+
+    @property
+    def auth_secret(self):
+        return self.username and self.api_key
