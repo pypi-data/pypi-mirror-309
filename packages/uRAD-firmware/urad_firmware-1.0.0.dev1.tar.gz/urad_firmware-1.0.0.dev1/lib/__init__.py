68a610c8943ef25923ccba808f29704bfc949d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:00 +0000

    Add package uRAD-firmware/1.0.0.dev1/urad_firmware-1.0.0.dev1.tar.gz

---
+++ packages/uRAD-firmware/urad_firmware-1.0.0.dev1.tar.gz/urad_firmware-1.0.0.dev1/lib/__init__.py
@@ -0,0 +1,5 @@
+
+__author__ = 'Kananelo Chabeli'
+__license__ = 'MIT'
+__description__ = 'The uRAD firmware library customized to extract only the raw RADAR data from uRAD RADAR system. Developed to run across all platforms.'
+__version__= '1.0.0'
