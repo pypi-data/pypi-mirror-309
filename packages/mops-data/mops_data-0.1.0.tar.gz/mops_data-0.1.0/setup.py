2b2006e25d96151b2d2b6873aa4d626915613540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:44 +0000

    Add package mops-data/0.1.0/mops_data-0.1.0.tar.gz

---
+++ packages/mops-data/mops_data-0.1.0.tar.gz/mops_data-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+
+    name='mops_data',  # 自設名稱或與自定義函式相同名稱皆可
+
+    version='0.1.0',
+
+    py_modules=['mops_data'],   # *.py  
+
+    install_requires=[
+
+        # List any dependencies here if needed
+
+    ],
+
+)
