727c3e19d627c7118827fff5198eb05caa875c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:23 +0000

    Add package timeseries-shaper/0.0.0.18/timeseries-shaper-0.0.0.18.tar.gz

---
+++ packages/timeseries-shaper/timeseries-shaper-0.0.0.18.tar.gz/timeseries-shaper-0.0.0.18/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "timeseries-shaper",
+    version = "0.0.0.18",
+    author = "Jakob Gabriel",
+    author_email = "jakob.gabriel5@googlemail.com",
+    description = "timeseries-shaper filters, transforms and engineer your timeseries dataframe",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://jakobgabriel.github.io/timeseries-shaper/timeseries_shaper.html",
+    project_urls = {
+        "Bug Tracker": "https://github.com/jakobgabriel/timeseries-shaper",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.10"
+)
