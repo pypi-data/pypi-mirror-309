53d053129befb13dd5e54c463ebdadc8532a4a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:08:45 +0000

    Add package llmkey/3.0/llmkey-3.0.tar.gz

---
+++ packages/llmkey/llmkey-3.0.tar.gz/llmkey-3.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import distutils.core
+
+setuptools.setup(
+    name='llmkey',
+    version="3.0",
+    install_requires=[
+        "openai",
+        "ollama",
+        "pynput",
+        "pystray",
+        "pyperclip",
+        "tkhtmlview",
+        "appdirs",
+
+        "PyGObject; platform_system=='Linux'" # need for pystray
+
+    ],
+    author='Tal Wrii',
+    long_description_content_type='text/markdown',
+    author_email='readwithai@gmail.com',
+    description='',
+    license='MIT',
+    keywords='LLM GPT clipboard',
+    url='',
+    packages=["llmkey"],
+    data_files=[
+        ('share/applications', ['llmkey.desktop']),
+        ('share/icons/hicolor/scalable/apps/', ["llmkey.svg"])
+    ],
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts': ['llm-key=llmkey.serve:main']
+    },
+)
