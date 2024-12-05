208b7c31612d89f01b2a25d3f8c4260c0101c0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:58 +0000

    Add package UtilMeta/2.6.1/utilmeta-2.6.1-py3-none-any.whl

---
+++ packages/UtilMeta/utilmeta-2.6.1-py3-none-any.whl/utilmeta/bin/template/full/config/service.py
@@ -0,0 +1,18 @@
+from utilmeta import UtilMeta
+from config.conf import configure
+from config.env import env
+{import_backend}    # noqa
+
+service = UtilMeta(
+    __name__,
+    name='{name}',
+    description='{description}',
+    backend={backend},  # noqa
+    production=env.PRODUCTION,
+    version=(0, 1, 0),
+    host='{host}' if env.PRODUCTION else '127.0.0.1',
+    port=80 if env.PRODUCTION else 8000,
+    api='service.api.RootAPI',
+    route='/api'
+)
+configure(service)
---
+++ packages/UtilMeta/utilmeta-2.6.1-py3-none-any.whl/utilmeta/core/response/backends/django.py
@@ -0,0 +1,67 @@
+from django.http.response import StreamingHttpResponse, HttpResponse, HttpResponseBase, FileResponse
+from typing import Union, TYPE_CHECKING
+from .base import ResponseAdaptor
+
+if TYPE_CHECKING:
+    from utilmeta.core.response import Response
+
+
+class DjangoResponseAdaptor(ResponseAdaptor):
+    response: Union[HttpResponse, StreamingHttpResponse]
+
+    @classmethod
+    def qualify(cls, obj):
+        return isinstance(obj, HttpResponseBase)
+
+    @classmethod
+    def reconstruct(cls, resp: Union['ResponseAdaptor', 'Response']):
+        if isinstance(resp, (HttpResponse, StreamingHttpResponse)):
+            return resp
+
+        from utilmeta.core.response import Response
+        if isinstance(resp, ResponseAdaptor):
+            resp = Response(response=resp)
+        elif not isinstance(resp, Response):
+            resp = Response(resp)
+
+        kwargs = dict(
+            status=resp.status,
+            reason=resp.reason,
+            content_type=resp.content_type,
+            charset=resp.charset,
+            headers=resp.prepare_headers()
+        )
+        # if resp.file:
+        #     response = FileResponse(resp.file, **kwargs)
+        # else:
+        response = HttpResponse(resp.body, **kwargs)
+        return response
+
+    @property
+    def status(self):
+        return self.response.status_code
+
+    @property
+    def reason(self):
+        return self.response.reason_phrase
+
+    @property
+    def headers(self):
+        return self.response.headers
+
+    @property
+    def body(self) -> bytes:
+        if self._body is not None:
+            return self._body
+        if isinstance(self.response, StreamingHttpResponse):
+            self._body = self.response.getvalue()
+        else:
+            self._body = self.response.content
+        return self._body
+
+    @property
+    def cookies(self):
+        return self.response.cookies
+
+    def close(self):
+        self.response.close()
