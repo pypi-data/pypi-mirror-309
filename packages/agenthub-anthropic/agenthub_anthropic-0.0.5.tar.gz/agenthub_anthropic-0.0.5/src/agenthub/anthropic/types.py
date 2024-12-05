4ecf11724bf0853d9c315f2eb758ee190255a352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:09 +0000

    Add package agenthub-anthropic/0.0.5/agenthub_anthropic-0.0.5.tar.gz

---
+++ packages/agenthub-anthropic/agenthub_anthropic-0.0.5.tar.gz/agenthub_anthropic-0.0.5/src/agenthub/anthropic/types.py
@@ -0,0 +1,40 @@
+from openai.types.chat import ChatCompletionMessage
+from openai.types.chat.chat_completion_message_tool_call import (
+    ChatCompletionMessageToolCall,
+    Function,
+)
+from typing import List, Callable, Union, Optional
+
+# Third-party imports
+from pydantic import BaseModel
+
+AgentFunction = Callable[[], Union[str, "Agent", dict]]
+
+class Agent(BaseModel):
+    name: str = "AI agent"
+    model: str = "gpt-4o"
+    instructions: Union[str, Callable[[], str]] = "You are a helpful AI agent."
+    functions: List[AgentFunction] = []
+    tool_choice: str = None
+    parallel_tool_calls: bool = True
+
+
+class Response(BaseModel):
+    messages: List = []
+    agent: Optional[Agent] = None
+    context_variables: dict = {}
+
+
+class Result(BaseModel):
+    """
+    Encapsulates the possible return values for an AI agent function.
+
+    Attributes:
+        value (str): The result value as a string.
+        agent (AI): The AI agent instance, if applicable.
+        context_variables (dict): A dictionary of context variables.
+    """
+
+    value: str = ""
+    agent: Optional[Agent] = None
+    context_variables: dict = {}
