0abe9efff22cff51fbad4f0f8eaa3a22fe8cff0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:09 +0000

    Add package param-sweeps/0.2.0b1/param_sweeps-0.2.0b1-py3-none-any.whl

---
+++ packages/param-sweeps/param_sweeps-0.2.0b1-py3-none-any.whl/param_sweeps/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2024 Mira Geoscience Ltd.
+#
+#  This file is part of param-sweeps.
+#
+#  param-sweeps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+
+__version__ = "0.2.0-beta.1"
---
+++ packages/param-sweeps/param_sweeps-0.2.0b1-py3-none-any.whl/param_sweeps/constants.py
@@ -0,0 +1,21 @@
+#  Copyright (c) 2024 Mira Geoscience Ltd.
+#
+#  This file is part of param-sweeps.
+#
+#  param-sweeps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from __future__ import annotations
+
+from typing import Any
+
+
+default_ui_json: dict[str, Any] = {
+    "title": "Parameter sweep",
+    "worker_uijson": None,
+    "run_command": "param_sweeps.driver",
+    "conda_environment": "param_sweeps",
+    "monitoring_directory": None,
+    "workspace_geoh5": None,
+    "geoh5": None,
+}
