66f788ecc3203b1a4159b02cb48375a84fb019a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:07:33 +0000

    Add package pyreglin/0.1.1/pyreglin-0.1.1-py3-none-any.whl

---
+++ packages/pyreglin/pyreglin-0.1.1-py3-none-any.whl/pyreglin/__init__.py
@@ -0,0 +1,6 @@
+from pyreglin.simdata.rlm import rlm
+from pyreglin.tab_anova.tab_anova import tab_anova
+from pyreglin.datasets.load_data import load_data
+
+__version__ = "0.1.1"
+__all__ = ['rlm', 'tab_anova', 'load_data']
