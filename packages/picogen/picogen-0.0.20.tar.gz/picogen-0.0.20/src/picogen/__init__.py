67bba88ebcf14fbad3cd9e21b6d216c6b4673192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:51 +0000

    Add package picogen/0.0.20/picogen-0.0.20.tar.gz

---
+++ packages/picogen/picogen-0.0.20.tar.gz/picogen-0.0.20/src/picogen/__init__.py
@@ -0,0 +1,8 @@
+"""Minimalist static website generator in Python."""
+
+__version__ = "0.0.20"
+
+
+from .deploy import deploy_website
+from .backup import backup_content
+from .generate import generate_website
