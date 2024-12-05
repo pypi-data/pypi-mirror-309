2bbf5ec1f44ec7198921d7f0a1c69ef057bedf3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:28 +0000

    Add package odoo14-addon-sale-commission-partial-settlement/14.0.1.1.1.dev3/odoo14_addon_sale_commission_partial_settlement-14.0.1.1.1.dev3-py3-none-any.whl

---
+++ packages/odoo14-addon-sale-commission-partial-settlement/odoo14_addon_sale_commission_partial_settlement-14.0.1.1.1.dev3-py3-none-any.whl/odoo/addons/sale_commission_partial_settlement/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Nextev
+{
+    "name": "Sales commissions based on paid amount",
+    "version": "14.0.1.1.0",
+    "author": "Nextev Srl," "Ooops," "Odoo Community Association (OCA)",
+    "maintainers": ["aleuffre", "renda-dev", "PicchiSeba"],
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "depends": ["sale_commission"],
+    "website": "https://github.com/OCA/commission",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_commission_view.xml",
+    ],
+    "installable": True,
+}
---
+++ packages/odoo14-addon-sale-commission-partial-settlement/odoo14_addon_sale_commission_partial_settlement-14.0.1.1.1.dev3-py3-none-any.whl/odoo/addons/sale_commission_partial_settlement/models/__init__.py
@@ -0,0 +1,6 @@
+from . import account_invoice_line_agent
+from . import account_invoice_line_agent_partial
+from . import account_partial_reconcile
+from . import sale_commission
+from . import sale_commission_settlement
+from . import sale_commission_settlement_line
