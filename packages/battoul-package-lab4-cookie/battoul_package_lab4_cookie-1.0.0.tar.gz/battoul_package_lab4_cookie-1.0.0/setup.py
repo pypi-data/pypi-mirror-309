6b7f5102ca8159df0cf5bd2b103a9ce7a466e783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:27 +0000

    Add package battoul-package-lab4-cookie/1.0.0/battoul_package_lab4_cookie-1.0.0.tar.gz

---
+++ packages/battoul-package-lab4-cookie/battoul_package_lab4_cookie-1.0.0.tar.gz/battoul_package_lab4_cookie-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='battoul_package_lab4_cookie',  
+   version='1.0.0',
+   author='Elbatoul',  
+   author_email='e.zerga@esi-sba.dz',  
+   packages=['battoul_package_lab4_cookie'],  
+   url='http://pypi.python.org/pypi/your_package_name/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['battoul_package_lab4_cookie']
+)
