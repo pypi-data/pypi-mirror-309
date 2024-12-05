ffb625d5397b0c77862bfe6c3d1fa0d33d8d89f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:08:28 +0000

    Add package kapaaup1/2.8/kapaaup1-2.8.tar.gz

---
+++ packages/kapaaup1/kapaaup1-2.8.tar.gz/kapaaup1-2.8/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import setuptools
+packages = ['kapaaup1']
+setup(name='kapaaup1',
+	version='2.8',
+	author='szblack',
+    packages=packages, 
+    package_dir={'requests': 'requests'},)
