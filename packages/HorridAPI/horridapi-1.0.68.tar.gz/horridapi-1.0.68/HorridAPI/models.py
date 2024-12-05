e1f6ee398dd7c019626bea8c23c311ec310f3321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:20 +0000

    Add package HorridAPI/1.0.68/horridapi-1.0.68.tar.gz

---
+++ packages/HorridAPI/horridapi-1.0.68.tar.gz/horridapi-1.0.68/HorridAPI/models.py
@@ -0,0 +1,49 @@
+# ethinda pakaram vera add akki marukatha erukan vendi pinne eth use akm 
+# now no need
+# gemini = "1"
+# gemma = "2"
+# gemini_pro = 3
+# gemmaa = 4
+# gpt = 5
+# gpt_o = 6
+
+
+
+hehmango = {    
+    "gemini-1.5-flash": 1,
+    "gemma": 2,
+    "gemini-pro": 3,
+    "gemma-2": 4,
+    "gpt-3.5": 5,
+    "gpt-4o-mini": 6,
+    "gpt-4o-mini-2": 7,
+    "llama3-70b": 8,
+    "claude-3.5-sonnet": 9,
+    "gpt-4o": 10,
+    "blackbox": "blackbox",
+    "gpt-3.5-turbo": 5,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "11": 11,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,   
+    11: 11
+}
+
+
