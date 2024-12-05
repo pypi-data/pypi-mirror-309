276c5c156d1ac44444c56e4a75963ae814858e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:08:41 +0000

    Add package mendeley-updated/0.4.2/mendeley_updated-0.4.2-py2.py3-none-any.whl

---
+++ packages/mendeley-updated/mendeley_updated-0.4.2-py2.py3-none-any.whl/mendeley/resources/__init__.py
@@ -0,0 +1,10 @@
+# NOTICE: This distribution has been modified from the original, found at https://github.com/Mendeley/mendeley-python-sdk/tree/master
+
+from .annotations import Annotations
+from .catalog import Catalog
+from .documents import Documents
+from .files import Files
+from .groups import Groups, GroupMembers
+from .profiles import Profiles
+from .trash import Trash
+from .folders import Folders, FolderDocuments
