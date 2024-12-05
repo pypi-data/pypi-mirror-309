4d30e1afa1b900db400a72d19e3b86bc62958e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:52 +0000

    Add package learn-genai/0.1.6/learn_genai-0.1.6.tar.gz

---
+++ packages/learn-genai/learn_genai-0.1.6.tar.gz/learn_genai-0.1.6/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="learn_genai",
+    version="0.1.6",
+    author="Tony Esposito",
+    author_email="peppedda4@gmail.com",
+    description="A package to learn Generative AI through practical examples",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fbanespo1/learn_genai.git",
+    packages=find_packages(),
+    install_requires=[
+    "streamlit",
+    "langchain",
+    "langchain-community",
+    "langchain-ollama",
+    "langgraph",
+    "beautifulsoup4",
+    "requests",
+    "chromadb",
+    "ollama",
+    "transformers",
+],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+   entry_points={
+    "console_scripts": [
+        "learn_genai=learn_genai.run_app:main",
+    ],
+}
+
+)
