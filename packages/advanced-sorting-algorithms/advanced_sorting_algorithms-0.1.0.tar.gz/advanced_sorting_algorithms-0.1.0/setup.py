3baf93cb6393ac75b22ccdc746e0c7951a44daac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:43 +0000

    Add package advanced-sorting-algorithms/0.1.0/advanced_sorting_algorithms-0.1.0.tar.gz

---
+++ packages/advanced-sorting-algorithms/advanced_sorting_algorithms-0.1.0.tar.gz/advanced_sorting_algorithms-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='advanced_sorting_algorithms',
+    version='0.1.0',
+    author='Hryharovich Daniil',
+    author_email='drink.wtoroy@gmail.com',
+    description='just sorting algorithms for you',
+    url='https://github.com/ваш_пользователь/my_library',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
