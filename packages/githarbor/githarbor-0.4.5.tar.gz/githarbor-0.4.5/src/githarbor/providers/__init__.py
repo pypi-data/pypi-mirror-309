fb1a7fd1826584f1f7f9fd8a7c75601415508b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:06:03 +0000

    Add package githarbor/0.4.5/githarbor-0.4.5.tar.gz

---
+++ packages/githarbor/githarbor-0.4.5.tar.gz/githarbor-0.4.5/src/githarbor/providers/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import importlib.util
+
+if importlib.util.find_spec("github"):
+    from githarbor.providers.githubrepository import GitHubRepository
+
+if importlib.util.find_spec("gitlab"):
+    from githarbor.providers.gitlabrepository import GitLabRepository
+
+if importlib.util.find_spec("giteapy"):
+    from githarbor.providers.gitearepository import GiteaRepository
+
+if importlib.util.find_spec("azure"):
+    from githarbor.providers.azurerepository import AzureRepository
+
+
+# if importlib.util.find_spec("atlassian"):
+#     from githarbor.providers.bitbucketrepository import BitbucketRepository
+
+__all__ = [
+    "GitHubRepository",
+    "GitLabRepository",
+    "GiteaRepository",
+    "AzureRepository",
+    # "BitbucketRepository",
+]
