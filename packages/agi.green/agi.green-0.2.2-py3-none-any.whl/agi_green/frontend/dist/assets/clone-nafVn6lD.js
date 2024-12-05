f1af1371964292650a64b53e81e5bf5cba32d7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:12 +0000

    Add package agi.green/0.2.2/agi.green-0.2.2-py3-none-any.whl

---
+++ packages/agi.green/agi.green-0.2.2-py3-none-any.whl/agi_green/frontend/dist/assets/clone-nafVn6lD.js
@@ -0,0 +1,9 @@
+import { c as baseClone } from "./graph-D9WLSh7X.js";
+var CLONE_SYMBOLS_FLAG = 4;
+function clone(value) {
+  return baseClone(value, CLONE_SYMBOLS_FLAG);
+}
+export {
+  clone as c
+};
+//# sourceMappingURL=clone-nafVn6lD.js.map
