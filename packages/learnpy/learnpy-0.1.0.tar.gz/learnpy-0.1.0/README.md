3c1034bc3cff5d456ecb8f4f45e44702a6b90480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:24 +0000

    Add package learnpy/0.1.0/learnpy-0.1.0.tar.gz

---
+++ packages/learnpy/learnpy-0.1.0.tar.gz/learnpy-0.1.0/README.md
@@ -0,0 +1,11 @@
+learnpy: All-In-One Importer for Python Maching Learning
+=======================
+
+learnpy is a package that simplifies the import process by importing all commonly used Python packages. It automatically detects the packages in current environment and imports the most commonly used packages.
+
+
+### Usage Example
+
+`from learnpy import *`
+
+
---
+++ packages/learnpy/learnpy-0.1.0.tar.gz/learnpy-0.1.0/learnpy/__init__.py
@@ -0,0 +1,37 @@
+import datetime as dt
+
+try:
+    import numpy as np
+except:
+    pass
+
+try:
+    import panda as pd
+except:
+    pass
+
+try:
+    import scipy
+except:
+    pass
+
+try:
+    import sklearn
+except:
+    pass
+
+try:
+    import matplotlib.pyplot as plt
+    import matplotlib.tickers as mtickers
+except:
+    pass
+
+try:
+    import tensorflow as tf
+except:
+    pass
+
+try:
+    import torch
+except:
+    pass
