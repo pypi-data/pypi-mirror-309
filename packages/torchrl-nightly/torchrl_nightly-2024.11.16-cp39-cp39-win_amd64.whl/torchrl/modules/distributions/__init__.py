5662e6631f21aa52996c8ffda58d500396de4f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:17 +0000

    Add package torchrl-nightly/2024.11.16/torchrl_nightly-2024.11.16-cp39-cp39-win_amd64.whl

---
+++ packages/torchrl-nightly/torchrl_nightly-2024.11.16-cp39-cp39-win_amd64.whl/torchrl/modules/distributions/__init__.py
@@ -0,0 +1,39 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from tensordict.nn import NormalParamExtractor
+
+from .continuous import (
+    Delta,
+    IndependentNormal,
+    NormalParamWrapper,
+    TanhDelta,
+    TanhNormal,
+    TruncatedNormal,
+)
+from .discrete import (
+    MaskedCategorical,
+    MaskedOneHotCategorical,
+    OneHotCategorical,
+    OneHotOrdinal,
+    Ordinal,
+    ReparamGradientStrategy,
+)
+
+distributions_maps = {
+    str(dist).lower(): dist
+    for dist in (
+        Delta,
+        IndependentNormal,
+        TanhDelta,
+        TanhNormal,
+        TruncatedNormal,
+        MaskedCategorical,
+        MaskedOneHotCategorical,
+        OneHotCategorical,
+        Ordinal,
+        OneHotOrdinal,
+    )
+}
---
+++ packages/torchrl-nightly/torchrl_nightly-2024.11.16-cp39-cp39-win_amd64.whl/torchrl/version.py
@@ -0,0 +1,2 @@
+__version__ = '2024.11.16'
+git_version = '527a26a27bb36eb6c94e6e4d7bf425c0556b68a0'
