2e5ddef56e892c36c889a2a54151aaf070074297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:38 +0000

    Add package agi.green/0.2.1/agi_green-0.2.1.tar.gz

---
+++ packages/agi.green/agi_green-0.2.1.tar.gz/agi_green-0.2.1/agi_green/frontend/dist/assets/clone-UKt3A5pd.js
@@ -0,0 +1,9 @@
+import { c as baseClone } from "./graph-BgFXEm22.js";
+var CLONE_SYMBOLS_FLAG = 4;
+function clone(value) {
+  return baseClone(value, CLONE_SYMBOLS_FLAG);
+}
+export {
+  clone as c
+};
+//# sourceMappingURL=clone-UKt3A5pd.js.map
