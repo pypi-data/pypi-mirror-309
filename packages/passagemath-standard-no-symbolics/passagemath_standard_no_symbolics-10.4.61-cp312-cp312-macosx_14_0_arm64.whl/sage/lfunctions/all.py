a393ec7701be99119efe1fdfcafd96e195244552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:05:47 +0000

    Add package passagemath-standard-no-symbolics/10.4.61/passagemath_standard_no_symbolics-10.4.61-cp312-cp312-macosx_14_0_arm64.whl

---
+++ packages/passagemath-standard-no-symbolics/passagemath_standard_no_symbolics-10.4.61-cp312-cp312-macosx_14_0_arm64.whl/sage/lfunctions/all.py
@@ -0,0 +1,9 @@
+from .all__sagemath_sympow import *
+
+from sage.misc.lazy_import import lazy_import
+
+lazy_import("sage.lfunctions.dokchitser", "Dokchitser")
+lazy_import("sage.lfunctions.zero_sums", "LFunctionZeroSum")
+lazy_import("sage.lfunctions.lcalc", "lcalc")
+
+del lazy_import
