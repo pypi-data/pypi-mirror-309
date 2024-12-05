3f4a5b13dde69b7a4e1194524d0a4ba1ca5b6202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:20 +0000

    Add package HorridAPI/1.0.68/HorridAPI-1.0.68-py3-none-any.whl

---
+++ packages/HorridAPI/HorridAPI-1.0.68-py3-none-any.whl/HorridAPI/__init__.py
@@ -0,0 +1,10 @@
+from .api import *
+from .Mango import *
+from .AiGenerativeContent import *
+from .MrzSong import *
+from .Llava import LLVA
+from .Async import *
+
+__version__ = "1.0.68"
+
+__all__ = ["api"]
