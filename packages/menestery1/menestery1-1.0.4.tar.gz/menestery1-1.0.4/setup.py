0efab796e44298f88a188aeb25529f6886bc2e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:27 +0000

    Add package menestery1/1.0.4/menestery1-1.0.4.tar.gz

---
+++ packages/menestery1/menestery1-1.0.4.tar.gz/menestery1-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name		= 'menestery1',
+	version		= '1.0.4',
+	py_modules	= ['menestery1'],
+	author		= 'hfpython',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+      )
