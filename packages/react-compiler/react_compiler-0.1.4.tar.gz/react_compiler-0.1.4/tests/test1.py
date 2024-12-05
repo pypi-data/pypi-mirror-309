43556b0e6e20984b24eef22c108a55cae36487d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:08 +0000

    Add package react-compiler/0.1.4/react_compiler-0.1.4.tar.gz

---
+++ packages/react-compiler/react_compiler-0.1.4.tar.gz/react_compiler-0.1.4/tests/test1.py
@@ -0,0 +1,20 @@
+import inspect
+from react import *
+
+def f0(alpha, x: Index('i'), y: Index('i')):
+    return alpha * x + y
+
+def f1(A: Index('i,j'), b: Index('j')):
+    return relu(A + b[None, :])
+
+def f2(alpha, A: Index('i,j')):
+    return where(A < 0, alpha * A, A)
+
+def f3(A: Index('i,j')):
+    b = sum(A, 1)
+    return A / b[:, None]
+
+
+for f in [f0, f1, f2, f3]:
+    newcode = compile_from_src(inspect.getsource(f), trie_fuse=1, parallelize=1)
+    print(newcode)
