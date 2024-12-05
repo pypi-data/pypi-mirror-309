d8dc03e5433d0dcb869c0cc27ad5672d59d58c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:07:33 +0000

    Add package bellatrex/0.2.3/bellatrex-0.2.3.tar.gz

---
+++ packages/bellatrex/bellatrex-0.2.3.tar.gz/bellatrex-0.2.3/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 22 11:21:19 2024
+
+@author: Klest Dedja
+"""
+import os #TODO: consider moving to pathlib for cross-OS consistency.
+from setuptools import find_packages, setup
+
+readme_path = os.path.join('README.md')
+
+with open(readme_path, 'r') as f:
+    text_description = f.read()
+
+
+version_file_path = os.path.join('app', 'bellatrex', 'version.txt')
+
+with open(version_file_path) as version_file:
+    synch_version = version_file.read().strip()
+
+setup(
+    name='bellatrex',
+    version=synch_version,
+    description='A toolbox for Building Explanations through a LocaLly AccuraTe Rule EXtractor',
+    package_dir={'': 'app'}, #where to find the code: in the app subfolder
+    packages=find_packages(where='app'),
+    include_package_data=True, #include additional files (which ones precisely is determined by the MANIFEST.in file)
+    package_data={
+        'bellatrex.datasets': ['*.csv'],
+    },
+    long_description=text_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Klest94/Bellatrex',
+    author='Klest Dedja',
+    author_email='daneel.olivaw94@gmail.com',
+    licence='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',    ],
+    install_requires=['scikit-learn >= 1.2', # we avoid making use (for now) of some validation.utils functions that appear in 1.3.1
+                      'threadpoolctl>=3.1',
+                      'scikit-survival>=0.22',
+                      'scipy>=1.11',
+                      'pandas>=1.5', #to load tutorial datasets
+                      'matplotlib>=3.7'],
+    extras_require={
+        'dev': ['pytest', 'twine'],
+        'gui': ['dearpygui>=1.6.2, <2.0', 'dearpygui-ext>=0.9.5, <1.0'] # for Graphical User Interface (demo)
+    },
+    python_requires='>=3.9, <3.13',
+)
---
+++ packages/bellatrex/bellatrex-0.2.3.tar.gz/bellatrex-0.2.3/tests/test_py_run.py
@@ -0,0 +1,103 @@
+'''
+Author: Klest Dedja
+Here we manually test most of the features
+'''
+import os
+import matplotlib.pyplot as plt
+
+import bellatrex as btrex
+print("Bellatrex version:", btrex.__version__)
+
+MAX_TEST_SAMPLES = 2
+PLOT_GUI = True
+
+root_folder = os.getcwd()
+print(root_folder)
+
+from sksurv.ensemble import RandomSurvivalForest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.model_selection import train_test_split
+
+
+from bellatrex.datasets import load_mtr_data, load_mlc_data
+from bellatrex.datasets import load_survival_data, load_binary_data, load_regression_data
+from bellatrex.utilities import get_auto_setup
+
+# X, y = load_binary_data(return_X_y=True)
+X, y = load_regression_data(return_X_y=True)
+# X, y = load_survival_data(return_X_y=True)
+X, y = load_mlc_data(return_X_y=True)
+# X, y = load_mtr_data(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+SETUP = get_auto_setup(y)
+print('Detected prediction task \'SETUP\':', SETUP)
+
+
+### instantiate original R(S)F estimator, works best with some pruning.
+if SETUP.lower() in 'survival':
+    clf = RandomSurvivalForest(n_estimators=100, min_samples_split=10,
+                                n_jobs=-2, random_state=0)
+
+elif SETUP.lower() in ['binary', 'multi-label']:
+    clf = RandomForestClassifier(n_estimators=100, min_samples_split=5,
+                                n_jobs=-2, random_state=0)
+
+elif SETUP.lower() in ['regression', 'multi-target']:
+    clf = RandomForestRegressor(n_estimators=100, min_samples_split=5,
+                                n_jobs=-2, random_state=0)
+else:
+    raise ValueError(f"Detection task {SETUP} not compatible with Bellatrex (yet)")
+
+
+from bellatrex import BellatrexExplain
+from bellatrex.wrapper_class import pack_trained_ensemble
+
+clf.fit(X_train, y_train)
+print('Model fitting complete.')
+
+clf_packed = pack_trained_ensemble(clf) #lightweight alternative for serialising trained model
+
+# In case a lightweight version of the model is stored, you can re-load the model and make it
+# compatible with Bellatrex through EnsembleWrapper()
+# from bellatrex.wrapper_class import EnsembleWrapper
+# clf2 = EnsembleWrapper(clf_packed)
+
+# fit RF model here. The hyperparameters are given
+# compatible with trained model clf, and with a wrapped dictionary as in clf_packed
+
+Btrex_fitted = BellatrexExplain(clf, set_up='auto',
+                                p_grid={"n_clusters": [1, 2, 3]},
+                                verbose=3).fit(X_train, y_train)
+
+from bellatrex.utilities import predict_helper
+
+for i in range(MAX_TEST_SAMPLES): # iterate for the first few samples in the test set
+
+    print(f"Explaining sample i={i}")
+
+    # Store the extracted rules in dedicated folder, if needed:
+    explan_dir = os.path.join(root_folder, "explanations-out")
+    os.makedirs(explan_dir, exist_ok=True)
+    FILE_OUT = os.path.join(explan_dir, "Rules_"+str(SETUP)+'_id'+str(i)+'.txt')
+
+    y_train_pred = predict_helper(clf, X_train)
+
+    tuned_method = Btrex_fitted.explain(X_test, i)
+    tuned_method.plot_overview(show=True, plot_gui=PLOT_GUI)
+
+    # tuned_method.plot_visuals(plot_max_depth=5,
+    #                           preds_distr=y_train_pred,
+    #                           conf_level=0.9,
+    #                           tot_digits=4)
+    plt.show()
+
+    # tuned_method.plot_overview(show=True)
+
+# if using Qt5Agg backend, the figures will be deleted unless we call the following:
+# plt.show()
+
+# %%
