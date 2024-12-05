ea49344fb156341e85382a3df4f6710db82c64db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:15 +0000

    Add package hanggman/1.0.0/hanggman-1.0.0-py3-none-any.whl

---
+++ packages/hanggman/hanggman-1.0.0-py3-none-any.whl/hangman/start.py
@@ -0,0 +1,12 @@
+from . import efx
+from .sides import LoginSetup
+from .game import Menu
+
+def main():				#THE ORIGIN
+	input(efx.Printer(efx.intro,delay=0.0005))
+
+	LoginSetup()
+
+	Menu()
+
+main()
