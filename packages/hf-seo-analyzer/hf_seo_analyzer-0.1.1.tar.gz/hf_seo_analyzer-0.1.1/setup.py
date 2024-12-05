bed4053e91e37e81b9f38842a079dcf7b054f9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:34 +0000

    Add package hf-seo-analyzer/0.1.1/hf_seo_analyzer-0.1.1.tar.gz

---
+++ packages/hf-seo-analyzer/hf_seo_analyzer-0.1.1.tar.gz/hf_seo_analyzer-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hf_seo_analyzer",
+    version="0.1.1",
+    description="A CLI and programmatic tool to analyze Markdown files for SEO using Huggingface API",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Thinh Vu",
+    author_email="mrthinh@live.com",
+    url="https://github.com/thinh-vu/hf_seo_analyzer",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.0.0",
+        "PyYAML>=5.4",
+        "argparse>=1.4.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "hf-seo-analyzer=hf_seo_analyzer.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
