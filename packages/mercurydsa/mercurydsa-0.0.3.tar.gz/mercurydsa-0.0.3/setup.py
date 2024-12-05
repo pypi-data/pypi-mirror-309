b05531186c002234a5dfb5c2993a8e889f15ae29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:12 +0000

    Add package mercurydsa/0.0.3/mercurydsa-0.0.3.tar.gz

---
+++ packages/mercurydsa/mercurydsa-0.0.3.tar.gz/mercurydsa-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mercurydsa",
+    version="0.0.3",
+    author="Walter Michel Raja",
+    author_email="waltermichelraja@gmail.com",
+    packages=find_packages()
+)
