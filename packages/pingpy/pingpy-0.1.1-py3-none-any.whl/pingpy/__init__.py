e2bbad2aaea40f00a920affaa21f76dacd7dd09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:33 +0000

    Add package pingpy/0.1.1/pingpy-0.1.1-py3-none-any.whl

---
+++ packages/pingpy/pingpy-0.1.1-py3-none-any.whl/pingpy/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+
+from .main import parse_args, ping, set_logging_format
---
+++ packages/pingpy/pingpy-0.1.1-py3-none-any.whl/pingpy/__main__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .main import ping
+
+if __name__ == "__main__":
+    ping()
