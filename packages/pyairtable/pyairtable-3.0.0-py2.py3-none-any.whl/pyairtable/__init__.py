18446cb59483c5dd25d79bdf180499702711940d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:58 +0000

    Add package pyairtable/3.0.0/pyairtable-3.0.0-py2.py3-none-any.whl

---
+++ packages/pyairtable/pyairtable-3.0.0-py2.py3-none-any.whl/pyairtable/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "3.0.0"
+
+from .api import Api, Base, Table
+from .api.enterprise import Enterprise
+from .api.retrying import retry_strategy
+from .api.workspace import Workspace
+
+__all__ = [
+    "Api",
+    "Base",
+    "Enterprise",
+    "Table",
+    "Workspace",
+    "retry_strategy",
+]
