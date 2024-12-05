b5b6866cb59df6b78a2b76e91daf38d1bd0c49f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:33 +0000

    Add package rdascore/4.0.2/rdascore-4.0.2.tar.gz

---
+++ packages/rdascore/rdascore-4.0.2.tar.gz/rdascore-4.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rdascore",
+    version="4.0.2",
+    description="Redistricting analytics for scoring ensembles of redistricting plans",
+    url="https://github.com/rdatools/rdascore",
+    author="alecramsay",
+    author_email="a73cram5ay@gmail.com",
+    license="MIT",
+    packages=[
+        "rdascore",
+    ],
+    install_requires=[
+        "rdabase",
+        "rdapy",
+    ],
+    zip_safe=False,
+)
