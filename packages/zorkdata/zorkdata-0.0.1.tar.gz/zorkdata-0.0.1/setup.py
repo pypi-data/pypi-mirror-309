77162b61ae3023e26510e8f72fb46cfa2a517981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:37:27 +0000

    Add package zorkdata/0.0.1/zorkdata-0.0.1.tar.gz

---
+++ packages/zorkdata/zorkdata-0.0.1.tar.gz/zorkdata-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="zorkdata",
+    version="0.0.1",
+    author="Hussain Sultan",
+    description="Multi-Engine Backplane",
+    packages=[],  # Empty list since this is just for name reservation
+)
