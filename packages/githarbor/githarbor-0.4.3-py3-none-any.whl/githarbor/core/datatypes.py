5ac7efceca069ac9e52c37318d4e0786f65090bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:07 +0000

    Add package githarbor/0.4.3/githarbor-0.4.3-py3-none-any.whl

---
+++ packages/githarbor/githarbor-0.4.3-py3-none-any.whl/githarbor/core/datatypes.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+import reprlib
+from typing import TypeVar
+
+
+T = TypeVar("T")
+
+
+class NiceReprList(list[T]):
+    def __repr__(self):
+        return reprlib.repr(list(self))
