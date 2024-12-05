fe856e9fb3620a73f260b724566d6404e7409b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:08 +0000

    Add package ColourScript/0.0.6/colourscript-0.0.6.tar.gz

---
+++ packages/ColourScript/colourscript-0.0.6.tar.gz/colourscript-0.0.6/README.md
@@ -0,0 +1,11 @@
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/colourscript)](https://pypi.org/project/ColourScript/)
+[![PyPI Licence](https://img.shields.io/pypi/l/colourscript.svg?v=1)](https://pypi.org/search/?c=License+%3A%3A+OSI+Approved+%3A%3A+MIT+License)
+# ColourScript Package
+
+This is a simple yet effective package to manipulate ASCII and other codes in Python. 
+More functionality being added in later versions. Data has been added in version 0.0.6 and above.
+
+>Authored by Tariq Tayebi 2024 Sydney, Australia
+
+ 
---
+++ packages/ColourScript/colourscript-0.0.6.tar.gz/colourscript-0.0.6/src/ColourScript/data.py
@@ -0,0 +1,35 @@
+
+
+class ANSII:
+
+    class codes:
+        COLOURS = {
+            # Text colours
+            "MAGENTA": "\033[35m",
+            "BLUE": "\033[34m",
+            "GREEN": "\033[32m",
+            "RED": "\033[31m",
+            "YELLOW": "\033[33m",
+            "BLACK": "\033[30m",
+            # Bright colours
+            "BRIGHT-RED": "\u001b[31;1m",
+            #Background colours
+            "RED-BACKGROUND": "\033[41m",
+            "BLUE-BACKGROUND": "\033[43m",
+            "GREEN-BACKGROUND": "\033[42m",
+            "YELLOW-BACKGROUND": "\033[44m",
+            "MAGENTA-BACKGROUND": "\033[45m",
+            "GREY-BACKGROUND": "\033[90m",
+            "ALICE-BLUE": "\033[94m",
+        }
+    class text_formatting:
+        FORMATS = {
+            "BOLD": "\033[1m",
+            "ITALIC": "\033[3m",
+            "UNDERLINE": "\033[4m",
+        }
+    class characters:
+        CHARACTERS = [
+        #     use a .csv file
+        ]
+
