7a53d672270d053a9df9a5aaf6dfb9792449e3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:50 +0000

    Add package browser-use/0.1.3/browser_use-0.1.3.tar.gz

---
+++ packages/browser-use/browser_use-0.1.3.tar.gz/browser_use-0.1.3/examples/simple_run.py
@@ -0,0 +1,29 @@
+"""
+Simple try of the agent.
+
+@dev You need to add OPENAI_API_KEY to your environment variables.
+"""
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import asyncio
+
+from langchain_openai import ChatOpenAI
+
+from browser_use import Agent
+
+llm = ChatOpenAI(model='gpt-4o')
+agent = Agent(
+	task='Find a one-way flight from Bali to Oman on 12 January 2025 on Google Flights. Return me the cheapest option.',
+	llm=ChatOpenAI(model='gpt-4o'),
+)
+
+
+async def main():
+	await agent.run()
+
+
+asyncio.run(main())
