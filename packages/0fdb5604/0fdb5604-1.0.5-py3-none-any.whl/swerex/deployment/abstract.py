84bc4d3586bf55c972e46761f46eba2031532c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:27 +0000

    Add package 0fdb5604/1.0.5/0fdb5604-1.0.5-py3-none-any.whl

---
+++ packages/0fdb5604/0fdb5604-1.0.5-py3-none-any.whl/swerex/deployment/abstract.py
@@ -0,0 +1,39 @@
+import asyncio
+from abc import ABC, abstractmethod
+
+from swerex.runtime.abstract import AbstractRuntime, IsAliveResponse
+
+__all__ = ["AbstractDeployment"]
+
+
+class AbstractDeployment(ABC):
+    @abstractmethod
+    async def is_alive(self, *, timeout: float | None = None) -> IsAliveResponse:
+        """Checks if the runtime is alive. The return value can be
+        tested with bool().
+
+        Raises:
+            DeploymentNotStartedError: If the deployment was not started.
+        """
+
+    @abstractmethod
+    async def start(self, *args, **kwargs):
+        """Starts the runtime."""
+
+    @abstractmethod
+    async def stop(self, *args, **kwargs):
+        """Stops the runtime."""
+
+    @property
+    @abstractmethod
+    def runtime(self) -> AbstractRuntime:
+        """Returns the runtime if running.
+
+        Raises:
+            DeploymentNotStartedError: If the deployment was not started.
+        """
+
+    def __del__(self):
+        """Stops the runtime when the object is deleted."""
+        print("Stopping runtime because Deployment object is deleted")
+        asyncio.run(self.stop())
