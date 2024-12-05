d45cac2c2e5e82a05909d17b4c793f4183492f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:48 +0000

    Add package ez-a-sync/0.23.0/ez_a_sync-0.23.0.tar.gz

---
+++ packages/ez-a-sync/ez_a_sync-0.23.0.tar.gz/ez_a_sync-0.23.0/setup.py
@@ -0,0 +1,30 @@
+from Cython.Build import cythonize
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    requirements = list(map(str.strip, f.read().split("\n")))[:-1]
+
+setup(
+    name="ez-a-sync",
+    packages=find_packages(),
+    use_scm_version={
+        "root": ".",
+        "relative_to": __file__,
+        "local_scheme": "no-local-version",
+        "version_scheme": "python-simplified-semver",
+    },
+    description="A library that makes it easy to define objects that can be used for both sync and async use cases.",
+    author="BobTheBuidler",
+    author_email="bobthebuidlerdefi@gmail.com",
+    url="https://github.com/BobTheBuidler/a-sync",
+    license="MIT",
+    install_requires=requirements,
+    setup_requires=["setuptools_scm"],
+    python_requires=">=3.8,<3.13",
+    package_data={
+        "a_sync": ["py.typed"],
+    },
+    ext_modules=cythonize("a_sync/a_sync/_flags.pyx")
+    + cythonize("a_sync/a_sync/_kwargs.pyx"),
+    zip_safe=False,
+)
