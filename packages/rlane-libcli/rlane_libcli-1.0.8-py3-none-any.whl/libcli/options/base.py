944f3c5657533f1a61656b25a5a03bc2d57f7f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:08:22 +0000

    Add package rlane-libcli/1.0.8/rlane_libcli-1.0.8-py3-none-any.whl

---
+++ packages/rlane-libcli/rlane_libcli-1.0.8-py3-none-any.whl/libcli/options/base.py
@@ -0,0 +1,8 @@
+"""Base class for our `Option`s."""
+
+__all__ = ["BaseOption"]
+
+
+class BaseOption:
+    # pylint: disable=too-few-public-methods
+    """Base class for our `Option`s."""
---
+++ packages/rlane-libcli/rlane_libcli-1.0.8-py3-none-any.whl/libcli/options/printurl.py
@@ -0,0 +1,46 @@
+"""Print `Project-URL` and exit."""
+
+import argparse
+import contextlib
+import importlib.metadata
+from typing import Any
+
+from libcli.actions.basehelp import BaseHelpAction
+from libcli.options.base import BaseOption
+
+__all__ = ["PrintUrlOption"]
+
+
+class PrintUrlOption(BaseOption):
+    # pylint: disable=too-few-public-methods
+    """Print `Project-URL` and exit."""
+
+    def __init__(self, parser: argparse.ArgumentParser | argparse._ArgumentGroup) -> None:
+        """Print `Project-URL` and exit."""
+
+        parser.add_argument(
+            "--print-url",
+            action=PrintUrlAction,
+            help="print project url and exit",
+        )
+
+
+class PrintUrlAction(BaseHelpAction):
+    """Print `Project-URL` and exit."""
+
+    def __call__(
+        self,
+        parser: argparse.ArgumentParser,
+        namespace: argparse.Namespace,
+        _values: Any,
+        _option_string: Any = None,
+    ) -> None:
+        """Print `Project-URL` and exit."""
+
+        with contextlib.suppress(importlib.metadata.PackageNotFoundError):
+            # https://packaging.python.org/en/latest/specifications/core-metadata/#project-url-multiple-use
+            distro = importlib.metadata.distribution(namespace.cli.distname)
+            if distro is not None and distro.metadata is not None:
+                print(distro.metadata.get("Project-URL", ""))
+
+        parser.exit()
