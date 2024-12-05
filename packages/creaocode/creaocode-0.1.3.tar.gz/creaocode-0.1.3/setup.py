824b7398ba59bb374f344b884456c665620da539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:51 +0000

    Add package creaocode/0.1.3/creaocode-0.1.3.tar.gz

---
+++ packages/creaocode/creaocode-0.1.3.tar.gz/creaocode-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='creaocode',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    author='dev@creao.ai',
+    author_email='dev@creao.ai',
+    description='Creao AI Core Library',
+    url='https://www.creao.ai/login',
+    classifiers=[
+        # Trove classifiers
+    ],
+)
