00adcef624cccb7e6027adb58c3ed086fe972221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:35 +0000

    Add package tabs2music/0.0.11/tabs2music-0.0.11.tar.gz

---
+++ packages/tabs2music/tabs2music-0.0.11.tar.gz/tabs2music-0.0.11/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tabs2music",
+    version="0.0.11",
+    packages=find_packages(),
+    install_requires=[
+        "midi2audio==0.1.1",
+        "MIDIUtil==1.2.1",
+    ],
+    author="Yusef Ahmed",
+    description="Converts guitar tabs (.txt) into audio (.wav)",
+    license='MIT',
+    url='https://github.com/yusefahmd/tabs2music',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
