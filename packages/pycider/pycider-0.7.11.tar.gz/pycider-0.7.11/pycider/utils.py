50ab96faf51ab9beb6d7d66e9a8c442c592b6a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:57 +0000

    Add package pycider/0.7.11/pycider-0.7.11.tar.gz

---
+++ packages/pycider/pycider-0.7.11.tar.gz/pycider-0.7.11/pycider/utils.py
@@ -0,0 +1,45 @@
+from typing import Generic, Sequence, TypeVar
+
+from pycider.deciders import Decider
+
+E = TypeVar("E")
+C = TypeVar("C")
+S = TypeVar("S")
+
+
+class InMemory(Generic[E, C, S]):
+    """Runs a decider in memory, performing decide and evolving the state."""
+
+    def __init__(self, decider: Decider[E, C, S]) -> None:
+        """Setup an in-memory executor for a decider.
+
+        Parameters:
+            decider: The decider to be stored internally.
+        """
+        self._decider = decider
+        self.state: S = self._decider.initial_state()  #: State of the decider
+
+    def command(self, command: C) -> Sequence[E]:
+        """Decide over a command and evolves the internal state.
+
+        Parameters:
+            command: Command to decide over
+
+        Returns:
+            The events that were evolved over resulting from decide
+        """
+        events = self._decider.decide(command, self.state)
+        for event in events:
+            self.state = self._decider.evolve(self.state, event)
+        return events
+
+    def __call__(self, command: C) -> Sequence[E]:
+        """Decide over a command and evolves the internal state.
+
+        Parameters:
+            command: Command to decide over
+
+        Returns:
+            The events that were evolved over resulting from decide
+        """
+        return self.command(command)
