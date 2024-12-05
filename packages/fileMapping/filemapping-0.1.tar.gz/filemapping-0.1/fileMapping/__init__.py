0f01d1d5edb766cb390c9f21649edf791dac9a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:10 +0000

    Add package fileMapping/0.1/filemapping-0.1.tar.gz

---
+++ packages/fileMapping/filemapping-0.1.tar.gz/filemapping-0.1/fileMapping/__init__.py
@@ -0,0 +1,5 @@
+from fileMapping import pluginLoading
+from fileMapping import plugIns
+
+File: plugIns.File = plugIns.File
+pathConversion: plugIns.pathConversion = plugIns.pathConversion
---
+++ packages/fileMapping/filemapping-0.1.tar.gz/filemapping-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fileMapping',
+    version='0.1',
+    author='朝歌夜弦',
+    author_email='bop-lp@qq.com',
+    description='',
+    packages=find_packages(),
+    install_requires=[],
+)
+
+# running check
