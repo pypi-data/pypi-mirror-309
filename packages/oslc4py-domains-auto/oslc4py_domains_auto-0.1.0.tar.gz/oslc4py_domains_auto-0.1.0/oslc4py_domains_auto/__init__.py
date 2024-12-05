b0fda9c710ccc33c58c0f9c581f0423f6bfeefdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:11 +0000

    Add package oslc4py-domains-auto/0.1.0/oslc4py_domains_auto-0.1.0.tar.gz

---
+++ packages/oslc4py-domains-auto/oslc4py_domains_auto-0.1.0.tar.gz/oslc4py_domains_auto-0.1.0/oslc4py_domains_auto/__init__.py
@@ -0,0 +1,6 @@
+from .AutomationRequest import AutomationRequest
+from .Contribution import Contribution
+from .AutomationResult import AutomationResult
+from .AutomationPlan import AutomationPlan
+from .ParameterDefinition import ParameterDefinition
+from .ParameterInstance import ParameterInstance
