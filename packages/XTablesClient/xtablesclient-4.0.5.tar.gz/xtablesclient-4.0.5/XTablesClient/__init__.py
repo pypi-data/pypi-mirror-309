d49ea2fa37fbe63625003d486b3cdfb0904ecfab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:50 +0000

    Add package XTablesClient/4.0.5/xtablesclient-4.0.5.tar.gz

---
+++ packages/XTablesClient/xtablesclient-4.0.5.tar.gz/xtablesclient-4.0.5/XTablesClient/__init__.py
@@ -0,0 +1,7 @@
+from .XTablesClient import XTablesClient
+from .ClientStatistics import *
+from .Utilities import *
+from .SocketClient import *
+
+# Specify what is exported when `import XTablesClient` is used
+__all__ = ["XTablesClient"]
