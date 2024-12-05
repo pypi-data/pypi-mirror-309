189f308ab18d70827859a3cc6cec092b7ba73f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:27 +0000

    Add package badbyte/0.1.11/badbyte-0.1.11.tar.gz

---
+++ packages/badbyte/badbyte-0.1.11.tar.gz/badbyte-0.1.11/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open(os.path.join('badbyte', 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name='badbyte',
+    version=version,
+    description='Deal with bad characters easily during exploit writing with badchars.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Karol Jerzy Celiński',
+    author_email='karol@celin.pl',
+    packages=find_packages(),
+    package_data={
+        '': ['VERSION'],
+    },
+    url='https://github.com/C3l1n/badbyte',
+    install_requires=[
+        'pwntools>=4.11.1'
+    ],
+    license='MIT',
+    scripts=[
+        "badbyte/badbyte"
+    ],
+)
+
