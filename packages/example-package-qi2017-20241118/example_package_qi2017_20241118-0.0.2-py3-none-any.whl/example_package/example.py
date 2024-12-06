9935795220bdb22d38ddb1ed3e40dd364151f9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:05 +0000

    Add package example-package-qi2017-20241118/0.0.2/example_package_qi2017_20241118-0.0.2-py3-none-any.whl

---
+++ packages/example-package-qi2017-20241118/example_package_qi2017_20241118-0.0.2-py3-none-any.whl/example_package/example.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File  : example.py
+@Author: crawlSpider
+@Address: https://weixin.sogou.com/weixin?type=1&s_from=input&query=%E7%BD%91%E8%99%ABspider&ie=utf8&_sug_=n&_sug_type_=
+@Date  : 2021/7/24 上午12:57
+@Desc  : 
+"""
+import time
+
+
+def add_one(number):
+    return number + 1.
+
+def add_two():
+    return time.time()
+
+
+if __name__ == '__main__':
+    pass
