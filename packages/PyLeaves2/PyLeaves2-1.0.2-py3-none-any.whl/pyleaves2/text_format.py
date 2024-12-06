b03ed165fcc94b0651b5169cf29060c84c3ca1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:41 +0000

    Add package PyLeaves2/1.0.2/PyLeaves2-1.0.2-py3-none-any.whl

---
+++ packages/PyLeaves2/PyLeaves2-1.0.2-py3-none-any.whl/pyleaves2/text_format.py
@@ -0,0 +1,15 @@
+# Copyright (©) 2025 https://github.com/nafis01aa
+# Telegram Link : https://t.me/NafisMuhtadi 
+# Repo Link : https://github.com/nafis01aa/PyLeaves2
+# License Link : https://github.com/nafis01aa/PyLeaves2/blob/main/LICENSE
+
+
+
+#example template 
+PROGRESS_BAR = """\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗂️ : {current} | {total}
+┣⪼ ⏳️ : {percentage}%
+┣⪼ 🚀 : {speed}/s
+┣⪼ ⏱️ : {est_time}
+╰━━━━━━━━━━━━━━━➣ """
---
+++ packages/PyLeaves2/PyLeaves2-1.0.2-py3-none-any.whl/pyleaves2/utils.py
@@ -0,0 +1,42 @@
+# Copyright (©) 2025 https://github.com/nafis01aa
+# Telegram Link : https://t.me/NafisMuhtadi 
+# Repo Link : https://github.com/nafis01aa/PyLeaves2
+# License Link : https://github.com/nafis01aa/PyLeaves2/blob/main/LICENSE
+
+
+import math, time
+
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2] 
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+
+
