7e261519146feb8b554f043d0f7fb9fd917f8187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:38 +0000

    Add package icds-data-definitions/0.0.11/icds_data_definitions-0.0.11.tar.gz

---
+++ packages/icds-data-definitions/icds_data_definitions-0.0.11.tar.gz/icds_data_definitions-0.0.11/icds_data_definitions/aerial_corridor/v1/schema.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class AerialCorridorRead(BaseModel):
+    "A class to read data from the Aerial Corridor table "
+    
+    url: str
+    min_cell_level: int
+    max_cell_level: int
+    start_datetime: str
+    end_datetime: str
+    created_at: str
+    updated_at: str
