2ff80150b975d6ac56efff43e41a9423c3d25fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:07:19 +0000

    Add package webinteract/1.11/webinteract-1.11.tar.gz

---
+++ packages/webinteract/webinteract-1.11.tar.gz/webinteract-1.11/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='webinteract',
+    version='1.11',    
+    description='A Python module for simple web interaction',
+    url='https://blog.pg12.org/web-page-interaction-in-python',
+    author='A Andersen',
+    author_email='a.andersen@pg12.org',
+    license='Modified BSD License',
+    packages=['webinteract'],
+    install_requires=['splinter',
+                      'keyring',                     
+                      'smartinput'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',  
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
