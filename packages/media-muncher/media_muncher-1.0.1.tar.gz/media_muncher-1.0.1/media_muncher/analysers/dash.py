d25c7b5493369675d703e39167c0a2572e877629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:14:20 +0000

    Add package media-muncher/1.0.1/media_muncher-1.0.1.tar.gz

---
+++ packages/media-muncher/media_muncher-1.0.1.tar.gz/media_muncher-1.0.1/media_muncher/analysers/dash.py
@@ -0,0 +1,54 @@
+from loguru import logger
+from media_muncher.codecstrings import CodecStringParser
+from media_muncher.handlers.dash import DASHHandler
+from mpd_inspector.parser.mpd_tags import MPD
+
+
+class DashAnalyser:
+    def __init__(self, handler: DASHHandler) -> None:
+        self.handler = handler
+        self.messages = []
+
+    def extract_renditions(self):
+        dash_obj: MPD = self.handler.document
+
+        if len(dash_obj.periods) > 1:
+            logger.warning(
+                "More than 1 period found. Only the first period will be taken into account"
+            )
+
+        period = dash_obj.periods[0]
+
+        audio_renditions = {}
+        video_renditions = {}
+
+        for adaptation_set in period.adaptation_sets:
+            if (
+                adaptation_set.content_type == "audio"
+                or adaptation_set.mime_type.startswith("audio")
+            ):
+                for representation in adaptation_set.representations:
+                    codec = CodecStringParser.parse_codec_string(
+                        representation.codecs or adaptation_set.codecs
+                    )
+
+                    codec["bitrate"] = representation.bandwidth
+                    audio_renditions[representation.id] = codec
+
+            if (
+                adaptation_set.content_type == "video"
+                or adaptation_set.mime_type.startswith("video")
+            ):
+                for representation in adaptation_set.representations:
+                    codec = CodecStringParser.parse_codec_string(
+                        representation.codecs or adaptation_set.codecs
+                    )
+                    codec["resolution"] = (representation.width, representation.height)
+                    codec["bitrate"] = representation.bandwidth
+                    codec["framerate"] = representation.frame_rate
+                    video_renditions[representation.id] = codec
+
+        return [*video_renditions.values(), *audio_renditions.values()]
+
+    def extract_packaging_info(self):
+        return {"container": "ISOBMFF"}
