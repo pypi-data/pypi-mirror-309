f28df414de0b47bd00604766e4d124d28183bd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:03:42 +0000

    Add package fairbench/0.4.1/fairbench-0.4.1-py3-none-any.whl

---
+++ packages/fairbench/fairbench-0.4.1-py3-none-any.whl/fairbench/bench/__init__.py
@@ -0,0 +1,3 @@
+from fairbench.bench.loader import *
+from fairbench.bench import fallbacks
+from fairbench.bench import demos
---
+++ packages/fairbench/fairbench-0.4.1-py3-none-any.whl/fairbench/bench/fallbacks/learning/__init__.py
@@ -0,0 +1,4 @@
+from fairbench.bench.fallbacks.learning.auc import *
+from fairbench.bench.fallbacks.learning.logistic_regression import *
+from fairbench.bench.fallbacks.learning.min_max_scaler import *
+from fairbench.bench.fallbacks.learning.train_test_split import *
