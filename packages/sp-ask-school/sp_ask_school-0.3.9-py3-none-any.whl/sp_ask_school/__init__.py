6e7b45d1b489edee8fae4e191ef1effba44261b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:49 +0000

    Add package sp-ask-school/0.3.9/sp_ask_school-0.3.9-py3-none-any.whl

---
+++ packages/sp-ask-school/sp_ask_school-0.3.9-py3-none-any.whl/sp_ask_school/__init__.py
@@ -0,0 +1,17 @@
+from .schools import (
+    find_school_by_operator_suffix,
+    find_queues_from_a_school_name,
+    get_shortname_by_full_school_name,
+    find_school_by_queue_or_profile_name,
+    find_school_abbr_by_queue_or_profile_name,
+    find_queue_by_criteria,
+    find_routing_model_by_profile_name,
+    sp_ask_school_dict,
+    FRENCH_QUEUES,
+    SMS_QUEUES,
+    PRACTICE_QUEUES,
+    HTF_schools,
+)
+
+__version__ = "0.3.9"
+__author__ = "Guinsly Mondésir"
