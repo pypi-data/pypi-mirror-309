53a9c1fe2744c1a2e9074bd2db7940ad74f7716b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:39 +0000

    Add package viturka-nn/0.1.31/viturka_nn-0.1.31.tar.gz

---
+++ packages/viturka-nn/viturka_nn-0.1.31.tar.gz/viturka_nn-0.1.31/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='viturka_nn',
+    version='0.1.31',
+    description='A client library for deep federated learning platform.',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'torch',
+        'torchvision',
+        'torchmetrics>=0.11.0',
+        'pycocotools',
+        'scikit-learn'
+    ],
+    python_requires='>=3.6'
+)
