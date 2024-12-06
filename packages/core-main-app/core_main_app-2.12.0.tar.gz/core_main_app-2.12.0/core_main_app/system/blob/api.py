113403020f779603d62141b8b64636eb6a06d755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:31 +0000

    Add package core-main-app/2.12.0/core_main_app-2.12.0.tar.gz

---
+++ packages/core-main-app/core_main_app-2.12.0.tar.gz/core_main_app-2.12.0/core_main_app/system/blob/api.py
@@ -0,0 +1,16 @@
+""" System API for Blob objects.
+"""
+
+from core_main_app.components.blob.models import Blob
+
+
+def get_by_id(blob_id):
+    """Return blob by its id.
+
+    Args:
+        blob_id:
+
+    Returns:
+
+    """
+    return Blob.get_by_id(blob_id)
