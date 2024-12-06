d325fe4b538d1ab33e0526ae64c54e6a70e93f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:46 +0000

    Add package PaperlogicSDK/1.0.5/PaperlogicSDK-1.0.5-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/PaperlogicSDK/PaperlogicSDK-1.0.5-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/PaperlogicSDK/__init__.py
@@ -0,0 +1,83 @@
+import click
+from .sign import sign_pplg, sign_test
+from .timestamp import timestamp_pplg, timestamp_test
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def cli():
+    pass
+
+@cli.command()
+@click.option('-i', '--input_file', type=str, help='File input', required=True)
+@click.option('-o', '--output_file', type=str, help='File output', required=True)
+@click.option('-tk', '--api_token', type=str, help='API Token', required=True)
+@click.option('-t', '--tenant_id', type=int, help='TenantID', required=True)
+@click.option('-pki', '--pki', type=int, help='Certificate Type', required=True, default=0)
+@click.option('-uid', '--user_id', type=int, help='UserID')
+@click.option('-e', '--email', type=str, help='Email')
+def sign(input_file, output_file, api_token, tenant_id, pki, user_id=None, email=None):
+    """Sign document"""
+    logo = """
+    +------------------------------+
+    | Sign document by Paperlogic  |
+    +------------------------------+
+    """
+    click.echo(f"Start Signing")
+    sign_pplg(input_file, output_file, api_token, tenant_id, pki, user_id, email)
+    click.echo(f"Complete Signing")
+    click.echo(logo)
+
+@cli.command()
+@click.option('-i', '--input_file', type=str, help='File input', required=True)
+@click.option('-o', '--output_file', type=str, help='File output', required=True)
+@click.option('-tk', '--api_token', type=str, help='API Token', required=True)
+@click.option('-t', '--tenant_id', type=int, help='TenantID', required=True)
+def timestamp(input_file, output_file, api_token, tenant_id):
+    """Timestamp document"""
+    logo = """
+    +-----------------------------------+
+    | Timestamp document by Paperlogic  |
+    +-----------------------------------+
+    """
+    click.echo(f"Start Timestamp")
+    timestamp_pplg(input_file, output_file, api_token, tenant_id)
+    click.echo(f"Complete Timestamp")
+    click.echo(logo)
+
+@cli.command()
+@click.option('-i', '--input_file', type=str, help='File input', required=True)
+@click.option('-o', '--output_file', type=str, help='File output', required=True)
+@click.option('-tk', '--api_token', type=str, help='API Token', required=True)
+@click.option('-t', '--tenant_id', type=int, help='TenantID', required=True)
+@click.option('-pki', '--pki', type=int, help='Certificate Type', required=True, default=0)
+@click.option('-uid', '--user_id', type=int, help='UserID')
+@click.option('-e', '--email', type=str, help='Email')
+def testsign(input_file, output_file, api_token, tenant_id, pki, user_id=None, email=None):
+    """Test sign document"""
+    logo = """
+    +-----------------------------------+
+    | Test sign document by Paperlogic  |
+    +-----------------------------------+
+    """
+    click.echo(f"Start Signing")
+    sign_test(input_file, output_file, api_token, tenant_id, pki, user_id, email)
+    click.echo(f"Complete Signing")
+    click.echo(logo)
+
+@cli.command()
+@click.option('-i', '--input_file', type=str, help='File input', required=True)
+@click.option('-o', '--output_file', type=str, help='File output', required=True)
+@click.option('-tk', '--api_token', type=str, help='API Token', required=True)
+@click.option('-t', '--tenant_id', type=int, help='TenantID', required=True)
+def testtimestamp(input_file, output_file, api_token, tenant_id):
+    """Test timestamp document"""
+    logo = """
+    +----------------------------------------+
+    | Test timestamp document by Paperlogic  |
+    +----------------------------------------+
+    """
+    click.echo(f"Start Timestamp")
+    timestamp_test(input_file, output_file, api_token, tenant_id)
+    click.echo(f"Complete Timestamp")
+    click.echo(logo)
