7231e692cc30e82361a8d630441b54ba69f0b7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:44 +0000

    Add package audb/1.10.2/audb-1.10.2-py3-none-any.whl

---
+++ packages/audb/audb-1.10.2-py3-none-any.whl/audb/core/config.py
@@ -0,0 +1,108 @@
+import os
+
+import oyaml as yaml
+
+import audeer
+
+from audb.core.define import CONFIG_FILE
+from audb.core.define import USER_CONFIG_FILE
+from audb.core.repository import Repository
+
+
+def load_configuration_file(config_file: str):
+    r"""Read configuration from YAML file.
+
+    Args:
+        config_file: path to configuration file.
+            File doesn't have to exist
+
+    Returns:
+        dictionary containing configuration entries
+
+    Raises:
+        ValueError: if ``repositories`` section is present,
+            but empty
+        ValueError: if repository has no ``host``
+            ``backend``, or ``name`` key
+
+    """
+    if os.path.exists(config_file):
+        with open(config_file) as cf:
+            config = yaml.load(cf, Loader=yaml.BaseLoader)
+    else:
+        config = {}
+
+    # Check that we have provided a valid repositories configuration
+    if "repositories" in config:
+        if len(config["repositories"]) == 0:
+            raise ValueError(
+                "You cannot specify an empty 'repositories:' section "
+                f"in the configuration file '{USER_CONFIG_FILE}'."
+            )
+        for n, repo in enumerate(config["repositories"]):
+            if "host" not in repo:
+                raise ValueError(
+                    f"Your repository is missing a 'host' entry: '{repo}'."
+                )
+            if "backend" not in repo:
+                raise ValueError(
+                    f"Your repository is missing a 'backend' entry: '{repo}'."
+                )
+            if "name" not in repo:
+                raise ValueError(
+                    f"Your repository is missing a 'name' entry: '{repo}'."
+                )
+
+    return config
+
+
+# Read in configuration from global and user file
+root = os.path.dirname(os.path.realpath(__file__))
+global_config_file = os.path.join(root, CONFIG_FILE)
+user_config_file = audeer.path(USER_CONFIG_FILE)
+global_config = load_configuration_file(global_config_file)
+user_config = load_configuration_file(user_config_file)
+global_config.update(user_config)
+
+
+class config:
+    """Get/set configuration values for the :mod:`audb` module.
+
+    The configuration values are read in during module import
+    from the :ref:`configuration file <configuration>`
+    :file:`~/.audb.yaml`.
+    You can change the configuration values after import,
+    by setting the attributes directly.
+    The :ref:`caching <caching>` related configuration values
+    can be overwritten by environment variables.
+
+    Examples:
+        >>> config.CACHE_ROOT
+        '~/audb'
+        >>> config.CACHE_ROOT = "~/caches/audb"
+        >>> config.CACHE_ROOT
+        '~/caches/audb'
+
+    """
+
+    CACHE_ROOT = global_config["cache_root"]
+    r"""Default user cache folder."""
+
+    REPOSITORIES = [
+        Repository(r["name"], r["host"], r["backend"])
+        for r in global_config["repositories"]
+    ]
+    r"""Repositories, will be iterated in given order.
+
+    A repository is defined by the object :class:`audb.Repository`,
+    containing the following attributes:
+
+    * :attr:`audb.Repository.name`: repository name, e.g. ``'data-local'``
+    * :attr:`audb.Repository.backend`: backend name, e.g. ``'artifactory'``
+    * :attr:`audb.Repository.host`: host name,
+      e.g. ``'https://artifactory.audeering.com/artifactory'``
+
+    """
+
+    SHARED_CACHE_ROOT = global_config["shared_cache_root"]
+    r"""Shared cache folder."""
---
+++ packages/audb/audb-1.10.2-py3-none-any.whl/docs/dependencies.rst
@@ -0,0 +1,134 @@
+.. Specify repository to overwrite local config files
+.. jupyter-execute::
+    :hide-code:
+    :hide-output:
+
+    import audb
+
+    audb.config.REPOSITORIES = [
+        audb.Repository(
+            name="data-public",
+            host="https://audeering.jfrog.io/artifactory",
+            backend="artifactory",
+        )
+    ]
+
+.. Specify pandas format output in cells
+.. jupyter-execute::
+    :hide-code:
+    :hide-output:
+
+    import pandas as pd
+
+    pd.set_option("display.max_columns", 7)
+
+
+.. _database-dependencies:
+
+Database dependencies
+=====================
+
+Media and table files of databases are stored
+in archive files.
+A database can also reuse an archive file
+from a previous version of a database
+if its content hasn't changed.
+
+We keep track of those dependencies
+and store some additional metadata about the audio files
+like duration and number of channels
+in a dependency table in a file :file:`db.parquet`
+for every version of a database.
+
+You request a :class:`audb.Dependencies` object with
+:func:`audb.dependencies`.
+
+.. jupyter-execute::
+
+    deps = audb.dependencies("emodb", version="1.4.1")
+
+You can see all entries by calling the returned object.
+
+.. jupyter-execute::
+
+    df = deps()
+    df.head()
+
+You can also use it to request certain aspects, e.g.
+
+.. jupyter-execute::
+
+    deps.duration("wav/03a01Fa.wav")
+
+See :class:`audb.Dependencies` for all available methods.
+
+
+Duration of a database
+----------------------
+
+If your database contains only WAV or FLAC files,
+we store the duration in seconds of every file
+in the database dependency table.
+
+.. jupyter-execute::
+
+    deps = audb.dependencies("emodb", version="1.4.1")
+    df = deps()
+    df.duration[:10]
+
+For those databases
+you can get their overall duration with:
+
+.. jupyter-execute::
+
+    audb.info.duration("emodb", version="1.4.1")
+
+The duration of parts of a database
+can be calculated
+by first loading the dependency table
+and filter for the selected media files.
+The following calculates the duration
+of the first ten files in the *emotion* table
+of the emodb database.
+
+.. jupyter-execute::
+
+    import numpy as np
+
+    df = audb.load_table("emodb", "emotion", version="1.4.1", verbose=False)
+    files = df.index[:10]
+    duration_in_sec = np.sum([deps.duration(f) for f in files])
+    pd.to_timedelta(duration_in_sec, unit="s")
+
+If your table is a segmented table,
+and you would like to get the duration
+of its segments
+that contain a label
+you can use :func:`audformat.utils.duration`,
+which calculates the duration
+from the ``start`` and ``end`` entries.
+
+.. code-block:: python
+
+    df = audb.load_table("database-with-segmented-tables", "segmented-table")
+    audformat.utils.duration(df.dropna())
+
+Or you can count the duration of all segments within your database.
+
+.. code-block:: python
+
+    db = audb.load("database-with-segmented-tables", only_metadata=True)
+    audformat.utils.duration(db.segments)
+
+If your database contains files
+for which no duration information is stored
+in the dependency table of the database,
+like MP4 files,
+you have to download the database first
+and use :func:`audformat.utils.duration`
+to calculate the duration on the fly.
+
+.. code-block:: python
+
+    db = audb.load("database-with-videos")
+    audformat.utils.duration(db.files, num_workers=4)
