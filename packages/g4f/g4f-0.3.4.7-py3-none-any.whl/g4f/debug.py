042c477d5bc4f2e1a4544cb0f639856684f39df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:47 +0000

    Add package g4f/0.3.4.7/g4f-0.3.4.7-py3-none-any.whl

---
+++ packages/g4f/g4f-0.3.4.7-py3-none-any.whl/g4f/debug.py
@@ -0,0 +1,12 @@
+from .providers.types import ProviderType
+
+logging: bool = False
+version_check: bool = True
+last_provider: ProviderType = None
+last_model: str = None
+version: str = None
+log_handler: callable = print
+
+def log(text):
+    if logging:
+        log_handler(text)
