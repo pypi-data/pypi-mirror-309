b3599f9582db75e8c79b23ac8523dbe918429680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:42 +0000

    Add package ceo-py/0.6.3rc0/ceo_py-0.6.3rc0-py3-none-any.whl

---
+++ packages/ceo-py/ceo_py-0.6.3rc0-py3-none-any.whl/ceo/__init__.py
@@ -0,0 +1,7 @@
+import ceo.util.logger
+from .brain.agent import Agent
+from .brain.lm import get_openai_model
+
+__AUTHOR__ = '吴子豪 / Vortez Wohl'
+__EMAIL__ = 'vortez.wohl@gmail.com'
+__VERSION__ = '0.6.3-preview'
