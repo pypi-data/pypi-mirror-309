3853540f7f9a4b8750ceb5dbf572a61ca2377d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:27 +0000

    Add package stac-fastapi.pgstac/3.0.1/stac_fastapi.pgstac-3.0.1-py3-none-any.whl

---
+++ packages/stac-fastapi.pgstac/stac_fastapi.pgstac-3.0.1-py3-none-any.whl/stac_fastapi/pgstac/app.py
@@ -0,0 +1,137 @@
+"""FastAPI application using PGStac.
+
+Enables the extensions specified as a comma-delimited list in
+the ENABLED_EXTENSIONS environment variable (e.g. `transactions,sort,query`).
+If the variable is not set, enables all extensions.
+"""
+
+import os
+
+from brotli_asgi import BrotliMiddleware
+from fastapi.responses import ORJSONResponse
+from stac_fastapi.api.app import StacApi
+from stac_fastapi.api.middleware import CORSMiddleware, ProxyHeaderMiddleware
+from stac_fastapi.api.models import (
+    ItemCollectionUri,
+    create_get_request_model,
+    create_post_request_model,
+    create_request_model,
+)
+from stac_fastapi.extensions.core import (
+    FieldsExtension,
+    FilterExtension,
+    SortExtension,
+    TokenPaginationExtension,
+    TransactionExtension,
+)
+from stac_fastapi.extensions.third_party import BulkTransactionExtension
+from starlette.middleware import Middleware
+
+from stac_fastapi.pgstac.config import Settings
+from stac_fastapi.pgstac.core import CoreCrudClient
+from stac_fastapi.pgstac.db import close_db_connection, connect_to_db
+from stac_fastapi.pgstac.extensions import QueryExtension
+from stac_fastapi.pgstac.extensions.filter import FiltersClient
+from stac_fastapi.pgstac.transactions import BulkTransactionsClient, TransactionsClient
+from stac_fastapi.pgstac.types.search import PgstacSearch
+
+settings = Settings()
+extensions_map = {
+    "transaction": TransactionExtension(
+        client=TransactionsClient(),
+        settings=settings,
+        response_class=ORJSONResponse,
+    ),
+    "query": QueryExtension(),
+    "sort": SortExtension(),
+    "fields": FieldsExtension(),
+    "pagination": TokenPaginationExtension(),
+    "filter": FilterExtension(client=FiltersClient()),
+    "bulk_transactions": BulkTransactionExtension(client=BulkTransactionsClient()),
+}
+
+if enabled_extensions := os.getenv("ENABLED_EXTENSIONS"):
+    extensions = [
+        extensions_map[extension_name] for extension_name in enabled_extensions.split(",")
+    ]
+else:
+    extensions = list(extensions_map.values())
+
+if any(isinstance(ext, TokenPaginationExtension) for ext in extensions):
+    items_get_request_model = create_request_model(
+        model_name="ItemCollectionUri",
+        base_model=ItemCollectionUri,
+        mixins=[TokenPaginationExtension().GET],
+        request_type="GET",
+    )
+else:
+    items_get_request_model = ItemCollectionUri
+
+post_request_model = create_post_request_model(extensions, base_model=PgstacSearch)
+get_request_model = create_get_request_model(extensions)
+
+api = StacApi(
+    settings=settings,
+    extensions=extensions,
+    client=CoreCrudClient(post_request_model=post_request_model),  # type: ignore
+    response_class=ORJSONResponse,
+    items_get_request_model=items_get_request_model,
+    search_get_request_model=get_request_model,
+    search_post_request_model=post_request_model,
+    middlewares=[
+        Middleware(BrotliMiddleware),
+        Middleware(ProxyHeaderMiddleware),
+        Middleware(
+            CORSMiddleware,
+            allow_origins=settings.cors_origins,
+            allow_methods=settings.cors_methods,
+        ),
+    ],
+)
+app = api.app
+
+
+@app.on_event("startup")
+async def startup_event():
+    """Connect to database on startup."""
+    await connect_to_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    """Close database connection."""
+    await close_db_connection(app)
+
+
+def run():
+    """Run app from command line using uvicorn if available."""
+    try:
+        import uvicorn
+
+        uvicorn.run(
+            "stac_fastapi.pgstac.app:app",
+            host=settings.app_host,
+            port=settings.app_port,
+            log_level="info",
+            reload=settings.reload,
+            root_path=os.getenv("UVICORN_ROOT_PATH", ""),
+        )
+    except ImportError as e:
+        raise RuntimeError("Uvicorn must be installed in order to use command") from e
+
+
+if __name__ == "__main__":
+    run()
+
+
+def create_handler(app):
+    """Create a handler to use with AWS Lambda if mangum available."""
+    try:
+        from mangum import Mangum
+
+        return Mangum(app)
+    except ImportError:
+        return None
+
+
+handler = create_handler(app)
