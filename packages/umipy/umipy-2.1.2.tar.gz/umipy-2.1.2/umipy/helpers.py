f6b9e93f99699f1482b54320c6252bac404269b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:40 +0000

    Add package umipy/2.1.2/umipy-2.1.2.tar.gz

---
+++ packages/umipy/umipy-2.1.2.tar.gz/umipy-2.1.2/umipy/helpers.py
@@ -0,0 +1,34 @@
+from umipy import bech32
+from umipy.constants import (
+    BASE_URL_ORIGINAL_MAINNET,
+    BASE_URL_LEGEND_TESTNET,
+    BASE_URL_LEGEND_MAINNET,
+    BASE_URL_ORIGINAL_TESTNET,
+)
+from umipy.enums import Prefix
+
+
+def get_api_url(is_testnet: bool, is_legend: bool) -> str:
+    match (is_legend, is_testnet):
+        case (True, False):
+            return BASE_URL_LEGEND_MAINNET
+        case (True, True):
+            return BASE_URL_LEGEND_TESTNET
+        case (False, True):
+            return BASE_URL_ORIGINAL_TESTNET
+        case (False, False):
+            return BASE_URL_ORIGINAL_MAINNET
+
+
+def get_send_version(is_legend: bool) -> int:
+    if is_legend:
+        return 100
+    else:
+        return 8
+
+
+def get_address_prefix(address: str) -> Prefix | None:
+    prefix, _, _ = bech32.bech32_decode(address=address)
+    if prefix is None:
+        return None
+    return Prefix(prefix)
