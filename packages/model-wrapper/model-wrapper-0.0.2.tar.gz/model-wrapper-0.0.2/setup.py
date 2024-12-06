be632f2c53ab325320c05066480e974b7dbde12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:03 +0000

    Add package model-wrapper/0.0.2/model-wrapper-0.0.2.tar.gz

---
+++ packages/model-wrapper/model-wrapper-0.0.2.tar.gz/model-wrapper-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+
+# INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
+def read(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with open(os.path.join(here, rel_path), 'r', encoding='UTF-8') as fp:
+        return fp.read()
+
+
+long_description = read("README.rst")
+
+
+setup(
+    name='model-wrapper',
+    packages=['model_wrapper', 'model_wrapper.training'],
+    description="Model wrapper for Pytorch, which can training, predict, evaluate, etc.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.0.2',
+    url='https://gitee.com/summry/model-wrapper',
+    author='summy',
+    author_email='xiazhongbiao@126.com',
+    keywords=['Pytorch', 'AI', 'Machine learning', 'Deep learning', 'torch'],
+    package_data={
+        # include json and txt files
+        '': ['*.rst', '*.dtd', '*.tpl'],
+    },
+    include_package_data=True,
+    python_requires='>=3.6',
+    zip_safe=False
+)
+
