b2e0eaf9e173a5645aa62057dc66f4a66446894a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:28 +0000

    Add package pg-httpserver/1.6.7/pg_httpserver-1.6.7-py3-none-any.whl

---
+++ packages/pg-httpserver/pg_httpserver-1.6.7-py3-none-any.whl/pg_httpserver/__init__.py
@@ -0,0 +1,10 @@
+from pg_httpserver.fapi import run, app, CODE_VERSION, get_session_user, get_context, get_game_context, \
+    get_client_ip
+from pg_httpserver.define import ENV_HANDLER_DIR, httpserver_init, \
+    ENV_NEEDS_BODY_MIDDLEWARE, GameException, GameErrorCode, RequestMap, RequestHeader, RequestData, \
+    ResponseMap, ResponseData, ResponseHeader, FieldContainer, GameContext, ExCodeCfg, NoticeCfg, \
+    DefaultResponse
+from pg_httpserver.manager import GameConfigManager, GamePropertyManager, GameExCodeManager, AutoIdManager, \
+    GameNoticeManager, ItemManager, ResourceBase, XdbManager
+from pg_httpserver.models import ReqData
+VERSION = "1.6.7"
