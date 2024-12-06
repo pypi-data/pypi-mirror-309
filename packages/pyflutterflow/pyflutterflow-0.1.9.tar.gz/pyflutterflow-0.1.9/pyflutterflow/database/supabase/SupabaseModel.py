ef3ac66650bf3a0f3c7f21cee5aca6c235083b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:00 +0000

    Add package pyflutterflow/0.1.9/pyflutterflow-0.1.9.tar.gz

---
+++ packages/pyflutterflow/pyflutterflow-0.1.9.tar.gz/pyflutterflow-0.1.9/pyflutterflow/database/supabase/SupabaseModel.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timezone
+from pydantic import Field
+from pyflutterflow.BaseModels import AppBaseModel
+
+
+class SupabaseModel(AppBaseModel):
+    id: int
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    # modified_at_utc: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    class Settings:
+        name = ""
