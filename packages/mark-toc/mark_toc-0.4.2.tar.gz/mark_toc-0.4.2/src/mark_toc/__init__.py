0ad039cc73678d91441c59959b08156d4e44f363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:40 +0000

    Add package mark-toc/0.4.2/mark_toc-0.4.2.tar.gz

---
+++ packages/mark-toc/mark_toc-0.4.2.tar.gz/mark_toc-0.4.2/src/mark_toc/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+__version__ = "0.4.2"
+
+
+def get_version(thing=None):
+    if thing is None:
+        return __version__
+    return "{thing} v{version}".format(thing=thing, version=__version__)
