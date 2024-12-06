eb7ba2027336fcb6a1426abf0377f0ab779d4303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:04 +0000

    Add package licomp-osadl/0.1.4/licomp_osadl-0.1.4-py2.py3-none-any.whl

---
+++ packages/licomp-osadl/licomp_osadl-0.1.4-py2.py3-none-any.whl/licomp_osadl/config.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+licomp_osadl_version = '0.1.4'
+my_supported_api_version = '0.2.0'
+
+cli_name = 'licomp-osadl'
+module_name = 'licomp_osadl'
+
+description = """
+Simple command line tool to check compatibility between two licenses, given context
+"""
+
+epilog = """
+
+AUTHOR
+  Henrik Sandklef
+
+PROJECT SITE
+  https://github.com/hesa/licomp-osadl
+
+REPORTING BUGS
+  File a ticket at https://github.com/hesa/licomp-osadl/issues
+
+COPYRIGHT
+  Copyright (c) 2024 Henrik Sandklef<hesa@sandklef.com>.
+  License GPL-3.0-or-later
+
+ATTRIBUTION
+  lci-osadl is using the license compatibility matrix from osadl.org.
+
+
+"""
