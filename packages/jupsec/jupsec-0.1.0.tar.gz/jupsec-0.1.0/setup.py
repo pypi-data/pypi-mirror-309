497e8dd836ef35593e11be9f45b4c23492e30872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:03:51 +0000

    Add package jupsec/0.1.0/jupsec-0.1.0.tar.gz

---
+++ packages/jupsec/jupsec-0.1.0.tar.gz/jupsec-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupsec',          # Replace with your package name
+    version='0.1.0',            # Replace with your package version
+    author='Hisham Raihan',         # Replace with your name
+    author_email='hishamraihan40@gmail.com',  # Replace with your email
+    description='does nothing',
+    packages=find_packages(),
+    python_requires='>=2.1'     # Specify the minimum Python version
+)
