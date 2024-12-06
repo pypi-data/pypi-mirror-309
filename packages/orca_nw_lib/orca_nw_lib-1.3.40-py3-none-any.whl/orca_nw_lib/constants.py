b5a64829e730801d9ca092253c6a4da0d9ba058f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:18 +0000

    Add package orca_nw_lib/1.3.40/orca_nw_lib-1.3.40-py3-none-any.whl

---
+++ packages/orca_nw_lib/orca_nw_lib-1.3.40-py3-none-any.whl/orca_nw_lib/constants.py
@@ -0,0 +1,18 @@
+##orca.yml
+discover_networks='discover_networks'
+device_gnmi_port='device_gnmi_port'
+device_username='device_username'
+device_password='device_password'
+device_conn_timeout='device_conn_timeout'
+request_timeout='request_timeout'
+ping_timeout='ping_timeout'
+
+#neo4j
+neo4j_protocol='neo4j_protocol'
+neo4j_url='neo4j_url'
+neo4j_user='neo4j_user'
+neo4j_password='neo4j_password'
+
+#env_var
+env_default_orca_nw_lib_config_file="ORCA_NW_LIB_CONFIG_FILE"
+env_default_logging_config_file = "ORCA_NW_LIB_LOGGING_CONFIG_FILE"
