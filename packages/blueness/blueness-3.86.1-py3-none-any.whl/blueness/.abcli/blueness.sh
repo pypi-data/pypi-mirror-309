e05f5f0bd694c4c77f3b03489429d20343f882f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:55 +0000

    Add package blueness/3.86.1/blueness-3.86.1-py3-none-any.whl

---
+++ packages/blueness/blueness-3.86.1-py3-none-any.whl/blueness/.abcli/blueness.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+function blueness() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    if [[ "$task" == "help" ]]; then
+        blueness upgrade "$@"
+        return
+    fi
+
+    if [ $task == "upgrade" ]; then
+        if [[ "$2" == "help" ]]; then
+            abcli_show_usage "blueness upgrade" \
+                "upgrade blueness and its modules 🌀."
+            return
+        fi
+
+        local module
+        for module in blueness \
+            blue_options \
+            blue_objects \
+            blue_geo \
+            blueflow \
+            abcli; do
+            pip install --upgrade $module
+        done
+        return
+    fi
+
+    abcli_generic_task \
+        plugin=blueness,task=$task \
+        "${@:2}"
+}
