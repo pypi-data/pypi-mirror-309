8d87aae2dae08314793c1619324f4b9912f89c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:09 +0000

    Add package codex/1.7.9/codex-1.7.9.tar.gz

---
+++ packages/codex/codex-1.7.9.tar.gz/codex-1.7.9/codex/static_root/assets/ssrBoot-D0SN4REy.6200754b59d8.js
@@ -0,0 +1,1 @@
+import{aR as t,bi as e,c as s,c4 as n}from"./main-B0XsokNO.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
