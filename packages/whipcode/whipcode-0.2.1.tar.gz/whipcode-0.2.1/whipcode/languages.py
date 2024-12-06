c6fe75eb7f653ea316d3523ad023795ddcdd3e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:12 +0000

    Add package whipcode/0.2.1/whipcode-0.2.1.tar.gz

---
+++ packages/whipcode/whipcode-0.2.1.tar.gz/whipcode-0.2.1/whipcode/languages.py
@@ -0,0 +1,32 @@
+"""Constants for languages"""
+
+
+class Langs:
+    PYTHON     = 1
+    JAVASCRIPT = 2
+    BASH       = 3
+    PERL       = 4
+    LUA        = 5
+    RUBY       = 6
+    C          = 7
+    CPP        = 8
+    RUST       = 9
+    FORTRAN    = 10
+    HASKELL    = 11
+    JAVA       = 12
+    GO         = 13
+    TYPESCRIPT = 14
+    CLISP      = 15
+    RACKET     = 16
+    CRYSTAL    = 17
+    CLOJURE    = 18
+    X86        = 19
+    ZIG        = 20
+    NIM        = 21
+    DLANG      = 22
+    CSHARP     = 23
+    RSCRIPT    = 24
+    DART       = 25
+    VB         = 26
+    FSHARP     = 27
+    PHP        = 28
