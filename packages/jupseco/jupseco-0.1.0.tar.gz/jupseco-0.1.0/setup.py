c2c1f7f793bacdc306d6bdef62f3e143790a75e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:03 +0000

    Add package jupseco/0.1.0/jupseco-0.1.0.tar.gz

---
+++ packages/jupseco/jupseco-0.1.0.tar.gz/jupseco-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupseco',                # Your package name
+    version='0.1.0',               # Initial version
+    author='Hisham',            # Your name
+    author_email='hishamraihann40@gmail.com',  # Your email
+    description='A package with useful functions like nothing',  # Short description
+    packages=find_packages(),      # Automatically finds the `jupysec` package
+    python_requires='>=2.1',       # Minimum Python version
+)
