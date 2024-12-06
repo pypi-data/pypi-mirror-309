cf5acdc96badb13f0c0a579b8e0b3f6842652640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:09 +0000

    Add package PyMyMethod/1.0.5/pymymethod-1.0.5.tar.gz

---
+++ packages/PyMyMethod/pymymethod-1.0.5.tar.gz/pymymethod-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyMyMethod',
+    version='1.0.5',
+    author='Mikuas',
+    packages=find_packages(),
+    install_requires=[
+        'PySide6',
+        'pyautogui',
+        'easyocr',
+        'comtypes',
+        'pycaw'
+    ]
+)
