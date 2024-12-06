1ce7f8830a445958bbd3e73a233fc9846513f39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:07 +0000

    Add package scAGDE/0.0.15/scagde-0.0.15.tar.gz

---
+++ packages/scAGDE/scagde-0.0.15.tar.gz/scagde-0.0.15/scAGDE/__init__.py
@@ -0,0 +1,14 @@
+"""
+@Project ：code 
+@File    ：__init__.py.py
+@IDE     ：PyCharm 
+@Author  ：Hao Gaoyang
+@Date    ：2024/6/12 11:36
+@Content ：
+"""
+import scAGDE.trainer
+import scAGDE.utils
+
+Trainer = scAGDE.trainer.Trainer
+Trainer_scale = scAGDE.trainer.Trainer_scale
+utils = scAGDE.utils
