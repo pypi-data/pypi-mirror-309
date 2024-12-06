248dee51a019606137852378eed47f44613142c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:30:56 +0000

    Add package nonebot-plugin-ba-tools/0.3.5/nonebot_plugin_ba_tools-0.3.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-ba-tools/nonebot_plugin_ba_tools-0.3.5-py3-none-any.whl/nonebot_plugin_ba_tools/get_ba_clairvoyance/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import Any
+
+from nonebot import require
+from nonebot.adapters.onebot.v11 import Bot
+
+from ..config import plugin_config
+from ..utils.get_img_from_name import get_img
+
+require("nonebot_plugin_alconna")
+from nonebot_plugin_alconna import (  # noqa: E402
+    Alconna,
+    AlconnaMatcher,
+    Args,
+    Image,
+    Match,
+    UniMessage,
+    on_alconna,
+)
+
+# TODO:命令别名
+clairvoyance: Alconna[Any] = Alconna("ba千里眼", Args["server", str])
+get_clairvoyance: type[AlconnaMatcher] = on_alconna(clairvoyance, use_cmd_start=True)
+
+
+@get_clairvoyance.assign("server")
+async def _(bot: Bot, server: Match[str]) -> None:
+    if server.available:
+        pre_msg: dict[str, int] = {"message_id": -1}
+        msg: UniMessage[Image] | None = await get_img(
+            f"{server.result}未来视", "千里眼"
+        )
+        if msg:
+            if plugin_config.loading_switch:
+                pre_msg = await get_clairvoyance.send("拼命加载图片中……")
+            await get_clairvoyance.send(msg)
+            if plugin_config.loading_switch:
+                await bot.delete_msg(message_id=pre_msg["message_id"])
+            await get_clairvoyance.finish()
+        else:
+            await get_clairvoyance.finish("不支持的服务器哦～")
