347d7ead71ccf72c822ef25b6adecff6c4a668b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:42 +0000

    Add package tyro/0.9.1/tyro-0.9.1.tar.gz

---
+++ packages/tyro/tyro-0.9.1.tar.gz/tyro-0.9.1/tests/conftest.py
@@ -0,0 +1,23 @@
+import sys
+from typing import List
+
+collect_ignore_glob: List[str] = []
+
+if not sys.version_info >= (3, 8):
+    collect_ignore_glob.append("*min_py38*.py")
+
+if not sys.version_info >= (3, 9):
+    collect_ignore_glob.append("*min_py39*.py")
+
+if not sys.version_info >= (3, 10):
+    collect_ignore_glob.append("*min_py310*.py")
+    collect_ignore_glob.append("*_min_py310_generated.py")
+
+if not sys.version_info >= (3, 12):
+    collect_ignore_glob.append("*min_py312*.py")
+
+if not sys.version_info >= (3, 11):
+    collect_ignore_glob.append("test_py311_generated/*.py")
+
+if sys.version_info >= (3, 13):
+    collect_ignore_glob.append("*_exclude_py313*.py")
