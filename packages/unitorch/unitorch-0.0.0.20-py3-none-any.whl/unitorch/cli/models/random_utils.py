4e7e6c6529a91402028c51c2bc5141ee5a936f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:58 +0000

    Add package unitorch/0.0.0.20/unitorch-0.0.0.20-py3-none-any.whl

---
+++ packages/unitorch/unitorch-0.0.0.20-py3-none-any.whl/unitorch/cli/models/random_utils.py
@@ -0,0 +1,49 @@
+# Copyright (c) FULIUCANSHENG.
+# Licensed under the MIT License.
+
+import os
+import io
+import requests
+import time
+import base64
+import json
+import logging
+import torch
+import torch.nn as nn
+import numpy as np
+from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union
+from random import random
+
+from unitorch.cli import (
+    add_default_section_for_init,
+    add_default_section_for_function,
+    register_process,
+)
+
+
+class RandomProcessor:
+    def __init__(
+        self,
+    ):
+        pass
+
+    @classmethod
+    @add_default_section_for_init("core/process/random")
+    def from_core_configure(cls, config, **kwargs):
+        pass
+
+    @register_process("core/process/random/float")
+    def _float(
+        self,
+        low: float = 0.0,
+        high: float = 1.0,
+    ):
+        return random() * (high - low) + low
+
+    @register_process("core/process/random/int")
+    def _int(
+        self,
+        low: int = 0,
+        high: int = 100,
+    ):
+        return int(random() * (high - low) + low)
