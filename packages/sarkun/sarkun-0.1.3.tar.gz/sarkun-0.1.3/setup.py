f4fd4b3c26fcb60592f4c8c129d1e2ff42aa24a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:30 +0000

    Add package sarkun/0.1.3/sarkun-0.1.3.tar.gz

---
+++ packages/sarkun/sarkun-0.1.3.tar.gz/sarkun-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="sarkun",  # Package name
+    version="0.1.3",          # Version number
+    packages=find_packages(),  # Automatically find packages
+    include_package_data=True,  # Include non-Python files like README.md, LICENSE, etc.
+    install_requires=[       # Dependencies (if any)
+        'pyfiglet',
+        'colorama',
+        'emoji',
+    ],
+    entry_points={          # Define command-line entry points
+        'console_scripts': [
+            'sarkun = sarkun.sarkun:main',  # This maps the command "community-tool" to the main() function
+        ],
+    },
+    classifiers=[  # Optional, metadata about your package
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    long_description=open('README.md').read(),  # Read the README file for the long description
+    long_description_content_type='text/markdown',  # Markdown format
+    url='https://github.com/kaankarakoc42/sarkun',  # URL to your project
+    author='Mevlüt Kaan Karakoç',  # Author name
+    author_email='karakockaan326@gmail.com',  # Author email
+    license='MIT',  # License type
+)
