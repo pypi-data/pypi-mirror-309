0416076a045051424a95f1054822890b300abbea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:41 +0000

    Add package proste-ai2/0.1.3/proste_ai2-0.1.3.tar.gz

---
+++ packages/proste-ai2/proste_ai2-0.1.3.tar.gz/proste_ai2-0.1.3/README.md
@@ -0,0 +1,12 @@
+# Proste AI
+
+**Proste AI** to prosty pakiet AI w Pythonie, który umożliwia zarządzanie danymi, tworzenie modeli oraz generowanie odpowiedzi na podstawie wytrenowanego modelu. Pakiet obsługuje wczytywanie danych z plików CSV, trenowanie modeli i ich zapis oraz późniejsze wykorzystanie.
+
+---
+
+## Instalacja
+
+Najpierw zainstaluj pakiet za pomocą `pip`:
+
+```bash
+pip install proste-ai
---
+++ packages/proste-ai2/proste_ai2-0.1.3.tar.gz/proste_ai2-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="proste_ai2",                    # Nazwa pakietu
+    version="0.1.3",                     # Wersja
+    description="Prosty pakiet AI do nauki",  # Krótki opis
+    long_description=open("README.md").read(),  # Długi opis (z README.md)
+    long_description_content_type="text/markdown",  # Typ długiego opisu
+    author="Situus",                 # Twoje imię lub pseudonim
+    author_email="situs1235@gmail.com",  # Twój email
+    license="MIT",                       # Licencja projektu
+    packages=find_packages(),            # Automatyczne wykrywanie modułów
+    install_requires=[                   # Wymagane zależności
+        "scikit-learn>=1.0",
+    ],
+    python_requires=">=3.7",             # Minimalna wersja Pythona
+    classifiers=[                        # Klasyfikatory
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
