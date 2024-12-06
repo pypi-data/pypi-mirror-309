b4bd5cdb4201600a98f41ad5f212718d3f134cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:22 +0000

    Add package nuclia/4.3.2/nuclia-4.3.2.tar.gz

---
+++ packages/nuclia/nuclia-4.3.2.tar.gz/nuclia-4.3.2/nuclia/lib/utils.py
@@ -0,0 +1,64 @@
+import json
+from typing import Union
+
+import httpx
+import requests
+from tabulate import tabulate
+
+from nuclia.exceptions import RateLimitError, UserTokenExpired, DuplicateError
+from nucliadb_models.resource import ResourceList
+from nucliadb_models.search import SyncAskResponse
+from nuclia.lib.models import ActivityLogsOutput
+
+
+def handle_http_errors(response: Union[httpx.Response, requests.models.Response]):
+    if (
+        response.status_code == 403
+        and "Hydra token is either unexistent or revoked" in response.text
+    ):
+        raise UserTokenExpired()
+    elif response.status_code == 429:
+        raise RateLimitError(f"Rate limited: {response.text}")
+    elif response.status_code == 409:
+        raise DuplicateError(f"Duplicate resource: {response.text}")
+    elif response.status_code >= 400:
+        raise httpx.HTTPError(f"Status code {response.status_code}: {response.text}")
+
+
+def serialize(obj):
+    if isinstance(obj, ResourceList):
+        data = []
+        for resource in obj.resources:
+            status = resource.metadata.status if resource.metadata is not None else ""
+            data.append(
+                [resource.id, resource.icon, resource.title, status, resource.slug]
+            )
+        return tabulate(
+            data,
+            headers=["UUID", "Icon", "Title", "Status", "Slug"],
+        )
+    if isinstance(obj, SyncAskResponse):
+        if obj.status != "success":
+            response = f"ERROR: {obj.error_details}"
+        elif obj.answer_json is not None:
+            response = f"JSON: {json.dumps(obj.answer_json, indent=2)} \n"
+        else:
+            response = f"Answer: {obj.answer} \n"
+
+        if obj.metadata:
+            response += f"Metadata: {obj.metadata.model_dump_json(indent=2)}\n"
+
+        response += "Resources: \n"
+        data = []
+        for resource in obj.retrieval_results.resources.values():
+            data.append([resource.id, resource.icon, resource.title])
+        response += tabulate(
+            data,
+            headers=["UUID", "Icon", "Title"],
+        )
+        return response
+
+    if isinstance(obj, ActivityLogsOutput):
+        obj = obj.model_dump(exclude_unset=True)
+
+    return obj
