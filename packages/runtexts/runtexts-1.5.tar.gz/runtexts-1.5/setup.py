2e18a2727a99767fe7f42951cf0cccb4b0860b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:00 +0000

    Add package runtexts/1.5/runtexts-1.5.tar.gz

---
+++ packages/runtexts/runtexts-1.5.tar.gz/runtexts-1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="runtexts",
+    version="1.5",
+    packages=find_packages(),
+    install_requires=[
+        "pyautogui",
+        "colorama",
+        "requests",
+        "keyboard",
+        "tkinter",  
+    ],
+    entry_points={
+        "console_scripts": [
+            "runtexts=runtexts.cli:main", 
+            "runtexts-gui=runtexts.gui:main",  
+        ],
+    },
+    author="Maruf Ovi",
+    author_email="fornet.ovi@gmail.com",
+    description="A CLI and GUI tool to send automated messages.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamovi/project_runtexts",
+    license="MIT",
+    keywords="automation, messaging, CLI, pyautogui, tkinter, GUI",
+    python_requires=">=3.6",
+)
