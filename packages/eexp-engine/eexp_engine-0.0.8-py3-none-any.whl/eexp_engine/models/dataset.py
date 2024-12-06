72bb9b0d5cf5c5975df372b572b62b01ca717dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:13 +0000

    Add package eexp-engine/0.0.8/eexp_engine-0.0.8-py3-none-any.whl

---
+++ packages/eexp-engine/eexp_engine-0.0.8-py3-none-any.whl/eexp_engine/models/dataset.py
@@ -0,0 +1,22 @@
+class Dataset:
+
+    def __init__(self, name):
+        self.name = name
+        self.name_in_task_signature = None
+        self.name_in_generating_task = None
+        self.path = ""
+
+    def add_path(self, path):
+        self.path = path
+
+    def set_name_in_task_signature(self, name_in_task_signature):
+        self.name_in_task_signature = name_in_task_signature
+
+    def set_name_in_generating_task(self, name_in_generating_task):
+        self.name_in_generating_task = name_in_generating_task
+
+    def print(self, tab=""):
+        print(f"{tab}\twith dataset name : {self.name}")
+        print(f"{tab}\twith dataset name in task signature : {self.name_in_task_signature}")
+        print(f"{tab}\twith dataset name in generating task : {self.name_in_generating_task}")
+        print(f"{tab}\twith dataset path : {self.path}")
