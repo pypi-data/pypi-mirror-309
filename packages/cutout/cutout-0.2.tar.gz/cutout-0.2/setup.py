45b3adee8ac668757698fe96639f290f5a5ee738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:32 +0000

    Add package cutout/0.2/cutout-0.2.tar.gz

---
+++ packages/cutout/cutout-0.2.tar.gz/cutout-0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+	name = "cutout",
+	version = "0.2",
+	author = "Ava Polzin",
+	author_email = "apolzin@uchicago.edu",
+	description = "Survey cutouts plotted directly; no user choices, just plotting!",
+	packages = ["cutout", "cutout/survey", "cutout/tools"],
+	url = "https://github.com/avapolzin/cutout",
+	license = 'MIT',
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Topic :: Scientific/Engineering :: Astronomy",
+		"Topic :: Scientific/Engineering :: Physics"],
+	python_requires = ">=3",
+	install_requires = ["astropy", "matplotlib", "numpy", 
+		"unagi"]
+)
