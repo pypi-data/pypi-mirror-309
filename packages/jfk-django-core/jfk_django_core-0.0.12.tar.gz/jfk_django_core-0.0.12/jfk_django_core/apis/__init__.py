ff08cb7050b9eaf694d0199e782be8470903c6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:28 +0000

    Add package jfk-django-core/0.0.12/jfk_django_core-0.0.12.tar.gz

---
+++ packages/jfk-django-core/jfk_django_core-0.0.12.tar.gz/jfk_django_core-0.0.12/jfk_django_core/apis/__init__.py
@@ -0,0 +1,1 @@
+from .jfk_authentication import *
---
+++ packages/jfk-django-core/jfk_django_core-0.0.12.tar.gz/jfk_django_core-0.0.12/jfk_django_core/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class JfkDjangoCoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jfk_django_core'
+    verbose_name = 'JFK Django Core'
