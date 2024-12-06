5b55aa10c78a7a48f4f765d16b7491dd6ce257a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:56 +0000

    Add package load-data-from-postgresql-rs/0.1.2/load_data_from_postgresql_rs-0.1.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/load-data-from-postgresql-rs/load_data_from_postgresql_rs-0.1.2-cp311-cp311-macosx_11_0_arm64.whl/load_data_from_postgresql_rs/__init__.py
@@ -0,0 +1,5 @@
+from .load_data_from_postgresql_rs import *
+
+__doc__ = load_data_from_postgresql_rs.__doc__
+if hasattr(load_data_from_postgresql_rs, "__all__"):
+    __all__ = load_data_from_postgresql_rs.__all__
