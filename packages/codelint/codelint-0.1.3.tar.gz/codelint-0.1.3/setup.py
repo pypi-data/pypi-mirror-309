e7d6b858215c5057c91588c7e83c15e980b05c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:15 +0000

    Add package codelint/0.1.3/codelint-0.1.3.tar.gz

---
+++ packages/codelint/codelint-0.1.3.tar.gz/codelint-0.1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codelint",
+    version="0.1.3",
+    packages=find_packages(),
+     install_requires=[
+        "colorama>=0.4.6",
+    ],
+    entry_points={
+        "console_scripts": [
+            "codelint=codelint.checker:main",
+        ],
+    },
+    author="Aditya Gupta",
+    author_email="aditya98gupta@gmail.com",
+    description="A custom PEP 8 checker module",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aditya-1998k/CodeLint",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    extras_require={
+        "dev": [
+            "pytest",
+        ],
+    },
+)
