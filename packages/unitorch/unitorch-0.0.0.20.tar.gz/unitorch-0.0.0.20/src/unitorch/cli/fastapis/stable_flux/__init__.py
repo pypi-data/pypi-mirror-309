e3f8a0791dc7a36d37215c2c296c1ee1a9f87172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:58 +0000

    Add package unitorch/0.0.0.20/unitorch-0.0.0.20.tar.gz

---
+++ packages/unitorch/unitorch-0.0.0.20.tar.gz/unitorch-0.0.0.20/src/unitorch/cli/fastapis/stable_flux/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) FULIUCANSHENG.
+# Licensed under the MIT License.
+
+from unitorch.cli.fastapis.stable_flux.text2image import (
+    StableFluxForText2ImageFastAPIPipeline,
+)
+from unitorch.cli.fastapis.stable_flux.image2image import (
+    StableFluxForImage2ImageFastAPIPipeline,
+)
+from unitorch.cli.fastapis.stable_flux.inpainting import (
+    StableFluxForImageInpaintingFastAPIPipeline,
+)
