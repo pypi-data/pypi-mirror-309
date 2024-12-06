e02946d657419bd940deec037dc451749de44f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:53 +0000

    Add package wristband/0.2.3/wristband-0.2.3.tar.gz

---
+++ packages/wristband/wristband-0.2.3.tar.gz/wristband-0.2.3/wristband/users/create_input_users_csv.py
@@ -0,0 +1,9 @@
+from .users_utils import UsersService
+
+def main():
+    # Initialize service without credentials
+    service = UsersService()
+    service.create_input_users_csv()
+
+if __name__ == "__main__":
+    main()
