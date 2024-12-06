058c3d99e78e34bbe582c03b25dff8193c540301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:17 +0000

    Add package comtraderecord/1.0.1/comtraderecord-1.0.1-py3-none-any.whl

---
+++ packages/comtraderecord/comtraderecord-1.0.1-py3-none-any.whl/comtraderecord/__init__.py
@@ -0,0 +1,12 @@
+"""
+Comtrade file
+"""
+
+from .pyComtrade import *
+from .pyRelayAlg import *
+from .comtradeWriter import *
+
+# import metadata not covered by *:
+from ._version import version as __version__
+
+
