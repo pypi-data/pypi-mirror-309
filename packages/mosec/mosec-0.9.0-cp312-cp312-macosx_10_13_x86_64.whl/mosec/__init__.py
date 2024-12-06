10e6807c19b8f58776db7b3388c47a254357a60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:06:01 +0000

    Add package mosec/0.9.0/mosec-0.9.0-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/mosec/mosec-0.9.0-cp312-cp312-macosx_10_13_x86_64.whl/mosec/__init__.py
@@ -0,0 +1,47 @@
+# Copyright 2022 MOSEC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""MOSEC is a machine learning model serving framework."""
+
+from mosec.errors import (
+    ClientError,
+    DecodingError,
+    EncodingError,
+    ServerError,
+    ValidationError,
+)
+from mosec.log import get_logger
+from mosec.runtime import Runtime
+from mosec.server import Server
+from mosec.worker import SSEWorker, Worker
+
+try:
+    from mosec._version import __version__  # type: ignore
+except ImportError:
+    from setuptools_scm import get_version  # type: ignore
+
+    __version__ = get_version(root="..", relative_to=__file__)
+
+__all__ = [
+    "Server",
+    "Worker",
+    "SSEWorker",
+    "Runtime",
+    "ServerError",
+    "ClientError",
+    "ValidationError",
+    "EncodingError",
+    "DecodingError",
+    "get_logger",
+]
