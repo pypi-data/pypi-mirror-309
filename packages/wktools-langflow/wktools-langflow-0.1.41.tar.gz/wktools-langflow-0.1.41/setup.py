c5c667fd9b0d7ccb224aee50e95e003c896398f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:44 +0000

    Add package wktools-langflow/0.1.41/wktools-langflow-0.1.41.tar.gz

---
+++ packages/wktools-langflow/wktools-langflow-0.1.41.tar.gz/wktools-langflow-0.1.41/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="wktools-langflow",
+    version="0.1.41",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],  # Add any dependencies here
+    description="A tools for langflow",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/kokhou/jwtools",
+    author="kokhou",
+    author_email="kokhou.choi@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
