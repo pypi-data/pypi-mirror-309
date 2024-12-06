32fe3af3598be00e5547ee33ca0d7cf152a4a339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:03:58 +0000

    Add package EDAeasy/1.2.9/edaeasy-1.2.9-py3-none-any.whl

---
+++ packages/EDAeasy/edaeasy-1.2.9-py3-none-any.whl/edaeasy/__init__.py
@@ -0,0 +1,13 @@
+# Version
+__version__ = "1.0.1"
+
+# Importing
+from .functions import (
+    resumen_datos,
+    tabulacion_frecuencias_proporciones,
+    resumen_frecuencias_proporciones,
+    calculo_prevalencia,
+    generar_documento,
+    paired_descriptive_analysis,
+)
+from .preprocessing import convertir_tipos_columnas
