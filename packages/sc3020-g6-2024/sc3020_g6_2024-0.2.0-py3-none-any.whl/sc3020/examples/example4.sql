c5f9f1af8269fa132d7da15010a4bbd181d00655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:27 +0000

    Add package sc3020-g6-2024/0.2.0/sc3020_g6_2024-0.2.0-py3-none-any.whl

---
+++ packages/sc3020-g6-2024/sc3020_g6_2024-0.2.0-py3-none-any.whl/sc3020/examples/example4.sql
@@ -0,0 +1,24 @@
+-- Code copied from: https://github.com/2ndQuadrant/pg-tpch/blob/master/queries/q04.sql
+
+
+select
+	o_orderpriority,
+	count(*) as order_count
+from
+	orders
+where
+	o_orderdate >= date '1997-07-01'
+	and o_orderdate < date '1997-07-01' + interval '3' month
+	and exists (
+		select
+			*
+		from
+			lineitem
+		where
+			l_orderkey = o_orderkey
+			and l_commitdate < l_receiptdate
+	)
+group by
+	o_orderpriority
+order by
+	o_orderpriority;
