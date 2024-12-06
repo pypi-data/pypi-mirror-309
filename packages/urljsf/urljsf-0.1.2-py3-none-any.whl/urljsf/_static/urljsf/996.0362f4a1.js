41b1358cfd02c19b11b640befa2d05d2bc599f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:54 +0000

    Add package urljsf/0.1.2/urljsf-0.1.2-py3-none-any.whl

---
+++ packages/urljsf/urljsf-0.1.2-py3-none-any.whl/urljsf/_static/urljsf/996.0362f4a1.js
@@ -0,0 +1,2 @@
+/*! For license information please see 996.0362f4a1.js.LICENSE.txt */
+export const id=996;export const ids=[996];export const modules={KPOv:(o,s,t)=>{o.exports=t.p+"bootstrap.min.css?v=b4944622"}};
