36b39942a772203630008846f229deb5ac0a130c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:25 +0000

    Add package MyBpm/0.2.3/mybpm-0.2.3.tar.gz

---
+++ packages/MyBpm/mybpm-0.2.3.tar.gz/mybpm-0.2.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# DevVersion: Python3.6.8
+# Date: 2020-09-25 09:13
+# PyCharm|setup
+
+from setuptools import (setup, find_packages)
+
+setup(
+    # 包名
+    name="MyBpm",
+    # 版本
+    version="0.2.3",
+    # 包的解释地址
+    # long_description=open('ReadMe.md', encoding='utf-8').read(),
+    # 需要包含的子包列表
+    packages=find_packages()
+)
