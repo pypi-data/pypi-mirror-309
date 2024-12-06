757a03b1d0fefce0e477062d7dfd7ad4e8494252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:31 +0000

    Add package zndraw/0.5.4a0/zndraw-0.5.4a0.tar.gz

---
+++ packages/zndraw/zndraw-0.5.4a0.tar.gz/zndraw-0.5.4a0/zndraw/__init__.py
@@ -0,0 +1,46 @@
+from zndraw.base import Extension
+from zndraw.draw import (
+    Box,
+    Circle,
+    Cone,
+    Custom2DShape,
+    Cylinder,
+    Dodecahedron,
+    Ellipsoid,
+    Icosahedron,
+    Material,
+    Octahedron,
+    Plane,
+    Rhomboid,
+    Ring,
+    Sphere,
+    Tetrahedron,
+    Torus,
+    TorusKnot,
+)
+from zndraw.figure import Figure
+from zndraw.zndraw import ZnDraw, ZnDrawLocal
+
+__all__ = [
+    "ZnDraw",
+    "ZnDrawLocal",
+    "Extension",
+    "Plane",
+    "Sphere",
+    "Box",
+    "Circle",
+    "Cone",
+    "Cylinder",
+    "Dodecahedron",
+    "Icosahedron",
+    "Octahedron",
+    "Ring",
+    "Tetrahedron",
+    "Torus",
+    "TorusKnot",
+    "Rhomboid",
+    "Ellipsoid",
+    "Material",
+    "Custom2DShape",
+    "Figure",
+]
