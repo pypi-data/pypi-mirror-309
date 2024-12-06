416fca4756355eb045179ee2c74e5ede296c7c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:02 +0000

    Add package metafield/0.2.6/metafield-0.2.6-py3-none-any.whl

---
+++ packages/metafield/metafield-0.2.6-py3-none-any.whl/metafield/__init__.py
@@ -0,0 +1,4 @@
+from .nb_utils import parse_hash, find_tagged_cell, find_section
+from .git_utils import find_friends
+from .llm import remindme
+from .nb_depends import MetaGraph
---
+++ packages/metafield/metafield-0.2.6-py3-none-any.whl/metafield/app.py
@@ -0,0 +1,4 @@
+from .llm import remindme
+
+def main():
+    remindme()    
