69476dd6b0cb42ee77d11722259140c3b5e1e0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:28 +0000

    Add package abstract-solcatcher/0.0.3.45/abstract_solcatcher-0.0.3.45-py3-none-any.whl

---
+++ packages/abstract-solcatcher/abstract_solcatcher-0.0.3.45-py3-none-any.whl/abstract_solcatcher/other/flaskCalls.py
@@ -0,0 +1,78 @@
+from abstract_utilities import *
+from abstract_solcatcher.utils import get_async_response,getEndpointUrl,get_headers
+from abstract_solcatcher.async_utils import asyncPostRequest
+import json,requests,base58
+from abstract_solcatcher.abstract_rate_limit import makeLimitedCall
+from solana.rpc.types import TxOpts
+from solana.transaction import Transaction
+async def get_solcatcher_endpoint(endpoint,*args,**kwargs):
+    return await asyncPostRequest(url=getEndpointUrl(endpoint),data=kwargs,headers=get_headers())
+
+# Helper function to get Solcatcher API responses
+def get_solcatcher_api(endpoint, *args, **kwargs):
+    return get_async_response(get_solcatcher_endpoint, endpoint=endpoint, *args, **kwargs)
+  
+def getTxnTypeFromMint(address):
+    return get_async_response(get_solcatcher_endpoint,endpoint="getTxnTypeFromMint",address=address)
+
+def sendTransaction(txn, payer_keypair , opts=None, skip_preflight=True):
+  return get_async_response(get_solcatcher_endpoint,endpoint="sendTransaction",txn=txn, payer_keypair=payer_keypair, opts=opts)
+
+def getGenesisSignature(address,before=None,limit=None):
+  return get_async_response(get_solcatcher_endpoint,endpoint="getGenesisSignature",address=address,before=before,limit=limit)
+
+def getSignaturesForAddress(address, limit=10, before=None, after=None, finalized=None,encoding=None,commitment=None,errorProof=False):
+  return get_async_response(get_solcatcher_endpoint,endpoint="getSignaturesForAddress",address=address, limit=limit, before=before, after=after, finalized=finalized,encoding=encoding,commitment=commitment,errorProof=errorProof)
+
+def getTokenAccountBalance(account,mint=None,commitment=None):
+  return get_async_response(get_solcatcher_endpoint,endpoint="getTokenAccountBalance",account=account,mint=mint,commitment=commitment)
+
+def getTokenAccountsByOwner(account,mint=None,encoding=None):
+  return get_async_response(get_solcatcher_endpoint,endpoint="getTokenAccountsByOwner",account=account,mint=mint,encoding=encoding)
+
+def getMetaData(mint):
+  return get_async_response(get_solcatcher_endpoint,endpoint="getMetaData",mint=mint)
+
+def getTransaction(signature=None,commitment=None,maxSupportedTransactionVersion=None):
+  return get_async_response(get_solcatcher_endpoint,endpoint="getTransaction",signature=signature,commitment=commitment,maxSupportedTransactionVersion=maxSupportedTransactionVersion)
+
+def getLatestBlockHash(commitment=None):
+    return get_async_response(get_solcatcher_endpoint,endpoint="getLatestBlockhash",commitment=commitment)
+
+def getAccountInfo(account,encoding=None,commitment=None):
+  return get_async_response(client.get_account_info,endpoint="getAccountInfo",account=account,encoding=encoding,commitment=commitment)
+
+def getBlock(slot,encoding=None,maxSupportedTransactionVersion=None,transactionDetails= None,rewards=None):
+    return get_async_response(client.get_block,slot=slot,encoding=encoding,maxSupportedTransactionVersion=maxSupportedTransactionVersion,transactionDetails=transactionDetails,rewards=rewards)
+
+def getParsedTransaction(signature=None,txnData=None,programId=None,encoding=None,commitment=None,maxSupportedTransactionVersion=None):
+    return get_async_response(get_solcatcher_endpoint,endpoint="getParsedTransaction",signature=signature,txnData=txnData,programId=programId,encoding=encoding,commitment=commitment,maxSupportedTransactionVersion=maxSupportedTransactionVersion)
+
+def quickBuyData(mint=None):
+    return get_async_response(get_solcatcher_endpoint,endpoint="dbSearch",tableName="quickbuydata",searchValue=mint)
+
+def getTxLog(signature=None):
+    return get_async_response(get_solcatcher_endpoint,endpoint="getTxLog",signature=signature)
+
+def insertTxLog(txLog,signature=None):
+    return get_async_response(get_solcatcher_endpoint,endpoint="insertTxLog",txLogs=txnLog,signature=signature)
+
+async def bulkInsertTxnLogs_async(txnLogs):
+    for txLog in txLogs:
+        await asyncPostRequest(url=getEndpointUrl("insertTxLog"),data={"txLog":txLog,"signature":get_any_value(txLog,"signature")},headers=get_headers())
+
+def bulkInsertTxnLogs(txLogs):
+    return get_async_response(bulkInsertTxnLogs_async,txLogs=txnLogs,signature=signature)
+
+def sendTransaction(txn: Transaction, payer_keypair, opts=None,skip_preflight=None) -> dict:
+    skip_preflight = skip_preflight or True
+    opts = opts or TxOpts(skip_preflight=skip_preflight)
+    # Sign the transaction
+    txn.sign(payer_keypair)
+    method="sendTransaction"
+    # Serialize the transaction to a base64 string
+    txn_base64 = base58.b58encode(txn.serialize()).decode('utf-8')
+    params = [txn_base64, {"skipPreflight": opts.skip_preflight, "preflightCommitment": "finalized"}]
+    # Prepare the RPC request payload
+    # Send the transaction
+    return makeLimitedCall(method,params)
---
+++ packages/abstract-solcatcher/abstract_solcatcher-0.0.3.45-py3-none-any.whl/abstract_solcatcher/other/solana_rpc_calls.py
@@ -0,0 +1,139 @@
+from ..abstract_rate_limit import makeLimitedCall,makeLimitedDbCall
+from .utils import makeParams, get_async_response,combineParamDicts
+from solana.rpc.types import TokenAccountOpts,TxOpts
+from solana.transaction import Transaction
+import base58
+
+async def async_getBlock(slot,encoding=None,maxSupportedTransactionVersion=None,transactionDetails=None,rewards=None):
+    encoding = encoding or "jsonParsed"
+    maxSupportedTransactionVersion = maxSupportedTransactionVersion or 0  
+    transactionDetails = transactionDetails or "full"
+    rewards = rewards or False
+    method = "getBlock"
+    params = await makeParams(int(slot), encoding=encoding,maxSupportedTransactionVersion=maxSupportedTransactionVersion,transactionDetails=transactionDetails,rewards=rewards)
+    return await makeLimitedDbCall(method,  await combineParamDicts(params))
+
+def getBlock(slot, encoding=None,maxSupportedTransactionVersion=None,transactionDetails=None,rewards=None):
+    return get_async_response(async_getBlock, slot, encoding=encoding,maxSupportedTransactionVersion=maxSupportedTransactionVersion,transactionDetails=transactionDetails,rewards=rewards)
+
+async def async_getLatestBlockHash(commitment=None):
+    commitment = commitment or "processed"
+    method = "getLatestBlockhash"
+    params = await makeParams(commitment=commitment)
+    return await makeLimitedCall(method, params)
+
+def getLatestBlockHash(commitment=None):
+    return get_async_response(async_getLatestBlockHash, commitment)
+
+async def async_getSignaturesForAddress(address, limit=None, before=None, after=None, finalized=None,encoding=None,commitment=None,errorProof=False):
+    finalized = finalized or True
+    method = "getSignaturesForAddress"
+    params = await makeParams(address, limit=limit or 1000, before=before, after=after, finalized=finalized,encoding=encoding or 'jsonParsed',commitment=commitment or 0)
+    signatureArray = await makeLimitedCall(method, await combineParamDicts(params))
+    if errorProof:
+        signatureArray = [signatureData for signatureData in signatureArray if signatureData.get('err') == None]
+    return signatureArray
+
+def getSignaturesForAddress(address, limit=None, before=None, after=None, finalized=None,encoding=None,commitment=None,errorProof=False):
+    return get_async_response(async_getSignaturesForAddress,address, limit=limit, before=before, after=after, finalized=finalized,encoding=encoding,commitment=commitment,errorProof=errorProof)
+
+async def async_getTransaction(signature,maxSupportedTransactionVersion=None):
+    maxSupportedTransactionVersion=maxSupportedTransactionVersion or 0
+    method = "getTransaction"
+    params = await makeParams(signature, maxSupportedTransactionVersion=maxSupportedTransactionVersion)
+    return await makeLimitedDbCall(method, params)
+
+def getTransaction(signature,maxSupportedTransactionVersion=None):
+    return get_async_response(async_getTransaction, signature,maxSupportedTransactionVersion=maxSupportedTransactionVersion)
+
+async def async_getAccountInfo(account,encoding=None,commitment=None):
+    encoding = encoding or "jsonParsed"
+    commitment = commitment or 0
+    method = "getAccountInfo"
+    params = await makeParams(account, encoding=encoding,commitment=commitment)
+    return await makeLimitedDbCall(method, await combineParamDicts(params))
+
+def getAccountInfo(account,encoding=None,commitment=None):
+    return get_async_response(async_getAccountInfo, account,encoding=encoding,commitment=commitment)
+
+async def async_getBalance(account):
+    method = "getBalance"
+    params = await makeParams(account)
+    return await makeLimitedCall(method, params)
+
+def getBalance(account):
+    return get_async_response(async_getBalance, account)
+
+async def async_getTokenSupply(address):
+    method = "getTokenSupply"
+    params = await makeParams(address)
+    return await makeLimitedDbCall(method, params)
+
+def getTokenSupply(address):
+    return get_async_response(async_getTokenSupply, address)
+
+async def async_getTokenAccountBalance(account,mint=None,commitment=None):
+    method = "getTokenAccountBalance"
+    commitment=commitment or 0
+    params = await makeParams(account,mint=mint,commitment=commitment)
+    return await makeLimitedCall(method, params)
+
+def getTokenAccountBalance(account,mint=None,commitment=0):
+    return get_async_response(async_getTokenAccountBalance, account,mint=mint,commitment=commitment)
+
+async def async_getTokenAccountsByOwner(account,mint=None,encoding=None):
+    encoding = encoding or "jsonParsed"
+    method = "getTokenAccountsByOwner"
+    params = await makeParams(account, mint=mint,encoding=encoding)
+    return await makeLimitedCall(method, params)
+
+def getTokenAccountsByOwner(account,mint=None,encoding=None):
+    return get_async_response(async_getTokenAccountsByOwner, account,mint=mint,encoding=encoding)
+
+async def async_sendTransaction(txn: Transaction, payer_keypair,skipPreflight=True, preflightCommitment=None):
+    txn.sign(payer_keypair)
+    txn_base58 = base58.b58encode(txn.serialize()).decode('utf-8')
+    opts=TxOpts(skip_preflight=skipPreflight)
+    preflightCommitment = preflightCommitment or "finalized"
+    method = "sendTransaction"
+    params = await makeParams(txn_base58, skipPreflight=opts.skipPreflight, preflightCommitment=preflightCommitment)
+    return await makeLimitedCall(method, params)
+
+def sendTransaction(txn: Transaction, payer_keypair,skipPreflight=True, preflightCommitment=None):
+    return get_async_response(async_sendTransaction, txn, payer_keypair=payer_keypair, skipPreflight=skipPreflight, preflightCommitment=preflightCommitment)
+
+async def async_simulateTransaction(txn: Transaction, payer_keypair,skipPreflight=True, preflightCommitment=None):
+    txn.sign(payer_keypair)
+    txn_base58 = base58.b58encode(txn.serialize()).decode('utf-8')
+    opts=TxOpts(skip_preflight=skipPreflight)
+    preflightCommitment = preflightCommitment or "finalized"
+    method = "simulateTransaction"
+    params = await makeParams(txn_base58, skipPreflight=opts.skipPreflight, preflightCommitment=preflightCommitment)
+    return await makeLimitedCall(method, params)
+
+def simulateTransaction(txn: Transaction, payer_keypair,skipPreflight=True, preflightCommitment=None):
+    return get_async_response(async_simulateTransaction, txn, payer_keypair=payer_keypair, skipPreflight=skipPreflight, preflightCommitment=preflightCommitment)
+
+async def async_getGenesisSignature(address):
+    before = None
+    last_valid_signature = None  # This will store the last seen valid signature
+    while True:
+        signatureArray = await getSignaturesForAddress(address, before=before, errorProof=True)
+        if not signatureArray:
+            return last_valid_signature  # Return the oldest non-errored signature found or None
+        for signatureData in reversed(signatureArray):
+            if signatureData.get('err') is None:
+                last_valid_signature = signatureData['signature']  # Update last valid signature found
+        before = signatureArray[-1]['signature']
+        
+def getGenesisSignature(address):
+    return get_async_response(async_getGenesisSignature, address)
+
+async def async_getParsedTransaction(tx_sig=None,txnData=None,maxSupportedTransactionVersion=None):
+    maxSupportedTransactionVersion=maxSupportedTransactionVersion or 0
+    method = "getParsedTransaction"
+    params = await makeParams(tx_sig=tx_sig,txnData=txnData, maxSupportedTransactionVersion=maxSupportedTransactionVersion)
+    return await makeLimitedDbCall(method, params)
+
+def getParsedTransaction(tx_sig=None,txnData=None,maxSupportedTransactionVersion=None):
+    return get_async_response(tx_sig=tx_sig,txnData=txnData, maxSupportedTransactionVersion=maxSupportedTransactionVersion)
