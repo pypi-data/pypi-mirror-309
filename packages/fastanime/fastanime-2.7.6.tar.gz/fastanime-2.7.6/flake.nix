13ef5805391681036201bd64358cbc2eebd52e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:20 +0000

    Add package fastanime/2.7.6/fastanime-2.7.6.tar.gz

---
+++ packages/fastanime/fastanime-2.7.6.tar.gz/fastanime-2.7.6/flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "FastAnime Project Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+
+      python = pkgs.python310;
+      pythonPackages = python.pkgs;
+      fastanimeEnv = pythonPackages.buildPythonApplication {
+        pname = "fastanime";
+        version = "2.7.5";
+
+        src = ./.;
+
+        # Add runtime dependencies
+        propagatedBuildInputs = with pythonPackages; [
+          click
+          inquirerpy
+          requests
+          rich
+          thefuzz
+          yt-dlp
+          dbus-python
+          hatchling
+        ];
+
+        # Ensure compatibility with the pyproject.toml
+        format = "pyproject";
+      };
+
+    in
+    {
+      packages.default = fastanimeEnv;
+
+      # DevShell for development
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          fastanimeEnv
+          pythonPackages.hatchling
+        ];
+      };
+    });
+}
