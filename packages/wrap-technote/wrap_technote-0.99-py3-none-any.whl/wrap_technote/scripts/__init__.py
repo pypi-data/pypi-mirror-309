048dcf07c945699b40f13cb5ca79d064378a15e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:43 +0000

    Add package wrap-technote/0.99/wrap_technote-0.99-py3-none-any.whl

---
+++ packages/wrap-technote/wrap_technote-0.99-py3-none-any.whl/wrap_technote/scripts/__init__.py
@@ -0,0 +1,8 @@
+from ..charts_utils import apply_technote_chart_style
+
+apply_technote_chart_style()
+
+from .waterlevels import waterlevels_run
+from .salinities import salinities_run
+from .summaries import summaries_run
+from .rainfall import rainfall_run
