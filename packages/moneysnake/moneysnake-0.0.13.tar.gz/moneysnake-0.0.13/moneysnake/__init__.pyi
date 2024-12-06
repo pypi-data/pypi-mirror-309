415289dc6ad88c3b3786d26065ce7dba3b4360b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:57 +0000

    Add package moneysnake/0.0.13/moneysnake-0.0.13.tar.gz

---
+++ packages/moneysnake/moneysnake-0.0.13.tar.gz/moneysnake-0.0.13/moneysnake/__init__.pyi
@@ -0,0 +1,6 @@
+from .client import MB_URL as MB_URL, MB_VERSION_ID as MB_VERSION_ID, post_request as post_request, set_admin_id as set_admin_id, set_timeout as set_timeout, set_token as set_token
+from .contact import Contact as Contact, ContactPerson as ContactPerson
+from .external_sales_invoice import ExternalSalesInvoice as ExternalSalesInvoice
+from .payment import Payment as Payment
+
+__all__ = ['MB_URL', 'MB_VERSION_ID', 'post_request', 'set_admin_id', 'set_timeout', 'set_token', 'Contact', 'ContactPerson', 'ExternalSalesInvoice', 'Payment']
