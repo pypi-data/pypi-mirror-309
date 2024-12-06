db18026f42da26aebbf703319cee5d8cbe1b257f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:22 +0000

    Add package pypomes-crypto/0.3.2/pypomes_crypto-0.3.2.tar.gz

---
+++ packages/pypomes-crypto/pypomes_crypto-0.3.2.tar.gz/pypomes_crypto-0.3.2/src/pypomes_crypto/__init__.py
@@ -0,0 +1,21 @@
+from .crypto_pkcs7 import (
+    CryptoPkcs7,
+)
+from .crypto_pomes import (
+    CRYPTO_DEFAULT_HASH_ALGORITHM,
+    crypto_validate_p7s, crypto_validate_pdf,
+    crypto_compute_hash, crypto_generate_rsa_keys,
+)
+
+__all__ = [
+    # crypto_pkcs7
+    "CryptoPkcs7",
+    # crypto_pomes
+    "CRYPTO_DEFAULT_HASH_ALGORITHM",
+    "crypto_validate_p7s", "crypto_validate_pdf",
+    "crypto_compute_hash", "crypto_generate_rsa_keys",
+]
+
+from importlib.metadata import version
+__version__ = version("pypomes_crypto")
+__version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
