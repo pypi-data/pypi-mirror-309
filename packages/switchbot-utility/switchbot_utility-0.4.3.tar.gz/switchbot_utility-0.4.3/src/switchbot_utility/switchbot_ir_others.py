0cc1888898d86fc78ba6021df1299229527281bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:36 +0000

    Add package switchbot-utility/0.4.3/switchbot_utility-0.4.3.tar.gz

---
+++ packages/switchbot-utility/switchbot_utility-0.4.3.tar.gz/switchbot_utility-0.4.3/src/switchbot_utility/switchbot_ir_others.py
@@ -0,0 +1,21 @@
+from switchbot_utility.switchbot_device import SwitchbotDevice
+
+
+class IrOthers(SwitchbotDevice):
+    """IR virtual device others class"""
+
+    def __init__(self, deviceId):
+        super().__init__(deviceId)
+
+    def customize(self, button_name: str) -> str:
+        """Execute customized button
+
+        Args:
+            button_name (str):
+        """
+
+        body = {"commandType": "customize", "parameter": "default"}
+        body["command"] = button_name
+
+        response = self.command(self.deviceId, body)
+        return response.text
