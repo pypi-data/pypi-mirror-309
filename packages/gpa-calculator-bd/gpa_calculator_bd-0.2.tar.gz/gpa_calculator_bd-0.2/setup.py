b348470c6beb13c1fc1ec11aadbbeb3cce38b8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:43 +0000

    Add package gpa-calculator-bd/0.2/gpa_calculator_bd-0.2.tar.gz

---
+++ packages/gpa-calculator-bd/gpa_calculator_bd-0.2.tar.gz/gpa_calculator_bd-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gpa_calculator_bd",  
+    version="0.2",  
+    packages=find_packages(),  
+    description="A package to calculate GPA, CGPA, and average marks with rounding to 2 decimal places",  
+    author="Your Name",  
+    author_email="naimur.naiyimu@gmail.com",  
+    url="https://github.com/naimur-naiyimu/gpa_calculator_bd",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+)
