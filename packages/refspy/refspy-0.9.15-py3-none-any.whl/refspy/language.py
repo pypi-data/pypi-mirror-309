3bf60bd94c18097753461d945eb10189d6d906a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:01 +0000

    Add package refspy/0.9.15/refspy-0.9.15-py3-none-any.whl

---
+++ packages/refspy/refspy-0.9.15-py3-none-any.whl/refspy/language.py
@@ -0,0 +1,17 @@
+"""Data object for language options.
+
+Program strings that vary with the selected language.
+
+Attributes:
+    verse_markers: The equivalent of 'v.' and vv.' in English.
+    number_prefixes: The equivalent of 'I' and 'First' (etc) in English.
+"""
+
+from typing import Dict, List
+
+from pydantic import BaseModel
+
+
+class Language(BaseModel):
+    verse_markers: List[str]
+    number_prefixes: Dict[str, List[str]]
