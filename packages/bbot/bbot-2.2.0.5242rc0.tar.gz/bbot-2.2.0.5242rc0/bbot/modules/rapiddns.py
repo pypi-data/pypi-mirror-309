a7650a1a456659da1239f69402f120d31a68939a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:34 +0000

    Add package bbot/2.2.0.5242rc0/bbot-2.2.0.5242rc0.tar.gz

---
+++ packages/bbot/bbot-2.2.0.5242rc0.tar.gz/bbot-2.2.0.5242rc0/bbot/modules/rapiddns.py
@@ -0,0 +1,28 @@
+from bbot.modules.templates.subdomain_enum import subdomain_enum
+
+
+class rapiddns(subdomain_enum):
+    flags = ["subdomain-enum", "passive", "safe"]
+    watched_events = ["DNS_NAME"]
+    produced_events = ["DNS_NAME"]
+    meta = {
+        "description": "Query rapiddns.io for subdomains",
+        "created_date": "2022-08-24",
+        "author": "@TheTechromancer",
+    }
+
+    base_url = "https://rapiddns.io"
+
+    async def request_url(self, query):
+        url = f"{self.base_url}/subdomain/{self.helpers.quote(query)}?full=1#result"
+        response = await self.api_request(url, timeout=self.http_timeout + 10)
+        return response
+
+    def parse_results(self, r, query):
+        results = set()
+        text = getattr(r, "text", "")
+        for match in self.helpers.regexes.dns_name_regex.findall(text):
+            match = match.lower()
+            if match.endswith(query):
+                results.add(match)
+        return results
