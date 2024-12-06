7d02bb4b293c54d4a0449f86d527a07ef16a8c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:11 +0000

    Add package llm-sdk/0.2.6/llm_sdk-0.2.6.tar.gz

---
+++ packages/llm-sdk/llm_sdk-0.2.6.tar.gz/llm_sdk-0.2.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llm_sdk',
+    version='0.2.6',
+    author='Olsi Hoxha',
+    author_email='olsihoxha824l@gmail.com',
+    description='SDK to get all the available LLM models per Provider',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests',
+        'pydantic'
+    ],
+    python_requires='>=3.6',
+)
