7c4b93101bfca241d3a859a746ef1d26d23ee977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:45 +0000

    Add package keyring-proxy-stdio/0.1.11/keyring_proxy_stdio-0.1.11-py3-none-any.whl

---
+++ packages/keyring-proxy-stdio/keyring_proxy_stdio-0.1.11-py3-none-any.whl/keyring_proxy_stdio/__init__.py
@@ -0,0 +1,22 @@
+from typing import override
+
+from jaraco.classes import properties
+from keyring_proxy.backend import ProxyBackend
+from keyring_proxy.stdioproxy import DEFAULT_EXE_PATH, StdioClient
+from keyring_proxy.transport import TransportClient
+
+from keyring_proxy_stdio._version import __version__ as __version__
+
+PRIORITY = 9.9
+
+
+class StdioProxyBackend(ProxyBackend):
+    exe = DEFAULT_EXE_PATH
+
+    @override
+    def _get_transport(self) -> TransportClient:
+        return StdioClient.from_path(self.exe)
+
+    @properties.classproperty
+    def priority(cls):
+        return PRIORITY
