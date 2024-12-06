71687b094514cd7c79b9f0c5f31b765ee29f2841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:15 +0000

    Add package cas-base/1.0.8/cas_base-1.0.8-py3-none-any.whl

---
+++ packages/cas-base/cas_base-1.0.8-py3-none-any.whl/cas/base/logger.py
@@ -0,0 +1,16 @@
+import os
+import time
+from loguru import logger
+from .constant import log_level, log_dir
+
+log_path_app = os.path.join(log_dir, f"app.log")
+logger.remove()
+logger.add(
+    lambda msg: print(msg, end=""),
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> - <bold><level>{level}</level></bold>:<magenta>{message}</magenta>",
+    colorize=True,
+    level=log_level,
+    backtrace=False,
+    diagnose=False,
+)
+logger.add(log_path_app, rotation="00:00", retention="15 days", enqueue=True, encoding="UTF-8", level=log_level)
