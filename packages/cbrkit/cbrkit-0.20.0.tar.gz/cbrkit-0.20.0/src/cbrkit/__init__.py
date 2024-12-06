b7b07da06ba366ebe7554db17f1c8f5ff75b0204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:03:37 +0000

    Add package cbrkit/0.20.0/cbrkit-0.20.0.tar.gz

---
+++ packages/cbrkit/cbrkit-0.20.0.tar.gz/cbrkit-0.20.0/src/cbrkit/__init__.py
@@ -0,0 +1,28 @@
+"""
+**[🌟 GitHub Project 🌟](https://github.com/wi2trier/cbrkit)**
+
+.. include:: ../README.md
+
+"""
+
+from . import (
+    adapt,
+    eval,
+    helpers,
+    loaders,
+    retrieval,
+    reuse,
+    sim,
+    typing,
+)
+
+__all__ = [
+    "loaders",
+    "sim",
+    "retrieval",
+    "adapt",
+    "reuse",
+    "eval",
+    "typing",
+    "helpers",
+]
