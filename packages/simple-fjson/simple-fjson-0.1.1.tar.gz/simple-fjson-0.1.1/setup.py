845016f656a3d9cd25bdd45cdaf126df862c30fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:58 +0000

    Add package simple-fjson/0.1.1/simple-fjson-0.1.1.tar.gz

---
+++ packages/simple-fjson/simple-fjson-0.1.1.tar.gz/simple-fjson-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple-fjson',
+    version='0.1.1',
+    packages=find_packages(),
+    description='A flexible JSON parser',
+    long_description=open('readme-en.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='sjrsjz',
+    author_email='sjrsjz@gmail.com',
+    url='https://github.com/sjrsjz/fjson',  # 项目的URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
