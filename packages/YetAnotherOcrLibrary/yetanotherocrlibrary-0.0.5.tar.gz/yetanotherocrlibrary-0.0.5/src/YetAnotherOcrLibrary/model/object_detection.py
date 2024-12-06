5990245ce305cc60ed1163e34754c2041526d98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:07:27 +0000

    Add package YetAnotherOcrLibrary/0.0.5/yetanotherocrlibrary-0.0.5.tar.gz

---
+++ packages/YetAnotherOcrLibrary/yetanotherocrlibrary-0.0.5.tar.gz/yetanotherocrlibrary-0.0.5/src/YetAnotherOcrLibrary/model/object_detection.py
@@ -0,0 +1,7 @@
+class ObjectDetection(object):
+    def __init__(self, x, y, width, height, threshold):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.threshold = threshold
