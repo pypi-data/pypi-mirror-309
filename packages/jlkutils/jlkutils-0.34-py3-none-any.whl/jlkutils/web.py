a200e500a706595c9d6ec54d7aa95c451d940f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:03:55 +0000

    Add package jlkutils/0.34/jlkutils-0.34-py3-none-any.whl

---
+++ packages/jlkutils/jlkutils-0.34-py3-none-any.whl/jlkutils/web.py
@@ -0,0 +1,14 @@
+import re
+import requests
+def upload(filename):
+    with open(filename, 'rb') as file:
+        response = requests.post('https://bashupload.com/', files={'file': file})
+            
+        match = re.search(r'wget (https?://[^\s]+)', response.text)
+        if match:
+            picurl = match.group(1)
+            return picurl
+        else:
+            print("Fehler beim Upload")
+def ntfy(text, group):
+    requests.post(f"https://ntfy.sh/{group}", data=text.encode(encoding='utf-8'))
