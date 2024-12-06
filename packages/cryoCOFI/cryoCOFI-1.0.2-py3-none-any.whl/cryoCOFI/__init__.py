98b872491403f4c48e4b0f6bf4bf8649d7a00f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:51 +0000

    Add package cryoCOFI/1.0.2/cryoCOFI-1.0.2-py3-none-any.whl

---
+++ packages/cryoCOFI/cryoCOFI-1.0.2-py3-none-any.whl/cryoCOFI/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+def get_lib_path(lib_name):
+    '''
+    Get the path to the library.
+    Args:
+        lib_name: name of the library
+    Returns:
+        path: path to the library
+    '''
+    return os.path.join(os.path.dirname(__file__), 'lib', f'{lib_name}.so')
