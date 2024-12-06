cbf00dabb8720fa60c434d2e6dc02be318fcbcbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:24 +0000

    Add package django-wakawaka/1.6/django_wakawaka-1.6-py3-none-any.whl

---
+++ packages/django-wakawaka/django_wakawaka-1.6-py3-none-any.whl/wakawaka/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from wakawaka.models import Revision, WikiPage
+
+
+class RevisionInlines(admin.TabularInline):
+    model = Revision
+    extra = 1
+
+
+@admin.register(WikiPage)
+class WikiPageAdmin(admin.ModelAdmin):
+    inlines = (RevisionInlines,)
+
+
+@admin.register(Revision)
+class RevisionAdmin(admin.ModelAdmin):
+    pass
