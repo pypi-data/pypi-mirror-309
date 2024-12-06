6bda5ff1573e743d8ed0d172c209acf7b69aacc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:17 +0000

    Add package g4f/0.3.4.2/g4f-0.3.4.2-py3-none-any.whl

---
+++ packages/g4f/g4f-0.3.4.2-py3-none-any.whl/g4f/Provider/needs_auth/__init__.py
@@ -0,0 +1,23 @@
+from .gigachat         import *
+
+#from .MetaAIAccount   import MetaAIAccount
+#from .OpenaiAccount   import OpenaiAccount
+
+from .BingCreateImages import BingCreateImages
+from .DeepInfra        import DeepInfra
+from .DeepInfraImage   import DeepInfraImage
+from .Gemini           import Gemini
+from .GeminiPro        import GeminiPro
+from .Groq             import Groq
+from .HuggingFace      import HuggingFace
+from .MetaAI           import MetaAI
+from .MetaAIAccount    import MetaAIAccount
+from .OpenaiAPI        import OpenaiAPI
+from .OpenaiChat       import OpenaiChat
+from .PerplexityApi    import PerplexityApi
+from .Poe              import Poe
+from .Raycast          import Raycast
+from .Replicate        import Replicate
+from .Theb             import Theb
+from .ThebApi          import ThebApi
+from .WhiteRabbitNeo   import WhiteRabbitNeo
