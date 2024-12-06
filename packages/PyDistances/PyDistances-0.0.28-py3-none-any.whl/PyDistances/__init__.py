c8a307b2ff656cfb5b45c35005d36b2e7c470277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:26 +0000

    Add package PyDistances/0.0.28/PyDistances-0.0.28-py3-none-any.whl

---
+++ packages/PyDistances/PyDistances-0.0.28-py3-none-any.whl/PyDistances/__init__.py
@@ -0,0 +1,4 @@
+from .quantitative import Euclidean_dist_matrix, Euclidean_dist, Minkowski_dist_matrix, Minkowski_dist, Canberra_dist_matrix, Canberra_dist, Pearson_dist_matrix, Mahalanobis_dist_matrix, Mahalanobis_dist, Robust_Maha_dist_matrix, Robust_Maha_dist
+from .binary import  Sokal_dist_matrix, Sokal_dist, Jaccard_dist_matrix, Jaccard_dist
+from .multiclass import Matching_dist_matrix, Matching_dist
+from .mixed import GG_dist_matrix, GG_dist, RelMS_dist_matrix
