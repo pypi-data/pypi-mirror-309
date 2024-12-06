e57b61f1d2b198d99495a0da0d53ae0107fc9549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:56 +0000

    Add package uglyorange/0.1.4/uglyorange-0.1.4.tar.gz

---
+++ packages/uglyorange/uglyorange-0.1.4.tar.gz/uglyorange-0.1.4/uglyorange/install/app.py
@@ -0,0 +1,24 @@
+from .types import AbstractInstaller
+import codefast as cf
+
+
+class AppInstaller(AbstractInstaller):
+    def __init__(self, apps: list):
+        self.apps = apps
+
+    def update(self):
+        cf.shell("apt update -y", print_str=True)
+
+    def install(self):
+        self.update()
+        for app in self.apps:
+            cf.info(f"Installing {app}")
+            cf.shell(f"apt install -y {app}", print_str=True)
+
+def app_install():
+    apps = [
+        "vim", "neofetch", "curl", "emacs", "fish", "docker.io", "docker-compose",
+        "net-tools", "vnstat", "tree", "htop", "ttyd", "sysstat", "ufw", "lsb-release",
+        "snapd", "cron"
+    ]
+    AppInstaller(apps).install()
