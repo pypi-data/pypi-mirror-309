5fd66d5778b6b9c4965bbf47184667b8bd9e86cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:15 +0000

    Add package codelint/0.1.2/codelint-0.1.2-py3-none-any.whl

---
+++ packages/codelint/codelint-0.1.2-py3-none-any.whl/codelint/checker.py
@@ -0,0 +1,28 @@
+import sys
+from colorama import Fore, Style
+from codelint.model import CodeLint
+                
+def run_checks(self):
+    issues = []
+    issues.extend(CodeLint.check_line_length())
+    issues.extend(CodeLint.check_indentation())
+    issues.extend(CodeLint.check_snake_case())
+    issues.extend(CodeLint.check_trailing_whitespace())
+    issues.extend(CodeLint.check_excessive_blank_lines())
+    return issues
+    
+
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(Fore.RED + "Usage: python code_lint.py <file_path>" + Style.RESET_ALL)
+        sys.exit(1)
+    else:
+        linter = CodeLint(sys.argv[1])
+        result = linter.run_checks()
+        if result:
+            print(Fore.YELLOW + "Issues Found:" + Style.RESET_ALL)
+            print("\n".join(Fore.CYAN + issue + Style.RESET_ALL for issue in result))
+        else:
+            print(Fore.GREEN + "No issues found. Your code is clean!" + Style.RESET_ALL)
