34a170103fb46d6143ca6eb3a4a587e849b377f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:02 +0000

    Add package alchemizer/0.1.1/alchemizer-0.1.1-py3-none-any.whl

---
+++ packages/alchemizer/alchemizer-0.1.1-py3-none-any.whl/alchemizer/__init__.py
@@ -0,0 +1,4 @@
+"""
+Alchemizer: A meta-framework to facilitate development of frameworks 
+using identity map and unit of work patterns.
+"""
---
+++ packages/alchemizer/alchemizer-0.1.1-py3-none-any.whl/alchemizer/entity/__init__.py
@@ -0,0 +1,4 @@
+"""
+Implements base class for an entity, a framework-specific object which is
+managed by the identity map.
+"""
