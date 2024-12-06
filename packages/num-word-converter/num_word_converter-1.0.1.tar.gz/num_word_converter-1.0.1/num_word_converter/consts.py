76617d394c6b8132e7a5515fd5759530a4b14bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:22 +0000

    Add package num-word-converter/1.0.1/num_word_converter-1.0.1.tar.gz

---
+++ packages/num-word-converter/num_word_converter-1.0.1.tar.gz/num_word_converter-1.0.1/num_word_converter/consts.py
@@ -0,0 +1,4 @@
+UNITS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+         'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+TENS = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+SCALES = ['hundred', 'thousand', 'million', 'billion', 'trillion']
