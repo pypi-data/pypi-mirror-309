10abd03c42e49bf0a6e7d15f08f70577dd89c3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:32 +0000

    Add package pqpopups/1.0.9/pqpopups-1.0.9.tar.gz

---
+++ packages/pqpopups/pqpopups-1.0.9.tar.gz/pqpopups-1.0.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pqpopups',
+    version='1.0.9',
+    description='A custom PyQt5 popups package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='kwonyoungil',
+    author_email='kwen232@gmail.com',
+    url='https://github.com/kyi1107/pqpopups',
+    packages=find_packages(),
+    install_requires=[
+        'pqwidgets>=1.0.7',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
