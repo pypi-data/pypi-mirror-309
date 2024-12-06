faf0e79664566dbdc8980796a44b1102825f36f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:37 +0000

    Add package scikit-hep-testdata/0.5.0/scikit_hep_testdata-0.5.0-py3-none-any.whl

---
+++ packages/scikit-hep-testdata/scikit_hep_testdata-0.5.0-py3-none-any.whl/skhep_testdata/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from .local_files import data_path, download_all, known_files
+from .version import version as __version__
+
+__all__ = ("data_path", "download_all", "known_files", "__version__")
+
+
+def __dir__() -> tuple[str, ...]:  # pragma: no cover
+    return __all__
