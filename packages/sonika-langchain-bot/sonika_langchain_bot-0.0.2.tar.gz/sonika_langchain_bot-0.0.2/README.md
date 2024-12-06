b79e934868129ea8787b68e3a52a1c4f027c42e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:10 +0000

    Add package sonika-langchain-bot/0.0.2/sonika_langchain_bot-0.0.2.tar.gz

---
+++ packages/sonika-langchain-bot/sonika_langchain_bot-0.0.2.tar.gz/sonika_langchain_bot-0.0.2/README.md
@@ -0,0 +1,13 @@
+## Instalacion
+
+1. Creacion del ambiente virtual
+
+   `python -m venv myvenv`
+
+2. Ingresar al ambiente virtual
+
+   `source myvenv/bin/activate`
+
+3. Instalacion de dependencias necesarias
+
+   `pip install -r requirements.txt`
---
+++ packages/sonika-langchain-bot/sonika_langchain_bot-0.0.2.tar.gz/sonika_langchain_bot-0.0.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sonika-langchain-bot",
+    version="0.0.2", 
+    description="Agente langchain con LLM",
+    author="Erley Blanco Carvajal",
+    license="MIT License",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),  # Encuentra automáticamente todos los paquetes
+    install_requires=[
+        "langchain==0.3.0",
+        "langchain-community==0.3.0",
+        "langchain-core==0.3.5",
+        "langchain-openai==0.2.0",
+        "langgraph==0.2.39",
+        "langgraph-checkpoint==2.0.2",
+        "langgraph-sdk==0.1.34",
+        "dataclasses-json==0.6.7",
+        "python-dateutil==2.9.0.post0",
+        "tiktoken==0.7.0",
+        "pydantic==2.9.2",
+        "faiss-cpu==1.8.0.post1",
+        "pypdf==5.0.0",
+        "python-dotenv==1.0.1",
+        "typing_extensions==4.12.0",
+        "typing-inspect==0.9.0",
+    ],
+    extras_require={
+        "dev": [
+            "sphinx>=8.1.3,<9.0.0",
+            "sphinx-rtd-theme>=3.0.1,<4.0.0",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Cambia según la versión mínima de Python que soportes
+)
