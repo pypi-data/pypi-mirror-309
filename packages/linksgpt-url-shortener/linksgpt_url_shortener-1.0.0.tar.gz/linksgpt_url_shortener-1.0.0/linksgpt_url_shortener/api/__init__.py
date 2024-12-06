c9f5139966562e064f7cf2848adf2dd1b93911f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:38 +0000

    Add package linksgpt-url-shortener/1.0.0/linksgpt_url_shortener-1.0.0.tar.gz

---
+++ packages/linksgpt-url-shortener/linksgpt_url_shortener-1.0.0.tar.gz/linksgpt_url_shortener-1.0.0/linksgpt_url_shortener/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from linksgpt_url_shortener.api.v1_domains_api import V1DomainsApi
+from linksgpt_url_shortener.api.v1_links_api import V1LinksApi
+from linksgpt_url_shortener.api.v1_users_api import V1UsersApi
