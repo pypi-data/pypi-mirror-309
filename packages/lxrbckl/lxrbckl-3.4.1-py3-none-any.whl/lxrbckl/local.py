d734d611ea575f73050ddd723b80a15ee7aed8a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:55 +0000

    Add package lxrbckl/3.4.1/lxrbckl-3.4.1-py3-none-any.whl

---
+++ packages/lxrbckl/lxrbckl-3.4.1-py3-none-any.whl/lxrbckl/local.py
@@ -0,0 +1,125 @@
+# import <
+from re import split
+from json.decoder import JSONDecodeError
+from json import (
+   
+   dump,
+   load
+   
+)
+from os import (
+   
+   getcwd,
+   remove
+   
+)
+from os.path import (
+   
+   isfile,
+   dirname,
+   basename
+   
+)
+
+# >
+
+
+def getProjectPath(
+   
+   pFile: str = '',
+   pDelimeter: str = '/'
+   
+):
+   '''  '''
+   
+   cwd = getcwd()
+   file = pDelimeter.join(split(r'[/\\]+', pFile))
+   
+   return pDelimeter.join([dirname(cwd), basename(cwd), file])
+   
+
+def fileSet(
+   
+   pData,
+   pFile: str,
+   pIndent: int = 3,
+   pOverride: bool = True,
+   pPath: str = getProjectPath()
+   
+):
+   '''  '''
+      
+   # try (if permitted) <
+   # except (then not permitted) <
+   try:
+
+      # if (write to file) <
+      # else (throw error) <
+      if ((isfile(f'{pPath}{pFile}') is False) or (pOverride)):
+         
+         with open(f'{pPath}{pFile}', 'w') as fout:
+            
+            {
+               
+               'txt' : lambda : fout.write(pData),
+               'json' : lambda : dump(
+                  
+                  fp = fout,
+                  obj = pData,
+                  indent = pIndent
+                  
+               )
+               
+            }[pFile.split('.')[-1]]()
+
+      else: return 'File already exists.'
+
+      # >
+
+   except TypeError: return 'Invalid data for this filetype.'
+   
+   # >
+
+
+def fileGet(
+   
+   pFile: str,
+   pPath: str = getProjectPath()
+   
+):
+   '''  '''
+   
+   # try (if existing file) <
+   # except (then bad .json format) <
+   # except (then non-existing file) <
+   try:
+   
+      with open(f'{pPath}{pFile}', 'r') as fin:
+         
+         return {
+            
+            'txt' : lambda : fin.read(),
+            'json' : lambda : load(fin)
+            
+         }[pFile.split('.')[1]]()
+   
+   except JSONDecodeError: return False
+   except FileNotFoundError: return False
+   
+   # >
+
+
+def fileDel(
+   
+   pFile: str,
+   pPath: str = getProjectPath()
+
+):
+   '''  '''
+   
+   # try (if existing file) <
+   # except (then non-existing file) <
+   try: remove(f'{pPath}{pFile}')
+   except FileNotFoundError: return 'File does not exist.'
+   
+   # >
---
+++ packages/lxrbckl/lxrbckl-3.4.1-py3-none-any.whl/lxrbckl/remote.py
@@ -0,0 +1,155 @@
+# import <
+from requests import get
+from json import loads, dumps
+from json.decoder import JSONDecodeError
+from requests.exceptions import MissingSchema
+
+# >
+
+
+def githubSet(
+   
+   pData,
+   pFilename: str,
+   pGithub: object,
+   pRepository: str,
+   
+   isNew: bool = False,
+   pBranch: str = 'main'
+   
+):
+   '''  '''
+   
+   # try (if permitted) <
+   # except (then not permitted) <
+   try:
+   
+      repository = pGithub.get_repo(pRepository)
+      
+      # if (create) <
+      # elif (update) <
+      if (isNew): 
+      
+         repository.create_file(
+            
+            path = pFilename,
+            branch = pBranch,
+            content = dumps(pData),
+            message = 'Automated Set'
+            
+         )
+      
+      elif (not isNew):
+         
+         data = str(pData).replace('\'', '\"').replace('None', 'null')
+         content = repository.get_contents(path = pFilename, ref = pBranch)
+         
+         repository.update_file(
+            
+            content = data,
+            branch = pBranch,
+            sha = content.sha,
+            path = content.path,
+            message = 'Automated Update'
+            
+         )
+      
+      # >
+      
+   except: return {
+      
+      True : 'File already exists.',
+      False : 'File does not exist.'
+      
+   }[isNew]
+   
+   # >
+   
+
+def githubGet(
+   
+   pFilename: str,
+   pGithub: object,
+   pRepository: str,
+   
+   pBranch: str = 'main'
+   
+):
+   '''  '''
+   
+   # try (if existing) <
+   # except (then non-existing) <
+   try:
+   
+      # get repository <
+      # get content from repository <
+      repository = pGithub.get_repo(pRepository)
+      content = repository.get_contents(path = pFilename, ref = pBranch)
+
+      # >
+
+      return loads(content.decoded_content.decode())
+
+   except: return 'File does not exist.'
+
+   # >
+
+
+def githubDel(
+   
+   pGithub,
+   pFilename: str,
+   pRepository: str,
+   
+   pBranch: str = 'main',
+   pMessage: str = 'Automated Deletion'
+   
+):
+   '''  '''
+   
+   # try (if existing) <
+   # except (then non-existing) <
+   try:
+   
+      repository = pGithub.get_repo(pRepository)
+      file = repository.get_contents(path = pFilename, ref = pBranch)
+         
+      repository.delete_file(
+         
+         sha = file.sha,
+         path = pFilename,
+         branch = pBranch,
+         message = pMessage
+         
+      )
+         
+   except: return 'File does not exist.'
+   
+   # >
+   
+
+def requestsGet(
+   
+   pLink: str,
+   pShowError: bool = False
+
+):
+   '''  '''
+
+   # try (if valid link) <
+   # except (then invalid query) <
+   try: 
+      
+      return {
+      
+         'txt' : lambda : get(pLink).text,
+         'json' : lambda : loads(get(pLink).text)
+         
+      }[pLink.split('.')[-1]]()
+   
+   except Exception as e:
+      
+      if (pShowError): print('Error: ', e)
+      elif (not pShowError): pass
+   
+   # >
