1863d81324f70a662e81f7d9821d8ecb1bd27d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:23 +0000

    Add package abstract-solcatcher/0.0.3.81/abstract_solcatcher-0.0.3.81-py3-none-any.whl

---
+++ packages/abstract-solcatcher/abstract_solcatcher-0.0.3.81-py3-none-any.whl/abstract_solcatcher/sniping/pumpFun/idfs.py
@@ -0,0 +1,2 @@
+words="""logging,websockets,json,asyncio,websockets,logging,threading,time,base58,struct,base64,psycopg2,struct,base58,base64,time,json,asyncio""".split(',')
+input(str(list(set(words))).replace('"','').replace("'",'').replace('[','').replace(']',''))
