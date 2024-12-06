23ed94e22f9693f7ddb2ea5bbf04ef7a32bb9bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:18 +0000

    Add package pianosdk/16.331.0/pianosdk-16.331.0-py3-none-any.whl

---
+++ packages/pianosdk/pianosdk-16.331.0-py3-none-any.whl/pianosdk/publisher/models/source_eligible_term.py
@@ -0,0 +1,17 @@
+from datetime import date, datetime
+from pydantic.main import BaseModel
+from typing import Optional
+
+
+class SourceEligibleTerm(BaseModel):
+    variant_type: Optional[str] = None
+    term_id: Optional[str] = None
+    name: Optional[str] = None
+    type: Optional[str] = None
+    period_id: Optional[str] = None
+    period_name: Optional[str] = None
+    tooltip: Optional[str] = None
+    enabled: Optional[bool] = None
+
+
+SourceEligibleTerm.model_rebuild()
