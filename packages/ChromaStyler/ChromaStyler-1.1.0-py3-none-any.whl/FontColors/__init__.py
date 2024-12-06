9484b7af2c4e9aa902abf36515062f11c02497c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:50 +0000

    Add package ChromaStyler/1.1.0/ChromaStyler-1.1.0-py3-none-any.whl

---
+++ packages/ChromaStyler/ChromaStyler-1.1.0-py3-none-any.whl/FontColors/__init__.py
@@ -0,0 +1,11 @@
+# ChromaStyler/__init__.py
+from .color import Color
+from .style import FontOptions
+from .back import Background
+
+# Simplified imports for users
+color = Color
+style = FontOptions
+back  = Background
+
+__version__ = "1.1.0"
---
+++ packages/ChromaStyler/ChromaStyler-1.1.0-py3-none-any.whl/FontColors/back.py
@@ -0,0 +1,17 @@
+# ChromaStyler/background.py
+
+class Background:
+    reset        =  "\x1b[40m"
+    white        =  "\x1b[47m"
+    red          =  "\x1b[41m"
+    blue         =  "\x1b[44m"
+    green        =  "\x1b[42m"
+    cyan         =  "\x1b[46m"
+    purple       =  "\x1b[45m"
+    lightwhite   =  "\x1b[107m"
+    lightred     =  "\x1b[101m"
+    lightblue    =  "\x1b[104m"
+    lightgreen   =  "\x1b[102m"
+    lightcyan    =  "\x1b[106m"
+    lightpuple   =  "\x1b[105m"
+    grey         =  "\x1b[100m"
