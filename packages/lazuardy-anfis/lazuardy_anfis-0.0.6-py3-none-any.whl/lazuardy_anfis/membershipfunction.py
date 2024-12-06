2041d194bd60fe00e422d59676c1b75080e8c12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:40 +0000

    Add package lazuardy-anfis/0.0.6/lazuardy_anfis-0.0.6-py3-none-any.whl

---
+++ packages/lazuardy-anfis/lazuardy_anfis-0.0.6-py3-none-any.whl/lazuardy_anfis/membershipfunction.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon Mar 31 15:41:58 2014
+Updated on Fri Nov 15 00:00:00 2024
+
+@author:
+- twmeggs <twmeggs@gmail.com>
+- lazuardy-tech <contact@lazuardy.tech>
+"""
+
+from skfuzzy import gaussmf, gbellmf, sigmf
+
+
+class MemFuncs:
+    "Common base class for all employees"
+
+    funcDict = {"gaussmf": gaussmf, "gbellmf": gbellmf, "sigmf": sigmf}
+
+    def __init__(self, MFList):
+        self.MFList = MFList
+
+    def evaluateMF(self, rowInput):
+        if len(rowInput) != len(self.MFList):
+            print("Number of variables does not match number of rule sets")
+
+        return [
+            [
+                self.funcDict[self.MFList[i][k][0]](rowInput[i], **self.MFList[i][k][1])
+                for k in range(len(self.MFList[i]))
+            ]
+            for i in range(len(rowInput))
+        ]
