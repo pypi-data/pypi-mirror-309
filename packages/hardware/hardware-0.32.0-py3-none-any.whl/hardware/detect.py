6f05c2bb676dd7aa8ac866b867bd31cc0ccda1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:19 +0000

    Add package hardware/0.32.0/hardware-0.32.0-py3-none-any.whl

---
+++ packages/hardware/hardware-0.32.0-py3-none-any.whl/hardware/detect.py
@@ -0,0 +1,106 @@
+# Copyright (C) 2013-2015 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Main entry point for hardware and system detection routines in eDeploy."""
+
+import argparse
+import json
+import os
+import pprint
+import sys
+
+from hardware import areca
+from hardware.benchmark import cpu as bm_cpu
+from hardware.benchmark import disk as bm_disk
+from hardware.benchmark import mem as bm_mem
+from hardware import bios_hp
+from hardware import detect_utils
+from hardware import diskinfo
+from hardware import hpacucli
+from hardware import infiniband as ib
+from hardware import ipmi
+from hardware import megacli
+from hardware import rtc
+from hardware import sensors
+from hardware import system
+
+
+def parse_args(arguments):
+    """Arguments parser."""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', '--human',
+                        help='Print output in human readable format',
+                        action='store_true',
+                        default=False)
+
+    benchmark = parser.add_argument_group('benchmark')
+    benchmark.add_argument('--benchmark', '-b',
+                           choices=['cpu', 'mem', 'disk'],
+                           nargs='+',
+                           help=('Run benchmark for specific components. '
+                                 'Valid components are: cpu, mem, disk'))
+    benchmark.add_argument('--benchmark-disk-destructive',
+                           help=('If specified make the disk component '
+                                 'benchmark to be destructive'),
+                           action='store_true',
+                           default=False)
+
+    return parser.parse_args(arguments)
+
+
+def main():
+    """Command line entry point."""
+
+    os.environ["LANG"] = "en_US.UTF-8"
+    args = parse_args(sys.argv[1:])
+
+    hrdw = []
+
+    hrdw.extend(areca.detect())
+    hrdw.extend(hpacucli.detect())
+    hrdw.extend(megacli.detect())
+    hrdw.extend(diskinfo.detect())
+
+    system_info = system.detect()
+    if not system_info:
+        sys.exit(1)
+    hrdw.extend(system_info)
+
+    hrdw.extend(ipmi.detect())
+    hrdw.extend(ib.detect())
+    hrdw.extend(sensors.detect_temperatures())
+    hrdw.extend(ipmi.get_ipmi_sdr())
+    hrdw.extend(rtc.detect_rtc_clock())
+    hrdw.extend(detect_utils.detect_auxv())
+    hrdw.extend(detect_utils.parse_dmesg())
+    hrdw.extend(bios_hp.dump_hp_bios(hrdw))
+
+    if args.benchmark:
+        if 'cpu' in args.benchmark:
+            bm_cpu.cpu_perf(hrdw)
+        if 'mem' in args.benchmark:
+            bm_mem.mem_perf(hrdw)
+        if 'disk' in args.benchmark:
+            bm_disk.disk_perf(hrdw,
+                              destructive=args.benchmark_disk_destructive)
+
+    hrdw = detect_utils.clean_tuples(hrdw)
+
+    hrdw = list(filter(None, hrdw))
+
+    if args.human:
+        pprint.pprint(hrdw)
+    else:
+        print(json.dumps(hrdw))
