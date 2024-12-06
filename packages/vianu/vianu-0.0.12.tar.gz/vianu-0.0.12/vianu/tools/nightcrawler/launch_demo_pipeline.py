a276d00e727d4371afb84ade8db36ef08df40a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:27 +0000

    Add package vianu/0.0.12/vianu-0.0.12.tar.gz

---
+++ packages/vianu/vianu-0.0.12.tar.gz/vianu-0.0.12/vianu/tools/nightcrawler/launch_demo_pipeline.py
@@ -0,0 +1,42 @@
+import os
+import logging
+
+from vianu.tools.nightcrawler.src.client import NightcrawlerClient
+from dotenv import load_dotenv
+
+# Create a named logger
+logger = logging.getLogger("nightcrawler_logger")
+
+# Set the log level
+logger.setLevel(logging.INFO)
+
+# Define a custom formatter
+formatter = logging.Formatter(
+    fmt="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# Create a console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(console_handler)
+
+
+
+
+load_dotenv()
+
+# Instantiate the client
+nc_client = NightcrawlerClient()
+
+# Set API tokens
+nc_client.serpapi_token = os.getenv("SERP_API_TOKEN", "YOUR_SERPAPI_TOKEN")
+nc_client.zyte_api_key = os.getenv("ZYTE_API_TOKEN", "YOUR_ZYTE_API_KEY")
+
+# Perform search
+df = nc_client.search("sildenafil", num_results=5, location="Switzerland")
+
+# Display results
+print(df)
