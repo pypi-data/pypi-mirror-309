7abef0f9ce767fac1393dc037b84e3b09e6c6a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:16 +0000

    Add package Aitronos/2.0.0/Aitronos-2.0.0.tar.gz

---
+++ packages/Aitronos/Aitronos-2.0.0.tar.gz/Aitronos-2.0.0/README.md
@@ -0,0 +1,2 @@
+# Aitronos-python-package
+The freddy and other aitronos apis put in a neat package 
---
+++ packages/Aitronos/Aitronos-2.0.0.tar.gz/Aitronos-2.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Aitronos",
+    version="2.0.0",
+    packages=find_packages(),
+    install_requires=[],  # List your dependencies here
+    author="Phillip Loacker",
+    author_email="phillip.loacker@aitronos.com",
+    description="A Python package for interacting with the Freddy API",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Freddy-Development/aitronos-python-package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
