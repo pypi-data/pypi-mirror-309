92ef6ca943d9c37e7655963917f4ab2cfb398c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:19 +0000

    Add package aj-chalk/0.0.2/aj_chalk-0.0.2.tar.gz

---
+++ packages/aj-chalk/aj_chalk-0.0.2.tar.gz/aj_chalk-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aj_chalk",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="Akbar Jorayev",
+    author_email="akbarjonjorayev001@gmail.com",
+    url="https://github.com/akbarjorayev/aj-chalk-pkg",
+    keywords="chalk, python, color",
+    license="MIT"
+)
