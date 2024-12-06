f6cef94370603672484493253999744e10dfd8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:06 +0000

    Add package ovos-skill-days-in-history/0.3.7a1/ovos_skill_days_in_history-0.3.7a1-py3-none-any.whl

---
+++ packages/ovos-skill-days-in-history/ovos_skill_days_in_history-0.3.7a1-py3-none-any.whl/ovos_skill_days_in_history/__init__.py
@@ -0,0 +1,80 @@
+import os.path
+
+from ovos_date_parser import extract_datetime
+from ovos_utils import classproperty
+from ovos_utils.log import LOG
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_utils.time import now_local
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.intents import IntentBuilder
+from ovos_workshop.skills.ovos import OVOSSkill
+
+
+class TodayInHistory(OVOSSkill):
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(
+            internet_before_load=False,
+            network_before_load=False,
+            gui_before_load=False,
+            requires_internet=False,
+            requires_network=False,
+            requires_gui=False,
+            no_internet_fallback=True,
+            no_network_fallback=True,
+            no_gui_fallback=True,
+        )
+
+    def get_date(self, message):
+        utt = message.data.get("date") or message.data["utterance"]
+        try:
+            date = extract_datetime(utt, lang=self.lang)[0]
+        except Exception as e:
+            date = now_local()
+            if message.data.get("date"):
+                LOG.exception(f"failed to extract date in {self.lang} from {utt}")
+                # TODO - dedicated error dialog
+                #  this likely indicated a missing language in lingua-franca
+        return date
+
+    @intent_handler("deaths_in_history.intent")
+    def handle_deaths_intent(self, message):
+        date = self.get_date(message)
+        dialog = f"day_{date.day}_month_{date.month}_deaths"
+        event = f"{os.path.dirname(__file__)}/locale/{self.lang}/{dialog}.dialog"
+        if not os.path.isfile(event):
+            self.speak_dialog("unknown_date")
+            self.remove_context("prev_dialog")
+        else:
+            self.speak_dialog(dialog)
+            self.set_context("prev_dialog", dialog)
+
+    @intent_handler("births_in_history.intent")
+    def handle_births_intent(self, message):
+        date = self.get_date(message)
+        dialog = f"day_{date.day}_month_{date.month}_births"
+        self.speak_dialog(dialog)
+        self.set_context("prev_dialog", dialog)
+
+    @intent_handler("today_in_history.intent")
+    def handle_today_in_history_intent(self, message):
+        date = self.get_date(message)
+        dialog = f"day_{date.day}_month_{date.month}_events"
+        self.speak_dialog(dialog)
+        self.set_context("prev_dialog", dialog)
+
+    @intent_handler(IntentBuilder("TellMeMoreIntent").
+                    require("TellMeMore").
+                    require("prev_dialog"))
+    def handle_tell_me_more_intent(self, message):
+        """ Handler for follow-up inquiries 'tell me more'
+        enabled after initial response is complete
+        """
+        # TODO - add mechanism to avoid repeated responses
+        all_spoken = False
+        if all_spoken:
+            self.speak_dialog("thats_all")
+            self.remove_context("prev_dialog")
+        else:
+            dialog = message.data["prev_dialog"]
+            self.speak_dialog(dialog)
