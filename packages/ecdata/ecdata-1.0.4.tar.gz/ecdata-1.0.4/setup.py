7cb2fe79abde9fa58b9ee625be5cad58d0438a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:30 +0000

    Add package ecdata/1.0.4/ecdata-1.0.4.tar.gz

---
+++ packages/ecdata/ecdata-1.0.4.tar.gz/ecdata-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+req = ['polars==1.8.2',
+      'requests==2.31.0']
+
+
+setup(
+    name = 'ecdata',
+    version = '1.0.4',
+    description='a pip installable package to distribute the Executive Communications Dataset',
+    author = 'Joshua Allen',
+    author_email='joshua.f.allen@gmail.com',
+    license='CC-0',
+    packages = ['ecdata'],
+    url = 'https://github.com/Executive-Communications-Dataset/ecdata-py',
+    keywords=['Datasets'],
+    python_requires = '>= 3.10',
+    install_requires = req
+)
+
+
+
+
