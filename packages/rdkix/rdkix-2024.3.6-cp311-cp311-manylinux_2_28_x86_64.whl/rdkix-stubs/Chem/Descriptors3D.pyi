b0dd8094ead345f41b93e7832d615a2aa60640a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:50 +0000

    Add package rdkix/2024.3.6/rdkix-2024.3.6-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/rdkix/rdkix-2024.3.6-cp311-cp311-manylinux_2_28_x86_64.whl/rdkix-stubs/Chem/Descriptors3D.pyi
@@ -0,0 +1,29 @@
+"""
+ Descriptors derived from a molecule's 3D structure
+
+"""
+from __future__ import annotations
+from rdkix.Chem.Descriptors import _isCallable
+from rdkix.Chem import rdMolDescriptors
+__all__ = ['CalcMolDescriptors3D', 'descList', 'rdMolDescriptors']
+def CalcMolDescriptors3D(mol, confId = None):
+    """
+    
+        Compute all 3D descriptors of a molecule
+        
+        Arguments:
+        - mol: the molecule to work with
+        - confId: conformer ID to work with. If not specified the default (-1) is used
+        
+        Return:
+        
+        dict
+            A dictionary with decriptor names as keys and the descriptor values as values
+    
+        raises a ValueError 
+            If the molecule does not have conformers
+        
+    """
+def _setupDescriptors(namespace):
+    ...
+descList: list  # value = [('PMI1', <function <lambda> at 0x7f0b78557b00>), ('PMI2', <function <lambda> at 0x7f0b6a49ac00>), ('PMI3', <function <lambda> at 0x7f0b6a49ad40>), ('NPR1', <function <lambda> at 0x7f0b6a49ade0>), ('NPR2', <function <lambda> at 0x7f0b6a49ae80>), ('RadiusOfGyration', <function <lambda> at 0x7f0b6a49af20>), ('InertialShapeFactor', <function <lambda> at 0x7f0b6a49afc0>), ('Eccentricity', <function <lambda> at 0x7f0b6a49b060>), ('Asphericity', <function <lambda> at 0x7f0b6a49b100>), ('SpherocityIndex', <function <lambda> at 0x7f0b6a49b1a0>), ('PBF', <function <lambda> at 0x7f0b6a49b240>)]
