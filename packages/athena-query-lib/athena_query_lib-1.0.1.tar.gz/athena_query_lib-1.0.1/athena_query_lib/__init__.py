994418ee0576c7f01e4d0b02e1d1cbd690989ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:27 +0000

    Add package athena-query-lib/1.0.1/athena_query_lib-1.0.1.tar.gz

---
+++ packages/athena-query-lib/athena_query_lib-1.0.1.tar.gz/athena_query_lib-1.0.1/athena_query_lib/__init__.py
@@ -0,0 +1,11 @@
+# athena_query_lib/__init__.py
+
+from .query_handler import AthenaQueryHandler
+
+__all__ = ["AthenaQueryHandler"]
+
+__version__ = "1.0.0"
+__author__ = "Anyamanee P"
+__description__ = "A library for querying AWS Athena and processing results."
+
+
---
+++ packages/athena-query-lib/athena_query_lib-1.0.1.tar.gz/athena_query_lib-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="athena_query_lib",
+    version="1.0.1",
+    description="A library for querying AWS Athena and saving results.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Anyamanee P.",
+    author_email="anyamanee@techgateway.co.th",
+    url="https://github.com/AnyamaneePloy/athena-query-lib",
+    packages=find_packages(),
+    install_requires=[
+        "boto3",
+        "pandas",
+        "python-dotenv"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
