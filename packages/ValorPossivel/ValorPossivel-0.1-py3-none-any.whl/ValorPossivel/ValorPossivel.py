0f1d6cc7257e399c3368ae1c33d28a05ec42f3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:00 +0000

    Add package ValorPossivel/0.1/ValorPossivel-0.1-py3-none-any.whl

---
+++ packages/ValorPossivel/ValorPossivel-0.1-py3-none-any.whl/ValorPossivel/ValorPossivel.py
@@ -0,0 +1,20 @@
+# ValorPossivel.py
+# Biblioteca Teste de criação de API
+# Ela devolve um valor seguinte possível para a sequencia informada.
+
+
+def valorpossivel (listadevalores) :
+   """
+   Recebe uma lista de valores e devolve o proximo provavel valor para a lista.
+   Ex: 1,2,3,4,5 =>        6 - sequencia
+   2,4,6,8 =>             10 - pares
+   3,1,4,1,5,9,2 =>        6 - pi
+   A,B,C,D,E,F = >         G - alfabeto
+   or, ora, oral, orali => d - adição de letra a palavra (oralid-ade)
+   Returns:
+       valar : .
+   """
+   valor = listadevalores
+   return valor
+
+
