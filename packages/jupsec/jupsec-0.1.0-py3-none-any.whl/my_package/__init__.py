dace5ae569c6629acd9f50e696278d3097cf71f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:03:51 +0000

    Add package jupsec/0.1.0/jupsec-0.1.0-py3-none-any.whl

---
+++ packages/jupsec/jupsec-0.1.0-py3-none-any.whl/my_package/__init__.py
@@ -0,0 +1,13 @@
+from .moduel import exp1
+from .moduel import exp1ad
+from .moduel import exp2
+from .moduel import exp2ad
+from .moduel import exp3
+from .moduel import exp4
+from .moduel import exp5
+from .moduel import exp6
+from .moduel import exp7ad
+from .moduel import exp8
+from .moduel import exp9
+from .moduel import exp10
+
