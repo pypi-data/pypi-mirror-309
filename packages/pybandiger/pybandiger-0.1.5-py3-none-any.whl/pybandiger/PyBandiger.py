ab6cc93a09602092eab933cb7b63b82682ecab72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:09 +0000

    Add package pybandiger/0.1.5/pybandiger-0.1.5-py3-none-any.whl

---
+++ packages/pybandiger/pybandiger-0.1.5-py3-none-any.whl/pybandiger/PyBandiger.py
@@ -0,0 +1,32 @@
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+class PyBandiger:
+    def __init__(self):
+        self.le = {}
+        self.ss = StandardScaler()
+
+    def clean(self, data):
+        cat_col = data.select_dtypes(include='object').columns
+        num_col = data.select_dtypes(include=['int', 'float']).columns
+        for col in cat_col:
+            data[col] = data[col].fillna('Missing')
+        for col in num_col:
+            data[col] = data[col].fillna(data[col].mean())
+        return data
+
+    def EncodeAndScale_fit(self, data):
+        cat_col = data.select_dtypes(include='object').columns
+        num_col = data.select_dtypes(include=['int', 'float']).columns
+        for col in cat_col:
+            encoder = LabelEncoder()
+            self.le[col] = encoder.fit_transform(data[col])
+        data[num_col] = self.ss.fit_transform(data[num_col])
+        return data
+
+    def EncodeAndScale_transform(self, data):
+        cat_col = data.select_dtypes(include='object').columns
+        num_col = data.select_dtypes(include=['int', 'float']).columns
+        for col in cat_col:
+            data[col] = self.le[col].transform(data[col])
+        data[num_col] = self.ss.transform(data[num_col])
+        return data
