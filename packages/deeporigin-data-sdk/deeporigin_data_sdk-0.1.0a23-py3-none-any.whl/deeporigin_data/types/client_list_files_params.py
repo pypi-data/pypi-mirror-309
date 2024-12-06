333a0cb0d4e096c2ceedbdf53836e80363bbbe55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:42 +0000

    Add package deeporigin-data-sdk/0.1.0a23/deeporigin_data_sdk-0.1.0a23-py3-none-any.whl

---
+++ packages/deeporigin-data-sdk/deeporigin_data_sdk-0.1.0a23-py3-none-any.whl/deeporigin_data/types/client_list_files_params.py
@@ -0,0 +1,22 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Iterable
+from typing_extensions import Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["ClientListFilesParams", "Filter"]
+
+
+class ClientListFilesParams(TypedDict, total=False):
+    filters: Iterable[Filter]
+
+
+class Filter(TypedDict, total=False):
+    assigned_row_ids: Annotated[List[str], PropertyInfo(alias="assignedRowIds")]
+
+    file_ids: Annotated[List[str], PropertyInfo(alias="fileIds")]
+
+    is_unassigned: Annotated[bool, PropertyInfo(alias="isUnassigned")]
