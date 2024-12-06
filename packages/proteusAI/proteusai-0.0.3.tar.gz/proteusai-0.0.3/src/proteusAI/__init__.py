dcd0e07043a3dad67181cced372c84180897a168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:06:00 +0000

    Add package proteusAI/0.0.3/proteusai-0.0.3.tar.gz

---
+++ packages/proteusAI/proteusai-0.0.3.tar.gz/proteusai-0.0.3/src/proteusAI/__init__.py
@@ -0,0 +1,9 @@
+# This source code is part of the proteusAI package and is distributed
+# under the MIT License.
+
+
+from importlib import metadata
+
+__version__ = metadata.version("proteusAI")
+__name__ = "proteusAI"
+__author__ = "Jonathan Funk"
