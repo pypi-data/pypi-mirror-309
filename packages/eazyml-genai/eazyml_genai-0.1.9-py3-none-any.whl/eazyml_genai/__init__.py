bb975aa1da2cd9275b62bf6a515d50fa048da774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:14:05 +0000

    Add package eazyml-genai/0.1.9/eazyml_genai-0.1.9-py3-none-any.whl

---
+++ packages/eazyml-genai/eazyml_genai-0.1.9-py3-none-any.whl/eazyml_genai/__init__.py
@@ -0,0 +1,4 @@
+from . import compiledeazyml
+compiledeazyml.libeazymlgenaipython_cython_submodule()
+
+from eazyml_genai.genai.src.generative_ai.genai_main import upload_document_helper, get_information_helper
