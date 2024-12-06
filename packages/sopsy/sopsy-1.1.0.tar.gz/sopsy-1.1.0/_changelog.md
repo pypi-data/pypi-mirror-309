d52324c0833a0b9816d7249986b16d06fbc08277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:24:38 +0000

    Add package sopsy/1.1.0/sopsy-1.1.0.tar.gz

---
+++ packages/sopsy/sopsy-1.1.0.tar.gz/sopsy-1.1.0/_changelog.md
@@ -0,0 +1,9 @@
+## 1.1.0 (2024-11-18)
+
+### Feat
+
+- allow sops binary path customization (#75)
+
+[main 4e1f50c] bump: version 1.0.1 → 1.1.0
+ 2 files changed, 7 insertions(+), 1 deletion(-)
+
---
+++ packages/sopsy/sopsy-1.1.0.tar.gz/sopsy-1.1.0/src/sopsy/__init__.py
@@ -0,0 +1,22 @@
+"""SOPSy, a Python wrapper around SOPS.
+
+SOPS binary must be installed and available in your `$PATH`.
+"""
+
+from sopsy.errors import SopsyCommandFailedError
+from sopsy.errors import SopsyCommandNotFoundError
+from sopsy.errors import SopsyConfigNotFoundError
+from sopsy.errors import SopsyError
+from sopsy.errors import SopsyUnparsableOutpoutTypeError
+from sopsy.sopsy import Sops
+from sopsy.sopsy import SopsyInOutType
+
+__all__ = [
+    "SopsyCommandFailedError",
+    "SopsyCommandNotFoundError",
+    "SopsyConfigNotFoundError",
+    "SopsyError",
+    "SopsyUnparsableOutpoutTypeError",
+    "Sops",
+    "SopsyInOutType",
+]
