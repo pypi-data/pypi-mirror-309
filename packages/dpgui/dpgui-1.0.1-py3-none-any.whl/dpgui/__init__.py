473ec6f2f6c2f16a0b56f00f484430203aa39375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:11 +0000

    Add package dpgui/1.0.1/dpgui-1.0.1-py3-none-any.whl

---
+++ packages/dpgui/dpgui-1.0.1-py3-none-any.whl/dpgui/__init__.py
@@ -0,0 +1,15 @@
+"""DP-GUI Python package."""
+
+import logging
+
+from .dargs import generate_input_json as generate_dpgui_templates
+from .web import run as start_dpgui
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+__all__ = ["start_dpgui", "generate_dpgui_templates"]
