a9d8f4c291887a71c38fb42d114fd57ea45286f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:51 +0000

    Add package pyccl/3.1.2/pyccl-3.1.2.tar.gz

---
+++ packages/pyccl/pyccl-3.1.2.tar.gz/pyccl-3.1.2/pyccl/errors.py
@@ -0,0 +1,47 @@
+__all__ = ("CCLError", "CCLWarning", "CCLDeprecationWarning",)
+
+import warnings
+
+
+class CCLError(RuntimeError):
+    """A CCL-specific RuntimeError"""
+    def __repr__(self):
+        return "pyccl.CCLError(%r)" % (str(self))
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
+
+    def __hash__(self):
+        return hash(repr(self))
+
+
+class CCLWarning(RuntimeWarning):
+    """A CCL-specific warning"""
+    def __repr__(self):
+        return "pyccl.CCLWarning(%r)" % (str(self))
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
+
+    def __hash__(self):
+        return hash(repr(self))
+
+
+class CCLDeprecationWarning(DeprecationWarning):
+    """A CCL-specific deprecation warning."""
+    def __repr__(self):
+        return "pyccl.CCLDeprecationWarning(%r)" % (str(self))
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    @classmethod
+    def enable(cls):
+        warnings.simplefilter("always")
+
+    @classmethod
+    def disable(cls):
+        warnings.filterwarnings(action="ignore", category=cls)
