8f8253721177d2b2290aa98b0431c2564a4d4502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:02 +0000

    Add package pyluxcoretools/2.9a1.post1/pyluxcoretools-2.9a1.post1-py3-none-any.whl

---
+++ packages/pyluxcoretools/pyluxcoretools-2.9a1.post1-py3-none-any.whl/src/pyluxcoremaketx.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2024 Howetuft
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+import pyluxcoretools.pyluxcoremaketx.cmd
+
+def main():
+    argv = sys.argv
+    pyluxcoretools.pyluxcoremaketx.cmd.main(argv)
---
+++ packages/pyluxcoretools/pyluxcoretools-2.9a1.post1-py3-none-any.whl/src/pyluxcoremerge.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2024 Howetuft
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+import pyluxcoretools.pyluxcoremerge.cmd
+
+def main():
+    argv = sys.argv
+    pyluxcoretools.pyluxcoremerge.cmd.main(argv)
