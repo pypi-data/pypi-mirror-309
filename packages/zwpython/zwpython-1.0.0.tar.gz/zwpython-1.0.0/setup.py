51c383243a932caf6a74e70f0f6d6ed0383cc65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:18 +0000

    Add package zwpython/1.0.0/zwpython-1.0.0.tar.gz

---
+++ packages/zwpython/zwpython-1.0.0.tar.gz/zwpython-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='zwpython',
+    version='1.0.0',
+    description='用中文写python代码',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='52zwbc',
+    author_email='sdnygls@126.com',
+    url='https://www.52zwbc.com/',
+    install_requires=[],
+    license='MIT License',
+    packages=find_packages(),
+    platforms=["all"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
+# python setup.py sdist bdist_wheel
