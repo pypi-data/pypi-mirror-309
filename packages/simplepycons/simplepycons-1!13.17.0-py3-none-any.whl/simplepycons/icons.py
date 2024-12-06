044c2f7292537d61bba280838b95be4e18c992cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:41 +0000

    Add package simplepycons/1!13.17.0/simplepycons-1!13.17.0-py3-none-any.whl

---
+++ packages/simplepycons/simplepycons-1!13.17.0-py3-none-any.whl/simplepycons/icons.py
@@ -0,0 +1,34 @@
+
+from types import SimpleNamespace
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .base_icon import Icon
+
+
+class IconFactory:
+    def __init__(self, factory: "type[Icon]") -> None:
+        self._factory = factory
+        self._prototype = factory()
+
+    @property
+    def prototype(self) -> "Icon":
+        return self._prototype
+
+    def __call__(self) -> "Icon":
+        return self._factory()
+
+
+class IconCollection(SimpleNamespace):
+    def __init__(self, all_icons: "dict[str, type[Icon]]") -> None:
+        super().__init__()
+        self.__dict__.update(IconCollection.__from_all_icons(all_icons))
+
+    def __getitem__(self, name: str) -> "IconFactory":
+        return IconFactory(self.__dict__[f"get_{name}_icon"])()
+
+    @staticmethod
+    def __from_all_icons(
+            all_icons: "dict[str, type[Icon]]"
+    ) -> "dict[str, IconFactory]":
+        return {f"get_{k}_icon": IconFactory(v) for k, v in all_icons.items()}
