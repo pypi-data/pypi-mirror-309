aa5b3bc6a27c002c1847ea8cd53dde04ca3c6269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:52 +0000

    Add package properunits/0.0.3/properunits-0.0.3-py3-none-any.whl

---
+++ packages/properunits/properunits-0.0.3-py3-none-any.whl/properunits/length.py
@@ -0,0 +1,99 @@
+from .base import Magnitude
+
+_length_names = {
+    'm' : ['meter'],
+    'nm' : ['nanometers'],
+    'A' : ['Angstroms'],
+    'in' : ['inch', 'inches'],
+    'mi' : ['mile', 'miles'],
+    'yd' : ['yard', 'yards']
+}
+
+_length_conv = {
+    'nm' : lambda x: 1e-9*x,
+    'A' : lambda x: 1e-10*x,
+    'in' : lambda x: 2.54e-2*x,
+    'mi' : lambda x: 1609.34*x,
+    'yd' : lambda x: 0.9144*x,
+}
+
+_area_names = {
+    'm2' : ['m^2'],
+    'nm2' : ['nm^2'],
+    'A2' : ['A^2'],
+    'ha' : ['hectarea']
+}
+
+_area_conv = {
+    'nm2' : lambda x: 1e-18*x,
+    'A2' : lambda x: 1e-20*x,
+    'ha' : lambda x: 1e4*x
+}
+
+_volume_names = {
+    'l' : ['liter', 'liters'],
+    'cm3' : ['cm^3'],
+    'm3' : ['m^3']
+}
+
+_volume_conv = {
+    'l' : lambda x: 1e-6*x,
+    'cm3' : lambda x: 1e-6*x,
+}
+
+
+class Length(Magnitude):
+
+    _units = 'm'
+
+    def _convert(self, val, units):
+        key = self._check_units(units, _length_names)
+        if key == Length._units:
+            self._x = val
+        else:
+            self._x = _length_conv[key](val)
+
+    @property
+    def units(self):
+        return Length._units
+
+    def list_units():
+        return list(_length_names.keys())
+
+
+class Area(Magnitude):
+
+    _units = 'm2'
+
+    def _convert(self, val, units):
+        key = self._check_units(units, _area_names)
+        if key == Area._units:
+            self._x = val
+        else:
+            self._x = _area_conv[key](val)
+
+    @property
+    def units(self):
+        return Area._units
+
+    def list_units():
+        return list(_area_names.keys())
+
+
+class Volume(Magnitude):
+
+    _units = 'm3'
+
+    def _convert(self, val, units):
+        key = self._check_units(units, _volume_names)
+        if key == Volume._units:
+            self._x = val
+        else:
+            self._x = _volume_conv[key](val)
+
+    @property
+    def units(self):
+        return Volume._units
+
+    def list_units():
+        return list(_volume_names.keys())
