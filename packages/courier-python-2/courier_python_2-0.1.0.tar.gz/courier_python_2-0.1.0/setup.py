0be64b210bc885d5014c8e5c0a99408933a74118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:24 +0000

    Add package courier-python-2/0.1.0/courier_python_2-0.1.0.tar.gz

---
+++ packages/courier-python-2/courier_python_2-0.1.0.tar.gz/courier_python_2-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="courier-python-2",
+    version="0.1.0",
+    author="Fernando Basurto Echevarria",
+    author_email="fernando.basurto.echevarria@gmail.com",
+    description="Yet Another Math Library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/fbasurto/yaml",  # Replace with your repo
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
