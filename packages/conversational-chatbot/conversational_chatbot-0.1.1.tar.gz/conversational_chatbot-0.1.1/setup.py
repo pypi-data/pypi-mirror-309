41bcc23aeae7bcdd720b30458baa276f64a1d863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:06:15 +0000

    Add package conversational-chatbot/0.1.1/conversational_chatbot-0.1.1.tar.gz

---
+++ packages/conversational-chatbot/conversational_chatbot-0.1.1.tar.gz/conversational_chatbot-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="conversational_chatbot",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="Conversational chatbot",
+    author="Abdujalil",
+    author_email="achuliev@wiut.uz",
+    url="https://github.com/Abdujalilc/python_ai_basics.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
