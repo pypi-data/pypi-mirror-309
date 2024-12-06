4f342133872cbd20f0f3f6daca60f2df11667161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:55 +0000

    Add package datalakesdk/0.0.3/datalakesdk-0.0.3.tar.gz

---
+++ packages/datalakesdk/datalakesdk-0.0.3.tar.gz/datalakesdk-0.0.3/datalakesdk/types.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+class Resolution(BaseModel):
+    width: int
+    height: int
+
+class FileMeta(BaseModel):
+    resolution: Resolution
+    tokenLength: int
+    duration: int
+
+class UploadFileResponse(BaseModel):
+    url: str
+    object_name: str
+    uid: str
+    meta: FileMeta
+
+class UploadRawDataResponse(BaseModel):
+    raw_data_id: str
+
+class UploadAnnotationDataResponse(BaseModel):
+    annotation_data_id: str
+
+class CreateDataSetResponse(BaseModel):
+    dataset_id: str
+
+class SummaryAndDownloadDataSetResponse(BaseModel):
+    url: str
+    bucket: str
+    object_name: str
