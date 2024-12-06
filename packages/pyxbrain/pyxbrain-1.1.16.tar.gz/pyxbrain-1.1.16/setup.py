868fcb51d597eab1cb7b42f2d30aba8f32930aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:22 +0000

    Add package pyxbrain/1.1.16/pyxbrain-1.1.16.tar.gz

---
+++ packages/pyxbrain/pyxbrain-1.1.16.tar.gz/pyxbrain-1.1.16/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from xbrain.utils.config import Constants
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyxbrain',
+    version= Constants().VERSION,
+    description='xbrain专注于构建一款可解释的AI开发框架',
+    long_description=long_description, 
+    long_description_content_type="text/markdown",  
+    packages=['xbrain', 'xbrain.utils', 'xbrain.plugin', 'xbrain.core'],
+    zip_safe=False,
+    install_requires=[
+        'pyyaml==6.0.2',
+        'openai==1.48.0',
+        'requests==2.32.3',
+        'flask==3.0.3',
+        'flask-openapi3==3.1.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'xb = xbrain.main:main',
+        ],
+    },
+)
