a90f8c1eb65316028b9e24a90446ddce2c8aed4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:08:56 +0000

    Add package gintro/0.1.15/gintro-0.1.15.tar.gz

---
+++ packages/gintro/gintro-0.1.15.tar.gz/gintro-0.1.15/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gintro',
+    version='0.1.15',
+    author='jfshen',
+    packages=find_packages(),
+    install_requires=[
+        # 依赖列表
+    ],
+    # 其他元数据
+)
