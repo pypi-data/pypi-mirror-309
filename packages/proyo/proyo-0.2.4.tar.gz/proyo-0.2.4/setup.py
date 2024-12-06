8f530a9c9abccab2f1a1b09ce4a54aa2fe2f4303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:21 +0000

    Add package proyo/0.2.4/proyo-0.2.4.tar.gz

---
+++ packages/proyo/proyo-0.2.4.tar.gz/proyo-0.2.4/setup.py
@@ -0,0 +1,40 @@
+import os
+from os.path import join, dirname, abspath
+from setuptools import setup
+
+package_data = [
+    join(root, "*")
+    for folder in ["templates/", "macros/"]
+    for root, dirnames, filenames in os.walk(
+        join(dirname(abspath(__file__)), "proyo", folder)
+    )
+]
+
+setup(
+    name="proyo",
+    version="0.2.4",
+    description="A tool to broadcast notifications across various interfaces",
+    url="https://github.com/matthewscholefield/proyo",
+    author="Matthew Scholefield",
+    author_email="matthew331199@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="notify server",
+    packages=["proyo"],
+    install_requires=["setuptools"],
+    entry_points={
+        "console_scripts": [
+            "proyo=proyo.__main__:main",
+        ],
+    },
+    package_data={"proyo": package_data},
+)
