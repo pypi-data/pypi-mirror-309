6a72c1c4a5de98619184e8f9f55702fb06ab1ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:55 +0000

    Add package fastapy/1.0.5/fastapy-1.0.5.tar.gz

---
+++ packages/fastapy/fastapy-1.0.5.tar.gz/fastapy-1.0.5/README.md
@@ -0,0 +1,147 @@
+![PyPI - Version](https://img.shields.io/pypi/v/fastapy?label=version&color=blue)
+![tests workflow](https://github.com/aziele/fastapy/actions/workflows/run-tests.yml/badge.svg)
+![OS](https://img.shields.io/badge/OS-Linux%20MacOS%20Windows-7373e3)
+[![DOI](https://zenodo.org/badge/522251113.svg)](https://zenodo.org/doi/10.5281/zenodo.10462088)
+
+# fastapy
+A lightweight Python package to read and write sequence records in [FASTA format](https://en.wikipedia.org/wiki/FASTA_format).
+
+The design was inspired by the utility of BioPython’s SeqIO, which supports many sequence formats. This repo focuses only on FASTA records. It is faster than BioPython, can handle compressed FASTA files (gzip, bzip2, zip), and has no Python package dependencies.
+
+## Requirements
+Python >= 3.8
+
+## Installation
+
+You can install `fastapy` from [PyPI](https://pypi.org/project/fastapy/):
+
+```bash
+pip install fastapy
+```
+
+or directly from GitHub:
+
+```bash
+pip install "git+https://github.com/aziele/fastapy.git"
+```
+
+You can also use `fastapy` without installation since it doesn't have any dependencies. Simply clone or download this repository and you're ready to use it.
+
+```bash
+git clone https://github.com/aziele/fastapy.git
+cd fastapy
+python
+>>> import fastapy
+>>> fastapy.__doc__
+'A lightweight Python module to read and write FASTA sequence records'
+```
+
+## Quick Start
+Typical usage is to read a FASTA file and loop over the sequences record(s).
+
+```python
+import fastapy
+
+for record in fastapy.parse('tests/test.fasta'):
+    print(record.id, len(record), record.seq[:10], record.desc)
+```
+
+Output:
+
+```
+NP_002433.1  362   METDAPQPGL   RNA-binding protein Musashi homolog 1 [Homo sapiens]
+ENO94161.1    79   MKLLISGLGP   RRM domain-containing RNA-binding protein
+sequence     292   MKLSKIALMM
+```
+
+## Usage
+This module contains the `Record` class representing a FASTA sequence record and the `parse()` function to read FASTA records from a file.
+
+### Record object
+Record is an object that contains information on a FASTA sequence record, including id, description, and the sequence itself.
+
+```python
+import fastapy
+
+record = fastapy.Record(
+    id='NP_950171.2', 
+    seq='MEEEAETEEQQRFSYQQRLKAAVHYTVGCLCEEVALDKEMQFSKQTIAAISELTFRQCENFAKDLEMFASICRKRQE',
+    desc='APITD1-CORT protein isoform 2 [Homo sapiens]'
+)
+
+print(record.id)            # NP_950171.2
+print(record.desc)          # APITD1-CORT protein isoform 2 [Homo sapiens]
+print(record.seq)           # MEEEAE..
+print(record.description)   # >NP_950171.2 G APITD1-CORT protein isoform 2 [Homo sapiens]
+print(len(record))          # 77
+print('EEEA' in record)     # True
+```
+
+By default, the sequence line is wrapped to 70 characters. You can provide the line length. Use zero (or None) for no wrapping.
+
+```python
+print(record)
+# >NP_950171.2 APITD1-CORT protein isoform 2 [Homo sapiens]
+# MEEEAETEEQQRFSYQQRLKAAVHYTVGCLCEEVALDKEMQFSKQTIAAISELTFRQCENFAKDLEMFAS
+# ICRKRQE
+
+print(record.format(wrap=30))
+# >NP_001382951.1 G protein subunit gamma 5 [Homo sapiens]
+# MEEEAETEEQQRFSYQQRLKAAVHYTVGCL
+# CEEVALDKEMQFSKQTIAAISELTFRQCEN
+# FAKDLEMFASICRKRQE
+
+print(record.format(wrap=None))
+# >NP_950171.2 APITD1-CORT protein isoform 2 [Homo sapiens]
+# MEEEAETEEQQRFSYQQRLKAAVHYTVGCLCEEVALDKEMQFSKQTIAAISELTFRQCENFAKDLEMFASICRKRQE
+```
+
+### parse
+The `parse()` function is a generator to read FASTA records as `Record` objects one by one from a file (plain FASTA or compressed using gzip or bzip2). Because only one record is created at a time, very little memory is required.
+
+```python
+import fastapy
+
+for record in fastapy.parse('tests/test.fasta.gz'):
+    print(record.id)
+```
+
+For some tasks you may need to have a reusable access to the records. For this purpose, you can use the built-in Python `list()` function to turn the iterator into a list:
+
+```python
+import fastapy
+
+records = list(fastapy.parse('tests/test.fasta.gz'))
+print(records[0].id)   # First record
+print(records[-1].id)  # Last record
+```
+
+Another common task is to index your records by sequence identifier. Use `to_dict()` to turn a Record iterator (or list) into a dictionary.
+
+```python
+import fastapy
+
+records = fastapy.to_dict(fasta.parse('tests/test.fasta.gz'))
+print(records['NP_002433.1'])   # Use any record id
+```
+
+### read
+The `read()` function reads only the first FASTA record from a file. It does not read any subsequent records in the file.
+
+```python
+import fastapy
+
+seq_record = fastapy.read('tests/test.fasta')
+print(seq_record.id)           # NP_002433.1
+```
+
+## Test
+You can run tests to ensure that the module works as expected.
+
+```
+python -m unittest discover
+```
+
+## License
+
+[GNU General Public License, version 3](https://www.gnu.org/licenses/gpl-3.0.html)
---
+++ packages/fastapy/fastapy-1.0.5.tar.gz/fastapy-1.0.5/tests/test_fastapy.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+
+import pathlib
+import unittest
+
+import fastapy as fp
+
+TEST_DIR = pathlib.Path(__file__).resolve().parent
+
+class TestFasta(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.filename = TEST_DIR / 'test.fasta'
+
+    def test_record_id(self):
+        lst = ['NP_002433.1', 'ENO94161.1', 'sequence']
+        for i, record in enumerate(fp.parse(self.filename)):
+            self.assertEqual(record.id, lst[i])
+
+    def test_record_len(self):
+        lst = [362, 79, 292]
+        for i, record in enumerate(fp.parse(self.filename)):
+            self.assertEqual(len(record), lst[i])
+
+    def test_record_desc(self):
+        lst = [
+            'RNA-binding protein Musashi homolog 1 [Homo sapiens]',
+            'RRM domain-containing RNA-binding protein',
+            ''
+        ]
+        for i, record in enumerate(fp.parse(self.filename)):
+            self.assertEqual(record.desc, lst[i])
+
+    def test_record_description(self):
+        lst = [
+            '>NP_002433.1 RNA-binding protein Musashi homolog 1 [Homo sapiens]',
+            '>ENO94161.1 RRM domain-containing RNA-binding protein',
+            '>sequence'
+        ]
+        for i, record in enumerate(fp.parse(self.filename)):
+            self.assertEqual(record.description, lst[i])
+
+    def test_record_iter(self):
+        lst = [list('METDA'), list('MKLLI'), list('MKLSK')]
+        for i, record in enumerate(fp.parse(self.filename)):
+            self.assertEqual(list(record)[:5], lst[i])
+
+    def test_record_in(self):
+        lst = ['METDA', 'MKLLI', 'MKLSK']
+        for i, record in enumerate(fp.parse(self.filename)):
+            self.assertTrue(lst[i] in record)   
+
+    def test_record_format(self):
+        lst = [
+          # First sequence record
+          ('>NP_002433.1 RNA-binding protein Musashi homolog 1 [Homo sapiens]\n',
+           'METDAPQPGLASPDSPHDPCKMFIGGLSWQTTQEGLREYFGQFGEVKECLVMRDPLTKRS\n',
+           'RGFGFVTFMDQAGVDKVLAQSRHELDSKTIDPKVAFPRRAQPKMVTRTKKIFVGGLSVNT\n',
+           'TVEDVKQYFEQFGKVDDAMLMFDKTTNRHRGFGFVTFESEDIVEKVCEIHFHEINNKMVE\n',
+           'CKKAQPKEVMSPTGSARGRSRVMPYGMDAFMLGIGMLGYPGFQATTYASRSYTGLAPGYT\n',
+           'YQFPEFRVERTPLPSAPVLPELTAIPLTAYGPMAAAAAAAAVVRGTGSHPWTMAPPPGST\n',
+           'PSRTGGFLGTTSPGPMAELYGAANQDSGVSSYISAASPAPSTGFGHSLGGPLIATAFTNG\n',
+           'YH\n'),
+          # Second sequence record
+          ('>ENO94161.1 RRM domain-containing RNA-binding protein\n',
+           'MKLLISGLGPDTDLDTLRERMRHFGPVLDILVVREGDPERPWFIIDMDITPDVATEVARR\n',
+           'IDGIYFHGSFVHARVMLHD\n'),
+          # Thir sequence record
+          ('>sequence\n',
+           'MKLSKIALMMATLAASSAAWSHGYIEVPESRAYKCKLGSNTDCGRAQWEPQSVEQVSGFP\n',
+           'GGATPLDGQLASGGVNGFESLDRQGVNVWALNTMKPGPQTFTWYHTAKHKTNNWRYYITK\n',
+           'QDWDVNKPLSREAFEKEPFCEIDGHAKPPKDREVHQCVVPERTGYQVIYGVWEDASQPLT\n',
+           'MALSNVSEGHHMLKVIASNDNGQSIQPDIENFNLEAESTGGGGGNGDYNFVFPNALSKYT\n',
+           'AGTTVLQPKDGKVYQCKPFPYSGYCMQWNSGATHFEPGVGSNWQDAWILKK*\n')
+        ]
+        for i, record in enumerate(fp.parse(self.filename)):
+            self.assertEqual(record.format(wrap=60), "".join(lst[i]))
+
+    def test_get_compression_type_plain(self):
+        self.assertIsNone(fp.get_compression_type(self.filename))
+
+    def test_get_compression_type_gz(self):
+        file_type = fp.get_compression_type(TEST_DIR / 'test.fasta.gz')
+        self.assertEqual(file_type, 'gz')
+
+    def test_get_compression_type_bz2(self):
+        file_type = fp.get_compression_type(TEST_DIR / 'test.fasta.bz2')
+        self.assertEqual(file_type, 'bz2')
+
+    def test_get_compression_type_zip(self):
+        file_type = fp.get_compression_type(TEST_DIR / 'test.fasta.zip')
+        self.assertEqual(file_type, 'zip')
+
+    def test_parse_fasta_file(self):
+        lst = [r.id for r in fp.parse(TEST_DIR / 'test.fasta.gz')]
+        self.assertEqual(len(lst), 3)
+        self.assertEqual(set(lst), {'NP_002433.1', 'ENO94161.1', 'sequence'})
+
+    def test_parse_empty_file(self):
+        lst = [rec for rec in fp.parse(TEST_DIR / 'empty_file.fasta')]
+        self.assertEqual(len(lst), 0)
+
+    def test_parse_missing_file(self):
+        with self.assertRaises(FileNotFoundError):
+            list(fp.parse("non_existent_file.fasta"))
+
+    def test_parse_gz_file(self):
+        record = list(fp.parse(TEST_DIR / 'test.fasta.gz'))[0]
+        self.assertEqual(record.id, 'NP_002433.1')
+        self.assertEqual(len(record), 362)
+
+    def test_parse_bz2_file(self):
+        record = list(fp.parse(TEST_DIR / 'test.fasta.bz2'))[0]
+        self.assertEqual(record.id, 'NP_002433.1')
+        self.assertEqual(len(record), 362)
+
+    def test_parse_zip_file(self):
+        record = list(fp.parse(TEST_DIR / 'test.fasta.zip'))[0]
+        self.assertEqual(record.id, 'NP_002433.1')
+        self.assertEqual(len(record), 362)
+
+    def test_read(self):
+        record = fp.read(self.filename)
+        self.assertEqual(record.id, 'NP_002433.1')
+        self.assertEqual(len(record), 362)  
+
+    def test_read_gz_file(self):
+        record = fp.read(TEST_DIR / 'test.fasta.gz')
+        self.assertEqual(record.id, 'NP_002433.1')
+        self.assertEqual(len(record), 362)        
+
+    def test_read_zip_file(self):
+        record = fp.read(TEST_DIR / 'test.fasta.gz')
+        self.assertEqual(record.id, 'NP_002433.1')
+        self.assertEqual(len(record), 362)  
+
+    def test_read_bz2_file(self):
+        record = fp.read(TEST_DIR / 'test.fasta.bz2')
+        self.assertEqual(record.id, 'NP_002433.1')
+        self.assertEqual(len(record), 362)  
+
+    def test_to_dict(self):
+        d = fp.to_dict(fp.parse(self.filename))
+        self.assertEqual(len(d), 3)
+        self.assertEqual(len(d['ENO94161.1']), 79)
+
+    def test_to_dict_duplicate_records(self):
+        records = [
+          fp.Record(id='id1', seq='ATGC'),
+          fp.Record(id='id2', seq='CGTA'),
+          fp.Record(id='id1', seq='ATGC'),
+        ]
+        with self.assertRaises(ValueError):
+            fp.to_dict(records)
+
+    def test_to_dict_empty_records(self):
+        self.assertEqual(fp.to_dict([]), {})
+
+if __name__ == '__main__':
+    unittest.main()
