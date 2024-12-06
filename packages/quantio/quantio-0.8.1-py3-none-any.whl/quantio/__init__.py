d645d7f96b8499a5f59555a2f4a3c194ac80fe6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:06:16 +0000

    Add package quantio/0.8.1/quantio-0.8.1-py3-none-any.whl

---
+++ packages/quantio/quantio-0.8.1-py3-none-any.whl/quantio/__init__.py
@@ -0,0 +1,21 @@
+"""The main quantio package."""
+
+from .exceptions import CanNotAddTypesError, CanNotSubtractTypesError, NoUnitSpecifiedError
+from .quantities import Acceleration, Angle, Area, Frequency, Length, Mass, Power, Time, Velocity
+from .vector import Vector
+
+__all__ = [
+    "Acceleration",
+    "Angle",
+    "Area",
+    "Frequency",
+    "Length",
+    "Mass",
+    "Power",
+    "Time",
+    "Velocity",
+    "Vector",
+    "CanNotAddTypesError",
+    "CanNotSubtractTypesError",
+    "NoUnitSpecifiedError",
+]
