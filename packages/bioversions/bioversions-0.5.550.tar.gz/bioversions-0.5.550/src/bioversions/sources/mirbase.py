fdbae44738ecf2305b190449f3f4287956c8bfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:00 +0000

    Add package bioversions/0.5.550/bioversions-0.5.550.tar.gz

---
+++ packages/bioversions/bioversions-0.5.550.tar.gz/bioversions-0.5.550/src/bioversions/sources/mirbase.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""A getter for miRBase."""
+
+from bioversions.utils import Getter, VersionType
+
+__all__ = [
+    "MirbaseGetter",
+]
+
+PREFIX = "0_THIS_IS_RELEASE_"
+
+
+class MirbaseGetter(Getter):
+    """A getter for miRBase."""
+
+    bioregistry_id = "mirbase"
+    name = "miRBase"
+    homepage_fmt = "https://www.mirbase.org/download/PREVIOUS_RELEASES/{version}"
+    version_type = VersionType.semver_minor
+    collection = ["mirbase", "mirbase.family", "mirbase.mature"]
+
+    def get(self):
+        """Get the latest miRBase version number."""
+        return "22.1"
+        # Old code, doesn't work anymore because miRBase site deleted most of their FTP server and downloads
+        # url = "http://www.mirbase.org/download_readme/"
+        # soup = get_soup(url, verify=False)
+        # return soup.find("p").text.splitlines()[0].split()[-1]
+
+
+if __name__ == "__main__":
+    MirbaseGetter.print()
