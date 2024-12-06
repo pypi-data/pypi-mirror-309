97662efa635675298c784d05ffb8a2ed501bcc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:28 +0000

    Add package jlnester/1.0.0/jlnester-1.0.0.tar.gz

---
+++ packages/jlnester/jlnester-1.0.0.tar.gz/jlnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'jlnester',
+        version      = '1.0.0',
+        py_modules   = ['jlnester'],
+        author       = 'JiangLe',
+        author_email = '2773394158@qq.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+)
