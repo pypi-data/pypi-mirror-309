b291906e6c30a4202ac26647b72b78f9910db897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:44 +0000

    Add package langgraph-api-inmem/0.0.2rc1/langgraph_api_inmem-0.0.2rc1.tar.gz

---
+++ packages/langgraph-api-inmem/langgraph_api_inmem-0.0.2rc1.tar.gz/langgraph_api_inmem-0.0.2rc1/langgraph_api/base/api/openapi.py
@@ -0,0 +1,32 @@
+from functools import lru_cache
+
+import orjson
+
+from langgraph_api.base.config import LANGGRAPH_AUTH_TYPE
+from langgraph_api.base.shared.graph import GRAPHS
+from langgraph_api.base.shared.validation import openapi
+
+
+@lru_cache(maxsize=1)
+def get_openapi_spec() -> str:
+    # patch the graph_id enums
+    graph_ids = list(GRAPHS.keys())
+    for schema in (
+        "Assistant",
+        "AssistantCreate",
+        "AssistantPatch",
+        "GraphSchema",
+        "AssistantSearchRequest",
+    ):
+        openapi["components"]["schemas"][schema]["properties"]["graph_id"][
+            "enum"
+        ] = graph_ids
+    # patch the auth schemes
+    if LANGGRAPH_AUTH_TYPE == "langsmith":
+        openapi["security"] = [
+            {"x-api-key": []},
+        ]
+        openapi["components"]["securitySchemes"] = {
+            "x-api-key": {"type": "apiKey", "in": "header", "name": "x-api-key"}
+        }
+    return orjson.dumps(openapi)
