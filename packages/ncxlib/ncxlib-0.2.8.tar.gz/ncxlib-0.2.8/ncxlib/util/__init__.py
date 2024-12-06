7a99f60db996797953a36c83152dd82cafed2923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:10 +0000

    Add package ncxlib/0.2.8/ncxlib-0.2.8.tar.gz

---
+++ packages/ncxlib/ncxlib-0.2.8.tar.gz/ncxlib-0.2.8/ncxlib/util/__init__.py
@@ -0,0 +1,18 @@
+from .logs import (
+LOG,
+log,
+)
+from .preloaded import (
+CACHE_DIR,
+download,
+load_data,
+)
+from .splitter import (
+k_fold_cross_validation,
+train_test_split,
+)
+from .timer import (
+show_time,
+time_this,
+timer,
+)
