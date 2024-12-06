aee4a7f671a9985b2d81d653078c9eb34372debd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:16:57 +0000

    Add package odoo14-addon-ssi-rma-sale/14.0.1.0.1.dev1/odoo14_addon_ssi_rma_sale-14.0.1.0.1.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-rma-sale/odoo14_addon_ssi_rma_sale-14.0.1.0.1.dev1-py3-none-any.whl/odoo/addons/ssi_rma_sale/models/rma_customer_line.py
@@ -0,0 +1,48 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class RMACustomerLine(models.Model):
+    _name = "rma_customer_line"
+    _inherit = [
+        "rma_line_mixin",
+        "rma_customer_line",
+    ]
+
+    sale_line_id = fields.Many2one(
+        related="source_stock_move_id.sale_line_id",
+        store=False,
+    )
+
+    def _get_receipt_procurement_data(self):
+        _super = super(RMACustomerLine, self)
+        result = _super._get_receipt_procurement_data()
+        sale_line_id = self.sale_line_id and self.sale_line_id.id or False
+        to_refund = sale_line_id and True or False
+        result.update(
+            {
+                "sale_line_id": sale_line_id,
+                "to_refund": to_refund,
+            }
+        )
+        return result
+
+    def _get_delivery_procurement_data(self):
+        _super = super(RMACustomerLine, self)
+        result = _super._get_delivery_procurement_data()
+        sale_line_id = self.sale_line_id and self.sale_line_id.id or False
+        source_stock_move_id = (
+            self.source_stock_move_id and self.source_stock_move_id.id or False
+        )
+        to_refund = sale_line_id and True or False
+        result.update(
+            {
+                "sale_line_id": sale_line_id,
+                "origin_returned_move_id": source_stock_move_id,
+                "to_refund": to_refund,
+            }
+        )
+        return result
