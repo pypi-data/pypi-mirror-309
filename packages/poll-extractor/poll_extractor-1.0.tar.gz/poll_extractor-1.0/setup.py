b741f1501c80ca26eac14a50dcb2f60a0396124d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:17 +0000

    Add package poll-extractor/1.0/poll_extractor-1.0.tar.gz

---
+++ packages/poll-extractor/poll_extractor-1.0.tar.gz/poll_extractor-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+token = 'pypi-AgEIcHlwaS5vcmcCJDgzYzg4ZGMzLTY3MmMtNDMxMy04MjUxLTExMTgxY2E3Y2I3NwACKlszLCJkOWYzYTIwZi01YjE4LTQ4NGUtYWVjZS0yNjBmZWM5NzVhYjIiXQAABiB60YYu5UsUtuMn4mNmlDay4149VyugqP6XxhhZrVAHTg'
+setup(name='poll_extractor',
+      version='1.0',
+      description='A light python library that helps to make charts',
+      packages=['poll_extractor'],
+      author_email='andreyfakerare@gmail.com',
+      zip_safe=False)
