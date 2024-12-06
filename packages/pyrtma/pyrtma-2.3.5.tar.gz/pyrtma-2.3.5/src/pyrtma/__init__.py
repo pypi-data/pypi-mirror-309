c1cfb6f8d7c5eed2ddb28f84448b03e93ecdbce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:16 +0000

    Add package pyrtma/2.3.5/pyrtma-2.3.5.tar.gz

---
+++ packages/pyrtma/pyrtma-2.3.5.tar.gz/pyrtma-2.3.5/src/pyrtma/__init__.py
@@ -0,0 +1,14 @@
+from .message import *
+from .client import *
+from .client_logging import RTMALogger
+
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
