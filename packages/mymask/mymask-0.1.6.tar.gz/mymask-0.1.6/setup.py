bb781d9ca5c880f0eff70dd24b3e36af9677ad3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:28 +0000

    Add package mymask/0.1.6/mymask-0.1.6.tar.gz

---
+++ packages/mymask/mymask-0.1.6.tar.gz/mymask-0.1.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mymask',
+    version='0.1.6',
+    description='An interactive data masking tool for any x-y data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Mingyu Li',
+    author_email='lmytime@hotmail.com',
+    url='https://github.com/lmytime/mymask',
+    license='GPL3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'astropy',
+        'PyQt5',
+        'matplotlib'
+    ],
+    include_package_data=True,
+    package_data={'': ['icon.png']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mymask = mymask.app:main',  # 设置命令行入口
+        ],
+    },
+    python_requires='>=3.6',
+)
