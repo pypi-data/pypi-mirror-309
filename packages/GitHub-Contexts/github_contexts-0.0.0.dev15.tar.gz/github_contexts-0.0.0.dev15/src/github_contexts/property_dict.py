0d031704033561dee017d77c89a902496bedb8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:53 +0000

    Add package GitHub-Contexts/0.0.0.dev15/github_contexts-0.0.0.dev15.tar.gz

---
+++ packages/GitHub-Contexts/github_contexts-0.0.0.dev15.tar.gz/github_contexts-0.0.0.dev15/src/github_contexts/property_dict.py
@@ -0,0 +1,54 @@
+import copy as _copy
+
+
+class PropertyDict:
+
+    def __init__(self, data: dict):
+        self._data = dict(sorted(data.items()))
+        return
+
+    @property
+    def as_dict(self) -> dict:
+        """The data as a dictionary."""
+        return self._data
+
+    def get(self, name: str, default=None):
+        return self._data.get(name, default)
+
+    def __getattr__(self, name: str):
+        try:
+            return self._data[name]
+        except KeyError:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
+
+    def __getitem__(self, name: str):
+        return self._data[name]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+
+    def __contains__(self, name: str):
+        return name in self._data
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __repr__(self):
+        return f"PropertyDict({self._data})"
+
+    def __str__(self):
+        return str(self._data)
+
+    def __eq__(self, other):
+        return self._data == other._data
+
+    def __ne__(self, other):
+        return self._data != other._data
+
+    def __deepcopy__(self, memo):
+        # Use `deepcopy` on the internal dictionary to copy its contents
+        copied_data = _copy.deepcopy(self._data, memo)
+        return PropertyDict(copied_data)
