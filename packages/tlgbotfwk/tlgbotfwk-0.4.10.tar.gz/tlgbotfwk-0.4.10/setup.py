4b0ea3c00af8447a7f5498bc533bc91e1763fbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:16:58 +0000

    Add package tlgbotfwk/0.4.10/tlgbotfwk-0.4.10.tar.gz

---
+++ packages/tlgbotfwk/tlgbotfwk-0.4.10.tar.gz/tlgbotfwk-0.4.10/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tlgbotfwk",
+    version="0.4.10",  # Incremented version
+    description='A powerful and extensible Python-based Telegram bot framework',
+    author='Maker',
+    author_email='youremail@example.com',
+    url='https://github.com/gersonfreire/telegram_framework_bolt',  # Replace with your repository URL
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
