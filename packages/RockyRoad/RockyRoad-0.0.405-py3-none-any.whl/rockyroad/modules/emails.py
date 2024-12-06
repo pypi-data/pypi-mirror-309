57031c414cce14e754bc2c54be2ff99e814ea307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:14 +0000

    Add package RockyRoad/0.0.405/RockyRoad-0.0.405-py3-none-any.whl

---
+++ packages/RockyRoad/RockyRoad-0.0.405-py3-none-any.whl/rockyroad/modules/emails.py
@@ -0,0 +1,13 @@
+from .module_imports import *
+
+
+@headers({"Ocp-Apim-Subscription-Key": key})
+class Emails(Consumer):
+    def __init__(self, Resource, *args, **kw):
+        super().__init__(base_url=Resource._base_url, *args, **kw)
+
+    @returns.json
+    @json
+    @post("manual/paths/invoke")
+    def send(self, email_message: Body):
+        """This call will send an email message with the specified recipient, subject, and html/text body."""
