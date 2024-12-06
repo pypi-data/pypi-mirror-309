7c5ab568da112677c241fc3a322466194818dfa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:38 +0000

    Add package curve-apps/0.2.0b1/curve_apps-0.2.0b1.tar.gz

---
+++ packages/curve-apps/curve_apps-0.2.0b1.tar.gz/curve_apps-0.2.0b1/curve_apps/__init__.py
@@ -0,0 +1,31 @@
+#  '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2024 Mira Geoscience Ltd.                                       '
+#                                                                                '
+#  All rights reserved.                                                          '
+#                                                                                '
+#  This file is part of curve-apps.                                              '
+#                                                                                '
+#  curve-apps is distributed under the terms and conditions of the MIT License   '
+#  (see LICENSE file at the root of this source code package).                   '
+#  '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+
+
+__version__ = "0.2.0-beta.1"
+logging.basicConfig(level=logging.INFO)
+
+
+def assets_path() -> Path:
+    """Return the path to the assets folder."""
+
+    parent = Path(__file__).parent
+    folder_name = f"{parent.name}-assets"
+    assets_folder = parent.parent / folder_name
+    if not assets_folder.is_dir():
+        raise RuntimeError(f"Assets folder not found: {assets_folder}")
+
+    return assets_folder
