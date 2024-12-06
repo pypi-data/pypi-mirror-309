86c5c21edf31b1dde4144529876761cb74adba1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:19 +0000

    Add package switchbot-utility/0.4.2/switchbot_utility-0.4.2.tar.gz

---
+++ packages/switchbot-utility/switchbot_utility-0.4.2.tar.gz/switchbot_utility-0.4.2/src/switchbot_utility/switchbot_bot.py
@@ -0,0 +1,22 @@
+from switchbot_utility.battery_mixin import BatteryMixin
+from switchbot_utility.onoff_mixin import OnOffMixin
+from switchbot_utility.switchbot_device import SwitchbotDevice
+
+
+class SwitchbotBot(SwitchbotDevice, OnOffMixin, BatteryMixin):
+    """Switchbot bot class"""
+
+    def __init__(self, deviceId):
+        """Constructor"""
+        super().__init__(deviceId)
+
+    def get_power(self) -> dict:
+        """Returns ON/OFF state"""
+        status = self.get_status()
+        return status["power"]
+
+    def press(self) -> str:
+        """press action"""
+        self._body["command"] = "press"
+        result = self.command(self.deviceId, self._body)
+        return result.text
---
+++ packages/switchbot-utility/switchbot_utility-0.4.2.tar.gz/switchbot_utility-0.4.2/src/switchbot_utility/switchbot_robot_vacuum_cleaner_s1_plus.py
@@ -0,0 +1,9 @@
+from switchbot_utility.switchbot_robot_vacuum_cleaner_s1 import SwitchbotRobotVacuumCleanerS1
+
+
+class SwitchbotRobotVacuumCleanerS1Plus(SwitchbotRobotVacuumCleanerS1):
+    """Switchbot Vacuum Cleaner S1 Plus class"""
+
+    def __init__(self, deviceId):
+        """Constructor"""
+        super().__init__(deviceId)
