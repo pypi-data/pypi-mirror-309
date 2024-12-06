d329de072a2b7fa455e832abaea00349d6d6e3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:11 +0000

    Add package lilliepy/0.8.0/lilliepy-0.8.0.tar.gz

---
+++ packages/lilliepy/lilliepy-0.8.0.tar.gz/lilliepy-0.8.0/lilliepy/main.py
@@ -0,0 +1,31 @@
+import subprocess as sbp
+import os, sys
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+def create_bat():
+    try:
+        bat_file_path = os.path.join(module_dir, "bin.bat")
+        sbp.run([bat_file_path], shell=True, check=True)
+    except sbp.CalledProcessError as e:
+        print(f"Error running .bat file: {e}")
+        sys.exit(1)
+
+def create_sh():
+    try:
+        sh_file_path = os.path.join(module_dir, "bin.sh")
+        sbp.run(["bash", sh_file_path], check=True)
+    except sbp.CalledProcessError as e:
+        print(f"Error running .sh file: {e}")
+        sys.exit(1)
+
+def create(_os = "windows"):
+    if _os == "windows" or _os == "batch" or _os == "cmd" or _os == "bat":
+        create_bat()
+    elif _os == "linus" or _os == "mac" or _os == "shell" or _os == "sh":
+        create_sh()
+    else:
+        create_sh()
+
+def version():
+    return 'LilliePy Version : 0.7.0'
