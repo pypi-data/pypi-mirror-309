93c25da0031f4e4199bb168ed897d44d6a0c865f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:59 +0000

    Add package dj-jwt-auth/1.7.0/dj_jwt_auth-1.7.0.tar.gz

---
+++ packages/dj-jwt-auth/dj_jwt_auth-1.7.0.tar.gz/dj_jwt_auth-1.7.0/django_jwt/exceptions.py
@@ -0,0 +1,10 @@
+class ConfigException(Exception):
+    """Base class for exceptions in this module."""
+
+    pass
+
+
+class BadRequestException(Exception):
+    """Base class for exceptions in this module."""
+
+    pass
