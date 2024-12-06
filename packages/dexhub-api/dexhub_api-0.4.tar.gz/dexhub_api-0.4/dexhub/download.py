8ad3cc516bba94b9cc25f2a3c39fa3af18b59d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:54 +0000

    Add package dexhub-api/0.4/dexhub_api-0.4.tar.gz

---
+++ packages/dexhub-api/dexhub_api-0.4.tar.gz/dexhub_api-0.4/dexhub/download.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+def my_dataset(project: str = None) -> Dict:
+    """
+    Get the dataset for the specified project.
+    
+    Args:
+        project (str): the name of the project to get the dataset for.
+    
+    Returns:
+        dict: the dataset for the specified project.
+    """
+    
+    return True 
+
+def global_dataset() -> Dict:
+    """
+    Get the entire global dataset.
+    
+    Returns:
+        dict: the entire global dataset.
+    """
+    return True
