b1af73ac38a1f6697e5cb3a2b0565c0474269c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:41 +0000

    Add package algoliasearch/4.9.2/algoliasearch-4.9.2-py3-none-any.whl

---
+++ packages/algoliasearch/algoliasearch-4.9.2-py3-none-any.whl/algoliasearch/http/user_agent.py
@@ -0,0 +1,24 @@
+from platform import python_version
+from sys import version_info
+from typing import Optional
+
+if version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
+from algoliasearch import __version__
+
+
+class UserAgent:
+    def __init__(self) -> None:
+        self.value = "Algolia for Python ({}); Python ({})".format(
+            __version__, str(python_version())
+        )
+
+    def get(self) -> str:
+        return self.value
+
+    def add(self, segment: str, version: Optional[str] = __version__) -> Self:
+        self.value += "; {} ({})".format(segment, version)
+        return self
