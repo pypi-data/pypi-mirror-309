35e6763c931c221a0d1563a96c788e6c9e7f62e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:24:54 +0000

    Add package mqt.qmap/2.8.0/mqt_qmap-2.8.0-cp311-cp311-manylinux_2_28_aarch64.whl

---
+++ packages/mqt.qmap/mqt_qmap-2.8.0-cp311-cp311-manylinux_2_28_aarch64.whl/mqt/qmap/load_calibration.py
@@ -0,0 +1,36 @@
+"""Module for loading calibration data."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from qiskit.providers.models import BackendProperties
+from qiskit.transpiler.target import Target
+
+if TYPE_CHECKING:
+    from .pyqmap import Architecture
+
+
+def load_calibration(architecture: Architecture, calibration: str | Target | BackendProperties | None = None) -> None:
+    """Load a calibration from a string, BackendProperties, or Target.
+
+    Args:
+        architecture: The architecture to load the calibration into.
+        calibration: The calibration to load.
+    """
+    if calibration is None:
+        return
+
+    if isinstance(calibration, str):
+        architecture.load_properties(calibration)
+    elif isinstance(calibration, BackendProperties):
+        from mqt.qmap.qiskit.backend import import_backend_properties
+
+        architecture.load_properties(import_backend_properties(calibration))
+    elif isinstance(calibration, Target):
+        from mqt.qmap.qiskit.backend import import_target
+
+        architecture.load_properties(import_target(calibration))
+    else:  # pragma: no cover
+        msg = f"Calibration type {type(calibration)} not supported."
+        raise TypeError(msg)
