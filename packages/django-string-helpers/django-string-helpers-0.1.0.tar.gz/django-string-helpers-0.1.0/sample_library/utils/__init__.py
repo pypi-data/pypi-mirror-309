cd8ea4599cc35f977ebb690ce4b54968767f65b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:44 +0000

    Add package django-string-helpers/0.1.0/django-string-helpers-0.1.0.tar.gz

---
+++ packages/django-string-helpers/django-string-helpers-0.1.0.tar.gz/django-string-helpers-0.1.0/sample_library/utils/__init__.py
@@ -0,0 +1,2 @@
+# This file marks the folder as a Python package
+__version__ = "0.1.0"
---
+++ packages/django-string-helpers/django-string-helpers-0.1.0.tar.gz/django-string-helpers-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-string-helpers",
+    version="0.1.0",
+    author="Rohit Hazare",
+    author_email="rohithazare20@gmail.com",
+    description="A collection of string helper utilities for Django applications",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/rohit2096/sample-library.git",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django>=3.2",
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
