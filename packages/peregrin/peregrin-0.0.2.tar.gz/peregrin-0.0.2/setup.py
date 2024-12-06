7d7ae5b782ef13f7500803232beb2f1127887266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:29 +0000

    Add package peregrin/0.0.2/peregrin-0.0.2.tar.gz

---
+++ packages/peregrin/peregrin-0.0.2.tar.gz/peregrin-0.0.2/setup.py
@@ -0,0 +1,15 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# This call to setup() does all the work
+setup(
+    name="peregrin",
+    version="0.0.2",
+    description="Demo",
+    author="Branislav Modriansky",
+    author_email="modrinaskybranislav@gmail.com",
+    license="MIT",
+    url="https://github.com/BranislavModriansky/Peregrin/tree/main",
+    packages=find_packages(),
+    include_package_data=True
+)
