051444c5e350d238f4faf2d08e27ec3c359dbb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:19 +0000

    Add package dtech-password-generator/1.0.0/dtech_password_generator-1.0.0-py3-none-any.whl

---
+++ packages/dtech-password-generator/dtech_password_generator-1.0.0-py3-none-any.whl/dtech_password_generator/__init__.py
@@ -0,0 +1,8 @@
+# password_generator/__init__.py
+
+# Import the password generator function to expose it at the package level
+from .passgen import password 
+
+# Define package metadata
+__version__ = "1.0.0"
+__author__ = "Daniel Wangari"
