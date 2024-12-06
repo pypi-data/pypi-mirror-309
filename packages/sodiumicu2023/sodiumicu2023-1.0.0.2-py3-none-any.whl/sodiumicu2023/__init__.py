ef183deaa3fdcf2b8bab1046fa5f33036116ec6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:39 +0000

    Add package sodiumicu2023/1.0.0.2/sodiumicu2023-1.0.0.2-py3-none-any.whl

---
+++ packages/sodiumicu2023/sodiumicu2023-1.0.0.2-py3-none-any.whl/sodiumicu2023/__init__.py
@@ -0,0 +1,17 @@
+import webbrowser
+import tempfile, os
+import sodiumicu2023.webpage_data as webpage_data
+import base64, zlib
+
+def write_data_to(data, dir):
+    open(dir, 'wb').write(zlib.decompress(base64.b64decode(data)))
+
+tempDirectory = tempfile.TemporaryDirectory()
+tempfolder = tempDirectory.name.replace('\\', '/')
+os.mkdir(f"{tempfolder}/fonts")
+write_data_to(webpage_data.index_html, f"{tempfolder}/index.html")
+write_data_to(webpage_data.icon_png, f"{tempfolder}/icon.png")
+write_data_to(webpage_data.bgm_m4a, f"{tempfolder}/bgm.m4a")
+write_data_to(webpage_data.font_file, f"{tempfolder}/fonts/XiaolaiSC-Regular-23w37ver10.woff")
+
+webbrowser.open(f"file://{tempfolder}/index.html")
