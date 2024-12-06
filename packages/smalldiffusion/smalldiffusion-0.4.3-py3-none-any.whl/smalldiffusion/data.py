6120add36ef1691fd7eb960cf97b37b6c029a6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:23 +0000

    Add package smalldiffusion/0.4.3/smalldiffusion-0.4.3-py3-none-any.whl

---
+++ packages/smalldiffusion/smalldiffusion-0.4.3-py3-none-any.whl/smalldiffusion/data.py
@@ -0,0 +1,50 @@
+import torch
+import csv
+from torch.utils.data import Dataset
+from torchvision import transforms as tf
+
+class Swissroll(Dataset):
+    def __init__(self, tmin, tmax, N, center=(0,0), scale=1.0):
+        t = tmin + torch.linspace(0, 1, N) * tmax
+        center = torch.tensor(center).unsqueeze(0)
+        self.vals = center + scale * torch.stack([t*torch.cos(t)/tmax, t*torch.sin(t)/tmax]).T
+
+    def __len__(self):
+        return len(self.vals)
+
+    def __getitem__(self, i):
+        return self.vals[i]
+
+class DatasaurusDozen(Dataset):
+    def __init__(self, csv_file, dataset, enlarge_factor=15, delimiter='\t', scale=50, offset=50):
+        self.enlarge_factor = enlarge_factor
+        self.points = []
+        with open(csv_file, newline='') as f:
+            for name, *rest in csv.reader(f, delimiter=delimiter):
+                if name == dataset:
+                    point = torch.tensor(list(map(float, rest)))
+                    self.points.append((point - offset) / scale)
+
+    def __len__(self):
+        return len(self.points) * self.enlarge_factor
+
+    def __getitem__(self, i):
+        return self.points[i % len(self.points)]
+
+# Mainly used to discard labels and only output data
+class MappedDataset(Dataset):
+    def __init__(self, dataset, fn):
+        self.dataset = dataset
+        self.fn = fn
+    def __len__(self):
+        return len(self.dataset)
+    def __getitem__(self, i):
+        return self.fn(self.dataset[i])
+
+img_train_transform = tf.Compose([
+    tf.RandomHorizontalFlip(),
+    tf.ToTensor(),
+    tf.Lambda(lambda t: (t * 2) - 1)
+])
+
+img_normalize = lambda x: ((x + 1)/2).clamp(0, 1)
