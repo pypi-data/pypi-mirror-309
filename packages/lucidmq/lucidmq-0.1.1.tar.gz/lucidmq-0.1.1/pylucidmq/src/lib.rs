74deca25b2102edbf6a7a28790c6fe23000e79c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:22 +0000

    Add package lucidmq/0.1.1/lucidmq-0.1.1.tar.gz

---
+++ packages/lucidmq/lucidmq-0.1.1.tar.gz/lucidmq-0.1.1/pylucidmq/src/lib.rs
@@ -0,0 +1,21 @@
+use pyo3::prelude::*;
+
+mod consumer;
+mod lucidmq;
+mod message;
+mod producer;
+
+use consumer::Consumer;
+use lucidmq::LucidMQ;
+use message::Message;
+use producer::Producer;
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn pylucidmq(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Message>()?;
+    m.add_class::<Producer>()?;
+    m.add_class::<Consumer>()?;
+    m.add_class::<LucidMQ>()?;
+    Ok(())
+}
