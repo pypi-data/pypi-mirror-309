22431fa7852d303d6074807d80ce824990877c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:55 +0000

    Add package mullvad-tool/0.1.2/mullvad_tool-0.1.2.tar.gz

---
+++ packages/mullvad-tool/mullvad_tool-0.1.2.tar.gz/mullvad_tool-0.1.2/mullvad_tool/portgen.py
@@ -0,0 +1,27 @@
+import os
+from json import dump, load
+
+import click
+
+
+def ensure_dir(path):
+    """Creates the specified directory if it doesn't already exist."""
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def init_portgen(starting_port, run_name, state_file):
+    ensure_dir(click.get_app_dir("mullvad"))
+    with open(state_file, "w") as f:
+        dump({run_name: starting_port}, f)
+    return "Ready to generate ports with portgen run"
+
+
+def portgen(run_name, state_file):
+    with open(state_file, "r") as f:
+        data = load(f)
+    port = data[run_name]
+    next_port = port + 1
+    with open(state_file, "w") as f:
+        dump({run_name: next_port}, f)
+    return port
