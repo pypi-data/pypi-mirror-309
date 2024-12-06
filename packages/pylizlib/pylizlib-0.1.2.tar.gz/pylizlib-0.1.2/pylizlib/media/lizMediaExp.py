bdf90c10c8f651bae5ed714911a712708ff92050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:36 +0000

    Add package pylizlib/0.1.2/pylizlib-0.1.2.tar.gz

---
+++ packages/pylizlib/pylizlib-0.1.2.tar.gz/pylizlib-0.1.2/pylizlib/media/lizMediaExp.py
@@ -0,0 +1,11 @@
+from pylizlib.media.liz_media import LizMedia
+from pylizlib.util import fileutils
+
+
+class LizMediaExporter:
+
+    def __init__(self, media: LizMedia):
+        self.media = media
+
+    def export_to_json(self):
+        fileutils.write_json_to_file(self.media.path, self.media.file_name + ".json", self.media.to_json_only_ai())
