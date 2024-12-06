8f31375956776eb3d941ecd93c5433bceb850df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:13:21 +0000

    Add package eta-utility/3.2.5/eta_utility-3.2.5-py3-none-any.whl

---
+++ packages/eta-utility/eta_utility-3.2.5-py3-none-any.whl/eta_utility/type_hints/__init__.py
@@ -0,0 +1,28 @@
+from .custom_types import (
+    Number as Number,
+    Path as Path,
+    PrivateKey as PrivateKey,
+    TimeStep as TimeStep,
+)
+from .types_connectors import (
+    AnyNode as AnyNode,
+    Nodes as Nodes,
+)
+
+# Only import eta_x types if it is installed
+try:
+    import gymnasium as gymnasium
+except ModuleNotFoundError:
+    pass
+else:
+    from .types_eta_x import (
+        ActionType as ActionType,
+        AlgoSettings as AlgoSettings,
+        EnvSettings as EnvSettings,
+        GymEnv as GymEnv,
+        MaybeCallback as MaybeCallback,
+        ObservationType as ObservationType,
+        PyoParams as PyoParams,
+        ResetResult as ResetResult,
+        StepResult as StepResult,
+    )
