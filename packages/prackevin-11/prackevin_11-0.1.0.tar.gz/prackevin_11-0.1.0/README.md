046907838d41cf0127b1218fc58c13288865d61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:56 +0000

    Add package prackevin-11/0.1.0/prackevin_11-0.1.0.tar.gz

---
+++ packages/prackevin-11/prackevin_11-0.1.0.tar.gz/prackevin_11-0.1.0/README.md
@@ -0,0 +1,18 @@
+# MyPrinter
+
+A simple Python library with 10 unique print functions.
+
+## Installation
+
+```bash
+pip install prackevin-11
+```
+
+## Usage
+
+```python
+from prackevin-11 import *
+
+print_message_1()
+print_message_2()
+```
---
+++ packages/prackevin-11/prackevin_11-0.1.0.tar.gz/prackevin_11-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prackevin-11",
+    version="0.1.0",
+    description="A library with 10 unique print functions.",
+    long_description="A simple package with 10 default print functions to customize as needed.",
+    long_description_content_type="text/plain",
+    author="Your Name",
+    author_email="your.email@example.com",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
