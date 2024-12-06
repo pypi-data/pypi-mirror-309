18cc98300b44bd677d1bb6f16866235d17015269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:04:56 +0000

    Add package chalkpy/2.61.6/chalkpy-2.61.6-py3-none-any.whl

---
+++ packages/chalkpy/chalkpy-2.61.6-py3-none-any.whl/chalk/operators/_literal.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+import pyarrow as pa
+
+from chalk.operators import StaticExpression
+
+
+class LiteralExpression(StaticExpression):
+    def __init__(self, value: Any, dtype: pa.DataType, name: str | None = None) -> None:
+        self.value = value
+        self.dtype = dtype
+        self.name = name
+
+        super().__init__()
+
+    def alias(self, name: str) -> StaticExpression:
+        return LiteralExpression(self.value, self.dtype, name)
