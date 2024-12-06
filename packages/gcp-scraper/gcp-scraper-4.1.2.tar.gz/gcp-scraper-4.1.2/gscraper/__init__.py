5fbcc79e93bc2ede026aca5da6ac39ad2f796436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:41 +0000

    Add package gcp-scraper/4.1.2/gcp-scraper-4.1.2.tar.gz

---
+++ packages/gcp-scraper/gcp-scraper-4.1.2.tar.gz/gcp-scraper-4.1.2/gscraper/__init__.py
@@ -0,0 +1,13 @@
+from .base.abstract import *
+from .base.gcloud import *
+from .base.session import *
+from .base.spider import *
+from .base.types import *
+
+from .utils import *
+from .utils.cast import *
+from .utils.date import *
+from .utils.logs import *
+from .utils.map import *
+
+__version__ = "4.1.2"
