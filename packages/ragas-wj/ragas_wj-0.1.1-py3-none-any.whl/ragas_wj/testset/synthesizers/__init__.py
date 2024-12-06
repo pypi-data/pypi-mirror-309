f9d4157b892423905148d931afe4c1a4a8b7f758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:06 +0000

    Add package ragas-wj/0.1.1/ragas_wj-0.1.1-py3-none-any.whl

---
+++ packages/ragas-wj/ragas_wj-0.1.1-py3-none-any.whl/ragas_wj/testset/synthesizers/__init__.py
@@ -0,0 +1,28 @@
+import typing as t
+
+from ragas.llms import BaseRagasLLM
+from ragas.testset.synthesizers.multi_hop import (
+    MultiHopAbstractQuerySynthesizer,
+    MultiHopSpecificQuerySynthesizer,
+)
+from ragas.testset.synthesizers.single_hop.specific import (
+    SingleHopSpecificQuerySynthesizer,
+)
+
+from .base import BaseSynthesizer
+
+QueryDistribution = t.List[t.Tuple[BaseSynthesizer, float]]
+
+
+def default_query_distribution(llm: BaseRagasLLM) -> QueryDistribution:
+    return [
+        (SingleHopSpecificQuerySynthesizer(llm=llm), 0.5),
+        (MultiHopAbstractQuerySynthesizer(llm=llm), 0.25),
+        (MultiHopSpecificQuerySynthesizer(llm=llm), 0.25),
+    ]
+
+
+__all__ = [
+    "BaseSynthesizer",
+    "default_query_distribution",
+]
