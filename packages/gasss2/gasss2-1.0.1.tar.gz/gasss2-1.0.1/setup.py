9fb732fec450259c583e16aef9e1f875ac9a1b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:05:10 +0000

    Add package gasss2/1.0.1/gasss2-1.0.1.tar.gz

---
+++ packages/gasss2/gasss2-1.0.1.tar.gz/gasss2-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gasss2',
+      version='1.0.1',
+      description='Gaspump with slippage',
+      packages=['gaspump'],
+      author_email='tafik47634@merotx.com',
+      zip_safe=True)
