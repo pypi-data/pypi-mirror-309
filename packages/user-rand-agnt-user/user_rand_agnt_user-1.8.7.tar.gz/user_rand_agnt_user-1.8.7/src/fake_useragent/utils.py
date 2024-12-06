f0fa0e0962483f092a444281ce73398c6ba8c282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:24 +0000

    Add package user-rand-agnt-user/1.8.7/user_rand_agnt_user-1.8.7.tar.gz

---
+++ packages/user-rand-agnt-user/user_rand_agnt_user-1.8.7.tar.gz/user_rand_agnt_user-1.8.7/src/fake_useragent/utils.py
@@ -0,0 +1,70 @@
+import json
+import sys
+
+# We need files() from Python 3.10 or higher
+if sys.version_info >= (3, 10):
+    import importlib.resources as ilr
+else:
+    import importlib_resources as ilr
+
+from fake_useragent.log import logger
+
+str_types = (str,)
+
+
+# Load all lines from browser.json file
+# Returns array of objects
+def load(fn="browsers.json"):
+    data, ret = [], None
+    try:
+        if fn == "browsers.json":
+            json_lines = (
+                ilr.files("fake_useragent.data").joinpath(fn).read_text()
+            )
+            for line in json_lines.splitlines():
+                data.append(json.loads(line))
+            ret = data
+        else:
+            ret = ilr.files("fake_useragent.data").joinpath(fn).read_bytes()
+    except Exception as exc:
+        # Empty data just to be sure
+        data = []
+        logger.warning(
+            "Unable to find local data/json file or could not parse the contents using importlib-resources. Try pkg-resource next.",
+            exc_info=exc,
+        )
+        try:
+            from pkg_resources import resource_filename
+
+            if fn == "browsers.json":
+                with open(
+                    resource_filename("fake_useragent", "data/"+str(fn))
+                ) as file:
+                    json_lines = file.read()
+                    for line in json_lines.splitlines():
+                        data.append(json.loads(line))
+            else:
+                with open(
+                    resource_filename("fake_useragent", "data/"+str(fn)),
+                    'rb'
+                ) as file:
+                    data = file.read()
+            ret = data
+        except Exception as exc2:
+            # Empty data just to be sure
+            data = []
+            logger.warning(
+                "Could not find local data/json file or could not parse the contents using pkg-resource.",
+                exc_info=exc2,
+            )
+
+    if not ret:
+        raise FakeUserAgentError("Data list is empty", ret)
+
+    if fn == "browsers.json" and not isinstance(ret, list):
+        raise FakeUserAgentError("Data is not a list ", ret)
+    return ret
+
+
+from fake_useragent import settings  # noqa # isort:skip
+from fake_useragent.errors import FakeUserAgentError  # noqa # isort:skip
