664de90f4c75dd9f1fe32e6d205df02fb4a29c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:23 +0000

    Add package makdo/7.16/makdo-7.16.tar.gz

---
+++ packages/makdo/makdo-7.16.tar.gz/makdo-7.16/setup.py
@@ -0,0 +1,22 @@
+import sys
+from setuptools import setup
+
+VERSION = '07.16'
+
+INSTALL_REQUIRES = ['python-docx', 'chardet', 'Levenshtein', 'tkinterdnd2', 'openpyxl', 'openai', 'llama_cpp_python']
+if sys.platform == 'win32':
+    INSTALL_REQUIRES.append('pywin32')
+
+setup(
+    name='makdo',
+    version=VERSION,
+    description='日本の公用文書（司法文書、行政文書）をMarkdown形式とMicrosoft Word形式との間で変換します',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Seiichiro HATA',
+    author_email='hata48915b@post.nifty.jp',
+    url='https://github.com/hata48915b/makdo/',
+    license='GPLv3+',
+    install_requires=INSTALL_REQUIRES,
+    packages=['makdo'],
+)
