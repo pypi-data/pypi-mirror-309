c11c65e1649dfc5cd75d417d263a8f266e8447c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:46 +0000

    Add package python-postgres/0.0.2/python_postgres-0.0.2-py3-none-any.whl

---
+++ packages/python-postgres/python_postgres-0.0.2-py3-none-any.whl/python_postgres/_transactions.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+from psycopg import AsyncCursor
+from psycopg_pool import AsyncConnectionPool
+
+from ._operations import _exec_query, _results
+from .types import Params, Query
+
+
+class Transaction:
+    def __init__(self, pool: AsyncConnectionPool, cur: AsyncCursor):
+        self._cur = cur
+        self._pool = pool
+
+    async def __call__(self, query: Query, params: Params = ()) -> list[tuple[Any, ...]] | int:
+        await _exec_query(self._pool, self._cur, query, params)
+        return await _results(self._cur)
