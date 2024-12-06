6aa3e76e1f6ff3aaa417d1e04915d39a480a8b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:15:47 +0000

    Add package mecher-appium-page/0.0.2/mecher_appium_page-0.0.2.tar.gz

---
+++ packages/mecher-appium-page/mecher_appium_page-0.0.2.tar.gz/mecher_appium_page-0.0.2/README.md
@@ -0,0 +1,1 @@
+Universal Appium Page instruments
---
+++ packages/mecher-appium-page/mecher_appium_page-0.0.2.tar.gz/mecher_appium_page-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    REQUIRES = f.readlines()
+
+setup(
+    name='mecher_appium_page',
+    version="0.0.2",
+    description='Universal Appium Page',
+    maintainer='mecher',
+    url='https://github.com/MaximChernyak98/mecher_appium_page',
+    keywords=['testing'],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    dependency_links=[],
+    long_description_content_type='text/plain',
+    long_description='Universal Appium Page',
+    setup_requires=['setuptools-git-versioning'],
+)
