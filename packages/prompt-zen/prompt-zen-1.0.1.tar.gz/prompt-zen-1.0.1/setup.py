d8cfef40822394d074a49b70141317d7273cbfa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:43 +0000

    Add package prompt-zen/1.0.1/prompt-zen-1.0.1.tar.gz

---
+++ packages/prompt-zen/prompt-zen-1.0.1.tar.gz/prompt-zen-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prompt-zen",
+    version="1.0.1",
+    description="A framework for iterative prompt refinement and optimization.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Philipp Heller",
+    license="BSD-3-Clause",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=2.2.3",
+        "langchain>=0.3.7",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
