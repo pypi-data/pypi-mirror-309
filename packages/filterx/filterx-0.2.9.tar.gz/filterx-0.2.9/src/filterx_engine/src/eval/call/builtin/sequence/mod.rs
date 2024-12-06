0596dad1aa538ee9e08d671f7c77f51e9dcab612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:12 +0000

    Add package filterx/0.2.9/filterx-0.2.9.tar.gz

---
+++ packages/filterx/filterx-0.2.9.tar.gz/filterx-0.2.9/src/filterx_engine/src/eval/call/builtin/sequence/mod.rs
@@ -0,0 +1,11 @@
+use crate::builtin_function;
+
+builtin_function! {
+    FUNCTION_SEQUENCE,
+    (gc, true, false),
+    (revcomp, true, true),
+    (to_fasta, false, false, (to_fa)),
+    (to_fastq, false, false, (to_fq)),
+    (qual, true, false),
+    (phred, false, false),
+}
---
+++ packages/filterx/filterx-0.2.9.tar.gz/filterx-0.2.9/src/filterx_source/src/block/mod.rs
@@ -0,0 +1,5 @@
+pub mod fastx;
+pub mod reader;
+
+pub use fastx::fasta;
+pub use fastx::fastq;
