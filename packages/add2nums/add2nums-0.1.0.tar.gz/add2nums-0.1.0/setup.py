982028e0d5cca52425b304249c245e267a852606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:13 +0000

    Add package add2nums/0.1.0/add2nums-0.1.0.tar.gz

---
+++ packages/add2nums/add2nums-0.1.0.tar.gz/add2nums-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='add2nums',  # Unique package name
+    version='0.1.0',    # Version number
+    author='Talha',
+    author_email='your.email@example.com',
+    description='A utility package for math and string operations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MuhammadTalha-crypto/sum',
+    packages=find_packages(where="src"),  # Automatically find packages
+    package_dir={'': 'src'},  # Root of your source code
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
