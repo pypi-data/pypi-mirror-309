006a755365ff1ed2d596fd2d0fa82555b7a8b882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:43 +0000

    Add package UseMod/1.1.1/usemod-1.1.1.tar.gz

---
+++ packages/UseMod/usemod-1.1.1.tar.gz/usemod-1.1.1/UseMod/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+"""
+Simple_Dropbox est un module qui fournit un accès simplifié à Dropbox.
+Il contient des fonctions pour interagir avec l'API de Dropbox.
+"""
+from .Dropbox import *
+from .Github import *
+from .Box import *
---
+++ packages/UseMod/usemod-1.1.1.tar.gz/usemod-1.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="UseMod",
+    version="1.1.1",
+    author="Grivy16",
+    author_email="grivy16public@gmail.com",
+    description="UseMod",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Grivy16/UseMod/wiki",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "UseMod": ["assets/*.ico", "assets/*.jpg"],  # Chemin vers les images dans le dossier UseMod/assets
+    },
+    install_requires=[
+        'requests',
+        'dropbox',
+        "clipboard",
+        "customtkinter",
+        "Pillow",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",  # Correction ici
+    ],
+    python_requires=">=3.6",
+)
