013b8eb1a56ae478df77a2c74f10004254fe6e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:09 +0000

    Add package MatrixApp/0.1.3/matrixapp-0.1.3.tar.gz

---
+++ packages/MatrixApp/matrixapp-0.1.3.tar.gz/matrixapp-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MatrixApp",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "numpy", "matplotlib", "pandas",
+    ],
+)
