d55df16d2f796331cb06846003383ef610942a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:55 +0000

    Add package wave-flow/1.0.4/wave_flow-1.0.4.tar.gz

---
+++ packages/wave-flow/wave_flow-1.0.4.tar.gz/wave_flow-1.0.4/WaveFlow/__init__.py
@@ -0,0 +1,8 @@
+from preRequisite.preRequisites import PreRequisitesWave
+from service.data.To import To
+from service.data.handler import DataHandler
+from service.generate.generator import Builder
+from service.transmite.transmitter import Transmitter
+
+# Enjoy that Wave
+
---
+++ packages/wave-flow/wave_flow-1.0.4.tar.gz/wave_flow-1.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as arq:
+    readme = arq.read()
+
+setup(
+    name="wave-flow",
+    version="1.0.4",
+    license="MIT License",
+    author="Lucas Lourenço",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email="dev.lucaslourenco@gmail.com",
+    keywords="wave build docx treatdata xlsx to word waveflow wave-flow",
+    description="WAVE - Workflow Automation and Versatile Engine",
+    packages=['WaveFlow'],
+    install_requires=["python-docx", "openpyxl", "pandas"],
+)
