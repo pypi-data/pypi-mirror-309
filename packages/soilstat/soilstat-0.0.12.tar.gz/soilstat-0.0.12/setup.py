4158b9f614c791f1f3b418526220e7955e244a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:52 +0000

    Add package soilstat/0.0.12/soilstat-0.0.12.tar.gz

---
+++ packages/soilstat/soilstat-0.0.12.tar.gz/soilstat-0.0.12/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="soilstat",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    author="Numan Burak Fidan",
+    author_email="numanburakfidan@yandex.com",
+    description="A toolbox for geotechnical engineering calculations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nubufi/soilstat",
+    packages=find_packages(
+        include=["soilstat", "soilstat.*"]
+    ),  # Automatically find packages in the directory
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["numpy"],  # Dependencies your project needs
+)
