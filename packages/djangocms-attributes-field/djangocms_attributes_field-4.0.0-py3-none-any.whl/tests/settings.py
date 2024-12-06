9bab8394dc72e14df28f51aa0fcc2e265f886a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:55 +0000

    Add package djangocms-attributes-field/4.0.0/djangocms_attributes_field-4.0.0-py3-none-any.whl

---
+++ packages/djangocms-attributes-field/djangocms_attributes_field-4.0.0-py3-none-any.whl/tests/settings.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+HELPER_SETTINGS = {
+    'INSTALLED_APPS': [
+        'djangocms_attributes_field',
+        'tests.test_app',
+    ],
+    'CMS_LANGUAGES': {
+        1: [{
+            'code': 'en',
+            'name': 'English',
+        }]
+    },
+    'LANGUAGE_CODE': 'en',
+    'ALLOWED_HOSTS': ['localhost'],
+    'CMS_CONFIRM_VERSION4': True,
+}
+
+
+def run():
+    from app_helper import runner
+    runner.cms('djangocms_attributes_field')
+
+
+if __name__ == '__main__':
+    run()
