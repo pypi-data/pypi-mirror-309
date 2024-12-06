f17163782fe4a9aed18999ab2eec9cb7246a9e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:33:25 +0000

    Add package NLPX/1.7.2/NLPX-1.7.2.tar.gz

---
+++ packages/NLPX/NLPX-1.7.2.tar.gz/NLPX-1.7.2/nlpx/log_utils.py
@@ -0,0 +1,8 @@
+from logging import basicConfig, INFO, getLogger
+
+logger = getLogger(__name__)
+basicConfig(level=INFO, format='[%(asctime)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+def info(message):
+	logger.info(message)
