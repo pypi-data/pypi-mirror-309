83151437bb81b5a9b01316b845cc13135d3b2295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:45 +0000

    Add package xiaomusic/0.3.48/xiaomusic-0.3.48.tar.gz

---
+++ packages/xiaomusic/xiaomusic-0.3.48.tar.gz/xiaomusic-0.3.48/xiaomusic/const.py
@@ -0,0 +1,26 @@
+SUPPORT_MUSIC_TYPE = [
+    ".mp3",
+    ".flac",
+    ".wav",
+    ".ape",
+    ".ogg",
+    ".m4a",
+]
+
+LATEST_ASK_API = "https://userprofile.mina.mi.com/device_profile/v2/conversation?source=dialogu&hardware={hardware}&timestamp={timestamp}&limit=2"
+COOKIE_TEMPLATE = "deviceId={device_id}; serviceToken={service_token}; userId={user_id}"
+
+PLAY_TYPE_ONE = 0  # 单曲循环
+PLAY_TYPE_ALL = 1  # 全部循环
+PLAY_TYPE_RND = 2  # 随机播放
+
+PLAY_TYPE_TTS = {
+    PLAY_TYPE_ONE: "已经设置为单曲循环",
+    PLAY_TYPE_ALL: "已经设置为全部循环",
+    PLAY_TYPE_RND: "已经设置为随机播放",
+}
+
+# 需要采用 mina 获取对话记录的设备型号
+GET_ASK_BY_MINA = {
+    "M01",
+}
