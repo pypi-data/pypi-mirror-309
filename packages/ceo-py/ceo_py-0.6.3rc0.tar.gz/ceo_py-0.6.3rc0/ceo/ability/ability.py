0e14519859f1b87e26a8002a7883dec3f41ee9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:42 +0000

    Add package ceo-py/0.6.3rc0/ceo_py-0.6.3rc0.tar.gz

---
+++ packages/ceo-py/ceo_py-0.6.3rc0.tar.gz/ceo_py-0.6.3rc0/ceo/ability/ability.py
@@ -0,0 +1,23 @@
+import json
+from typing import Callable
+
+
+class Ability:
+    def __init__(self, function: Callable):
+        self.name: str = function.__name__
+        self.description: str = function.__doc__
+        self.function: Callable = function
+        self.parameters: dict = dict()
+        for key, clazz in function.__annotations__.items():
+            if key != 'return':
+                self.parameters[key] = clazz.__name__
+
+    def __repr__(self):
+        return json.dumps(obj={
+            'name': self.name,
+            'description': self.description,
+            'parameters': self.parameters
+        }, ensure_ascii=False)
+
+    def __str__(self):
+        return self.__repr__()
