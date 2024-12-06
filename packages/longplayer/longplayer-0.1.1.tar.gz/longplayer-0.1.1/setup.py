baa27ebfaf55652fea3b99c3a0460a615eb5ceb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:03:45 +0000

    Add package longplayer/0.1.1/longplayer-0.1.1.tar.gz

---
+++ packages/longplayer/longplayer-0.1.1.tar.gz/longplayer-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name = 'longplayer',
+    version = '0.1.1',
+    description = 'Longplayer, a thousand-year long musical composition, implemented in Python',
+    long_description = open("README.md", "r").read(),
+    long_description_content_type = "text/markdown",
+    author = 'Daniel Jones and Jem Finer',
+    author_email = 'dan-code@erase.net',
+    url = 'https://github.com/TheLongplayerTrust/longplayer-python',
+    packages = ['longplayer'],
+    scripts = ['bin/longplayer'],
+    install_requires = [
+        'soundfile',
+        'sounddevice',
+    ],
+    keywords = ('sound', 'music', 'time', 'soundart'),
+    classifiers = [
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Artistic Software',
+        'Topic :: Communications',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop'
+    ],
+    package_data={
+        'longplayer': ['audio/20-20.aif']
+    },
+    include_package_data=True,
+)
