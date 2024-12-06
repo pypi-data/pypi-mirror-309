0d3b12f46d1555dff9fc4c1383fec4b648556b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:16 +0000

    Add package dpt/1.2.3/dpt-1.2.3.tar.gz

---
+++ packages/dpt/dpt-1.2.3.tar.gz/dpt-1.2.3/dpt/__init__.py
@@ -0,0 +1,6 @@
+from .processor import Project
+from .processor import Task
+from .processor import Module
+from .processor import Processor
+from .processor import CollectionType
+from .processor import ProcessorError
---
+++ packages/dpt/dpt-1.2.3.tar.gz/dpt-1.2.3/dpt/deployment/common.py
@@ -0,0 +1,4 @@
+coll_prefix = "sys_python_"
+sys_db_name = "dpt_python"
+mongo_uri: str = None 
+workspace_name: str = None 
