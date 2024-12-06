6145ced0a75a46fef9b5cce58a6bcb079506180f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:20 +0000

    Add package aqmp-compressor/0.1.6/aqmp-compressor-0.1.6.tar.gz

---
+++ packages/aqmp-compressor/aqmp-compressor-0.1.6.tar.gz/aqmp-compressor-0.1.6/aqmp/__init__.py
@@ -0,0 +1,1 @@
+from .aqmp import ImageCompressor
---
+++ packages/aqmp-compressor/aqmp-compressor-0.1.6.tar.gz/aqmp-compressor-0.1.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aqmp-compressor",
+    version="0.1.6",
+    description="A Python package for compressing and decompressing images using OMP and wavelet transforms.",
+    author="Emmanuel A. Tassone",
+    author_email="emmanueltassone@gmail.com",
+    url="https://github.com/Emmatassone/AQMP",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "Pillow",
+        "PyWavelets",
+        "anytree"
+    ],
+    entry_points={
+        "console_scripts": [
+            "aqmp=aqmp.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='<=3.11, >=3.9',
+)
