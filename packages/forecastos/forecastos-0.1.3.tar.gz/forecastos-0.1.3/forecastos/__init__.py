9f2b6b32502df14050ce6d83971b118b3f873cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:01 +0000

    Add package forecastos/0.1.3/forecastos-0.1.3.tar.gz

---
+++ packages/forecastos/forecastos-0.1.3.tar.gz/forecastos-0.1.3/forecastos/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+api_key = os.environ.get("FORECASTOS_API_KEY", "")
+api_endpoint = "https://app.forecastos.com/api/v1"
+
+from forecastos.feature import *
+from forecastos.provider import *
+from forecastos.global_utils import *
