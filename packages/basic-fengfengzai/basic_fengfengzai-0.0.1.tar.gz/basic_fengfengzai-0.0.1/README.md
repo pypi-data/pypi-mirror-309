2f46f61bf8d0a95bb28e47e8d2e78a54b212abcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:45 +0000

    Add package basic-fengfengzai/0.0.1/basic_fengfengzai-0.0.1.tar.gz

---
+++ packages/basic-fengfengzai/basic_fengfengzai-0.0.1.tar.gz/basic_fengfengzai-0.0.1/README.md
@@ -0,0 +1,12 @@
+# this is a simple test
+
+this is a simple python package for test pypi
+
+
+## for example
+
+from multiply.multi_by_three import multiply_by_three
+from divide.divide_by_three import divide_by_three
+
+multiply_by_three(9)
+divide_by_three(21)
---
+++ packages/basic-fengfengzai/basic_fengfengzai-0.0.1.tar.gz/basic_fengfengzai-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="basic_fengfengzai",
+    version="0.0.1",
+    author="dufeng1010",
+    author_email="dufeng@example.com",
+    description="short package description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://example.com",
+    project_urls = {
+        "Bug Tracker": "http://example.com",
+    },
+    package_dir= {"": "src"},
+    packages= setuptools.find_packages(where="src"),
+    python_requires=">=3.6"
+)
