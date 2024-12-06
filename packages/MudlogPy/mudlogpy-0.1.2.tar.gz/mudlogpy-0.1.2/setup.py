c156521c786398cf8f942007dd4525328201562a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:25 +0000

    Add package MudlogPy/0.1.2/mudlogpy-0.1.2.tar.gz

---
+++ packages/MudlogPy/mudlogpy-0.1.2.tar.gz/mudlogpy-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MudlogPy',
+    packages=find_packages(include=['MudlogPy']),
+    version='0.1.2',
+    description='This library plots the mudlog having gas chromatograph and lithology logs',
+    author='Landmark Resources Reservoir Characterization Department',
+    install_requires=[],
+    setup_requires=['numpy','lasio','matplotlib'],
+    tests_require=['numpy==1.26.4','lasio==0.31','matplotlib==3.7.5']
+)
