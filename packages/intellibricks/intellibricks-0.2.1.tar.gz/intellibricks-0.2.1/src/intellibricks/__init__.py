9f1140c0100677dcf22427b613c5b0decfb25777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:55 +0000

    Add package intellibricks/0.2.1/intellibricks-0.2.1.tar.gz

---
+++ packages/intellibricks/intellibricks-0.2.1.tar.gz/intellibricks-0.2.1/src/intellibricks/__init__.py
@@ -0,0 +1,31 @@
+from .llms import (
+    AIModel,
+    CompletionEngine,
+    CompletionEngineProtocol,
+    CompletionOutput,
+    Delta,
+    Message,
+    MessageChoice,
+    MessageRole,
+    ObservationParams,
+    StreamChoice,
+    TraceParams,
+    Usage,
+    Prompt,
+)
+
+__all__: list[str] = [
+    "CompletionEngine",
+    "CompletionOutput",
+    "AIModel",
+    "Usage",
+    "Message",
+    "MessageChoice",
+    "StreamChoice",
+    "Delta",
+    "MessageRole",
+    "CompletionEngineProtocol",
+    "TraceParams",
+    "ObservationParams",
+    "Prompt",
+]
