f57cc6e9f50d560b62d865fcdff272810df212a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:41 +0000

    Add package PyroArgs/1.3/pyroargs-1.3.tar.gz

---
+++ packages/PyroArgs/pyroargs-1.3.tar.gz/pyroargs-1.3/PyroArgs/__init__.py
@@ -0,0 +1,7 @@
+# PyroArgs/__init__.py
+from . import types, errors
+from .utils import DataHolder
+from .pyroargs import PyroArgs
+
+__all__ = ['PyroArgs', 'types', 'errors', 'DataHolder']
+__version__ = '1.3'  # ВЕРСИЯ
