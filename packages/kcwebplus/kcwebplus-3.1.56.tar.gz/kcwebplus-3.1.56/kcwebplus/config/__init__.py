7edc46832a8c8fe0a7373c944fb8a99f7bd13974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:28 +0000

    Add package kcwebplus/3.1.56/kcwebplus-3.1.56.tar.gz

---
+++ packages/kcwebplus/kcwebplus-3.1.56.tar.gz/kcwebplus-3.1.56/kcwebplus/config/__init__.py
@@ -0,0 +1,15 @@
+from .app import *
+kcwebplus={}
+kcwebplus['name']='kcwebplus'                             #项目的名称
+kcwebplus['version']='3.1.56'							#项目版本
+kcwebplus['description']='kcwebplus是建立在 kcweb 框架的基础上研发的一套 linux 服务器容器系统'       #项目的简单描述
+kcwebplus['long_description']='kcwebplus是建立在 kcweb 框架的基础上研发的一套 linux 服务器容器系统,其目的是为了解决可视化部署和架构的过程，系统本身是一款服务端管理软件，通过 web 可视化方式管理服务器，提升运维效率'     #项目详细描述
+kcwebplus['license']='Apache License 2.0'                    #开源协议
+kcwebplus['url']='https://docs.kwebapp.cn/index/index/3'
+kcwebplus['author']='百里-坤坤'  					 #名字
+kcwebplus['author_email']='kcweb@kwebapp.cn' 	     #邮件地址
+kcwebplus['maintainer']='百里' 						 #维护人员的名字
+kcwebplus['maintainer_email']='fk1402936534@qq.com'    #维护人员的邮件地址
+
+fdgrsgrsegsrsgrsbsdbftbrsbfdrtrtbdfsrsgr=kcwebplus['name'] #不要修改改参数，否则无法上传模块和插件
+
