d742ddc14d79af276e4d5028c21a3b47232bc8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:21 +0000

    Add package gencove/2.24.2/gencove-2.24.2-py2.py3-none-any.whl

---
+++ packages/gencove/gencove-2.24.2-py2.py3-none-any.whl/gencove/command/explorer/data/ls/main.py
@@ -0,0 +1,66 @@
+"""Configure explorer data ls subcommand."""
+import sys
+
+from ..common import (
+    GencoveExplorerManager,
+    request_is_from_explorer,
+    validate_explorer_user_data,
+)
+from ....base import Command
+from ....utils import user_has_aws_in_path
+
+
+class List(Command):
+    """List Explorer contents command executor."""
+
+    def __init__(self, ctx, path, credentials, options):
+        super().__init__(credentials, options)
+        self.ctx = ctx
+        self.path = path
+
+        # Populated after self.login() is called
+        self.user = None
+        self.organization = None
+        self.aws_session_credentials = None
+
+        # populated after self.execute() is called
+        self.organization_users = None
+
+    def validate(self):
+        """Validate ls"""
+        validate_explorer_user_data(self.user, self.organization)
+        user_has_aws_in_path(raise_exception=True)
+
+    def initialize(self):
+        """Initialize ls subcommand."""
+        self.login()
+        self.user = self.api_client.get_user_details()
+        self.organization = self.api_client.get_organization_details()
+
+        if not request_is_from_explorer():
+            self.aws_session_credentials = (
+                self.api_client.get_explorer_data_credentials()
+            )
+
+    def execute(self):
+        """Make a request to list Explorer objects."""
+        self.echo_debug("List Explorer contents.")
+
+        explorer_manager = GencoveExplorerManager(
+            aws_session_credentials=self.aws_session_credentials,
+            user_id=str(self.user.id),
+            organization_id=str(self.organization.id),
+            organization_users=self.api_client.get_organization_users(),
+        )
+
+        if self.path == explorer_manager.EXPLORER_SCHEME:
+            for namespace_key in explorer_manager.NAMESPACE_KEYS:
+                print(f"{explorer_manager.EXPLORER_SCHEME}{namespace_key}")
+            sys.exit(0)
+        if (
+            self.path.rstrip("/")
+            == f"{explorer_manager.EXPLORER_SCHEME}{explorer_manager.USERS}"
+        ):
+            explorer_manager.list_users()
+            sys.exit(0)
+        explorer_manager.execute_aws_s3_path("ls", self.path, self.ctx.args)
