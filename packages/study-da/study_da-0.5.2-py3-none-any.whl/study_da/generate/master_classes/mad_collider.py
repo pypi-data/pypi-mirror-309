b72591d4f779bd22e50adf702dba423b4877457c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:18 +0000

    Add package study-da/0.5.2/study_da-0.5.2-py3-none-any.whl

---
+++ packages/study-da/study_da-0.5.2-py3-none-any.whl/study_da/generate/master_classes/mad_collider.py
@@ -0,0 +1,333 @@
+"""This class is used to build a Xsuite collider from a madx sequence and optics."""
+
+# ==================================================================================================
+# --- Imports
+# ==================================================================================================
+
+# Import standard library modules
+import logging
+import os
+import shutil
+from typing import Any
+
+# Import third-party modules
+import xmask as xm
+import xmask.lhc as xlhc
+import xtrack as xt
+from cpymad.madx import Madx
+
+# Import user-defined modules
+from ..version_specific_files.hllhc13 import optics_specific_tools as ost_hllhc13
+from ..version_specific_files.hllhc16 import optics_specific_tools as ost_hllhc16
+from ..version_specific_files.runIII import optics_specific_tools as ost_runIII
+from ..version_specific_files.runIII_ions import (
+    optics_specific_tools as ost_runIII_ions,
+)
+from .utils import compress_and_write
+
+# ==================================================================================================
+# --- Class definition
+# ==================================================================================================
+
+
+class MadCollider:
+    """
+    MadCollider class is responsible for setting up and managing the collider environment using
+    MAD-X and xsuite.
+
+    Attributes:
+        sanity_checks (bool): Flag to enable or disable sanity checks.
+        links (str): Path to the links configuration.
+        beam_config (dict): Configuration for the beam.
+        optics (str): Path to the optics file.
+        enable_imperfections (bool): Flag to enable or disable imperfections.
+        enable_knob_synthesis (bool): Flag to enable or disable knob synthesis.
+        rename_coupling_knobs (bool): Flag to enable or disable renaming of coupling knobs.
+        pars_for_imperfections (dict): Parameters for imperfections.
+        ver_lhc_run (float | None): Version of LHC run.
+        ver_hllhc_optics (float | None): Version of HL-LHC optics.
+        ions (bool): Flag to indicate if ions are used.
+        phasing (dict): Phasing configuration.
+        path_collider_file_for_configuration_as_output (str): Path to save the collider.
+        compress (bool): Flag to enable or disable compression of collider file.
+
+    Methods:
+        ost: Property to get the appropriate optics specific tools.
+        prepare_mad_collider() -> tuple[Madx, Madx]: Prepares the MAD-X collider environment.
+        build_collider(mad_b1b2: Madx, mad_b4: Madx) -> xt.Multiline: Builds the xsuite collider.
+        activate_RF_and_twiss(collider: xt.Multiline) -> None: Activates RF and performs twiss analysis.
+        check_xsuite_lattices(line: xt.Line) -> None: Checks the xsuite lattices.
+        write_collider_to_disk(collider: xt.Multiline) -> None: Writes the collider to disk and
+            optionally compresses it.
+        clean_temporary_files() -> None: Cleans up temporary files created during the process.
+    """
+
+    def __init__(self, configuration: dict):
+        """
+        Initializes the MadCollider class with the given configuration.
+
+        Args:
+            configuration (dict): A dictionary containing the following keys:
+                - sanity_checks (bool): Flag to enable or disable sanity checks.
+                - links (str): Path to the links configuration.
+                - beam_config (dict): Configuration for the beam.
+                - optics_file (str): Path to the optics file.
+                - enable_imperfections (bool): Flag to enable or disable imperfections.
+                - enable_knob_synthesis (bool): Flag to enable or disable knob synthesis.
+                - rename_coupling_knobs (bool): Flag to enable or disable renaming of coupling
+                    knobs.
+                - pars_for_imperfections (dict): Parameters for imperfections.
+                - ver_lhc_run (float | None): Version of the LHC run, if applicable.
+                - ver_hllhc_optics (float | None): Version of the HL-LHC optics, if applicable.
+                - ions (bool): Flag to indicate if ions are used.
+                - phasing (dict): Configuration for phasing.
+                - path_collider_file_for_configuration_as_output (str): Path to the collider.
+                - compress (bool): Flag to enable or disable compression.
+        """
+        # Configuration variables
+        self.sanity_checks: bool = configuration["sanity_checks"]
+        self.links: str = configuration["links"]
+        self.beam_config: dict = configuration["beam_config"]
+        self.optics: str = configuration["optics_file"]
+        self.enable_imperfections: bool = configuration["enable_imperfections"]
+        self.enable_knob_synthesis: bool = configuration["enable_knob_synthesis"]
+        self.rename_coupling_knobs: bool = configuration["rename_coupling_knobs"]
+        self.pars_for_imperfections: dict = configuration["pars_for_imperfections"]
+        self.ver_lhc_run: float | None = configuration["ver_lhc_run"]
+        self.ver_hllhc_optics: float | None = configuration["ver_hllhc_optics"]
+        self.ions: bool = configuration["ions"]
+        self.phasing: dict = configuration["phasing"]
+
+        # Optics specific tools
+        self._ost = None
+
+        # Path to disk and compression
+        self.path_collider_file_for_configuration_as_output = configuration[
+            "path_collider_file_for_configuration_as_output"
+        ]
+        self.compress = configuration["compress"]
+
+    @property
+    def ost(self) -> Any:
+        """
+        Determines and returns the appropriate optics-specific tools (OST) based on the
+        version of HLLHC optics or LHC run configuration.
+
+        Raises:
+            ValueError: If both `ver_hllhc_optics` and `ver_lhc_run` are defined.
+            ValueError: If no optics-specific tools are available for the given configuration.
+
+        Returns:
+            Any: The appropriate OST module based on the configuration.
+        """
+        if self._ost is None:
+            # Check that version is well defined
+            if self.ver_hllhc_optics is not None and self.ver_lhc_run is not None:
+                raise ValueError("Only one of ver_hllhc_optics and ver_lhc_run can be defined")
+
+            # Get the appropriate optics_specific_tools
+            if self.ver_hllhc_optics is not None:
+                match self.ver_hllhc_optics:
+                    case 1.6:
+                        self._ost = ost_hllhc16
+                    case 1.3:
+                        self._ost = ost_hllhc13
+                    case _:
+                        raise ValueError("No optics specific tools for this configuration")
+            elif self.ver_lhc_run == 3.0:
+                self._ost = ost_runIII_ions if self.ions else ost_runIII
+            else:
+                raise ValueError("No optics specific tools for the provided configuration")
+
+        return self._ost
+
+    def prepare_mad_collider(self) -> tuple[Madx, Madx]:
+        # sourcery skip: extract-duplicate-method
+        """
+        Prepares the MAD-X collider environment and sequences for beam 1/2 and beam 4.
+
+        This method performs the following steps:
+        1. Creates the MAD-X environment using the provided links.
+        2. Initializes MAD-X instances for beam 1/2 and beam 4 with respective command logs.
+        3. Builds the sequences for both beams using the provided beam configuration.
+        4. Applies the specified optics to the beam 1/2 sequence.
+        5. Optionally performs sanity checks on the beam 1/2 sequence by running TWISS and checking
+            the MAD-X lattices.
+        6. Applies the specified optics to the beam 4 sequence.
+        7. Optionally performs sanity checks on the beam 4 sequence by running TWISS and checking
+            the MAD-X lattices.
+
+        Returns:
+            tuple[Madx, Madx]: A tuple containing the MAD-X instances for beam 1/2 and beam 4.
+        """
+        # Make mad environment
+        xm.make_mad_environment(links=self.links)
+
+        # Start mad
+        mad_b1b2 = Madx(command_log="mad_collider.log")
+        mad_b4 = Madx(command_log="mad_b4.log")
+
+        # Build sequences
+        self.ost.build_sequence(mad_b1b2, mylhcbeam=1, beam_config=self.beam_config)
+        self.ost.build_sequence(mad_b4, mylhcbeam=4, beam_config=self.beam_config)
+
+        # Apply optics (only for b1b2, b4 will be generated from b1b2)
+        self.ost.apply_optics(mad_b1b2, optics_file=self.optics)
+
+        if self.sanity_checks:
+            mad_b1b2.use(sequence="lhcb1")
+            mad_b1b2.twiss()
+            self.ost.check_madx_lattices(mad_b1b2)
+            mad_b1b2.use(sequence="lhcb2")
+            mad_b1b2.twiss()
+            self.ost.check_madx_lattices(mad_b1b2)
+
+        # Apply optics (only for b4, just for check)
+        self.ost.apply_optics(mad_b4, optics_file=self.optics)
+        if self.sanity_checks:
+            mad_b4.use(sequence="lhcb2")
+            mad_b4.twiss()
+            # ! Investigate why this is failing for run III
+            try:
+                self.ost.check_madx_lattices(mad_b4)
+            except AssertionError:
+                logging.warning("Some sanity checks have failed during the madx lattice check")
+
+        return mad_b1b2, mad_b4
+
+    def build_collider(self, mad_b1b2: Madx, mad_b4: Madx) -> xt.Multiline:
+        """
+        Build an xsuite collider using provided MAD-X sequences and configuration.
+
+        Parameters:
+        mad_b1b2 (Madx): MAD-X instance containing sequences for beam 1 and beam 2.
+        mad_b4 (Madx): MAD-X instance containing sequence for beam 4.
+
+        Returns:
+        xt.Multiline: Constructed xsuite collider.
+
+        Notes:
+        - Converts `ver_lhc_run` and `ver_hllhc_optics` to float if they are not None.
+        - Builds the xsuite collider with the specified sequences and configuration.
+        - Optionally performs sanity checks by computing Twiss parameters for beam 1 and beam 2.
+        """
+        # Ensure proper types to avoid assert errors
+        if self.ver_lhc_run is not None:
+            self.ver_lhc_run = float(self.ver_lhc_run)
+        if self.ver_hllhc_optics is not None:
+            self.ver_hllhc_optics = float(self.ver_hllhc_optics)
+
+        # Build xsuite collider
+        collider = xlhc.build_xsuite_collider(
+            sequence_b1=mad_b1b2.sequence.lhcb1,
+            sequence_b2=mad_b1b2.sequence.lhcb2,
+            sequence_b4=mad_b4.sequence.lhcb2,
+            beam_config=self.beam_config,
+            enable_imperfections=self.enable_imperfections,
+            enable_knob_synthesis=self.enable_knob_synthesis,
+            rename_coupling_knobs=self.rename_coupling_knobs,
+            pars_for_imperfections=self.pars_for_imperfections,
+            ver_lhc_run=self.ver_lhc_run,
+            ver_hllhc_optics=self.ver_hllhc_optics,
+        )
+        collider.build_trackers()
+
+        if self.sanity_checks:
+            collider["lhcb1"].twiss(method="4d")
+            collider["lhcb2"].twiss(method="4d")
+
+        return collider
+
+    def activate_RF_and_twiss(self, collider: xt.Multiline) -> None:
+        """
+        Activates RF and Twiss parameters for the given collider.
+
+        This method sets the RF knobs for the collider using the values specified
+        in the `phasing` attribute. It also performs sanity checks on the collider
+        lattices if the `sanity_checks` attribute is set to True.
+
+        Args:
+            collider (xt.Multiline): The collider object to configure.
+
+        Returns:
+            None
+        """
+        # Define a RF knobs
+        collider.vars["vrf400"] = self.phasing["vrf400"]
+        collider.vars["lagrf400.b1"] = self.phasing["lagrf400.b1"]
+        collider.vars["lagrf400.b2"] = self.phasing["lagrf400.b2"]
+
+        if self.sanity_checks:
+            for my_line in ["lhcb1", "lhcb2"]:
+                self.check_xsuite_lattices(collider[my_line])
+
+    def check_xsuite_lattices(self, line: xt.Line) -> None:
+        """
+        Check the Twiss parameters and tune values for a given xsuite Line object.
+
+        This method computes the Twiss parameters for the provided `line` using the
+        6-dimensional method with a specified matrix stability tolerance. It then
+        prints the Twiss results at all interaction points (IPs) and the horizontal
+        (Qx) and vertical (Qy) tune values.
+
+        Args:
+            line (xt.Line): The xsuite Line object for which to compute and display
+                            the Twiss parameters and tune values.
+
+        Returns:
+            None
+        """
+        tw = line.twiss(method="6d", matrix_stability_tol=100)
+        print(f"--- Now displaying Twiss result at all IPS for line {line}---")
+        print(tw.rows["ip.*"])
+        # print qx and qy
+        print(f"--- Now displaying Qx and Qy for line {line}---")
+        print(tw.qx, tw.qy)
+
+    def write_collider_to_disk(self, collider: xt.Multiline) -> None:
+        """
+        Writes the collider object to disk in JSON format and optionally compresses it into a ZIP
+        file.
+
+        Args:
+            collider (xt.Multiline): The collider object to be saved.
+
+        Returns:
+            None
+
+        Raises:
+            OSError: If there is an issue creating the directory or writing the file.
+
+        Notes:
+            - The method ensures that the directory specified in
+                `self.path_collider_file_for_configuration_as_output` exists.
+            - If `self.compress` is True, the JSON file is compressed into a ZIP file to reduce
+                storage usage.
+        """
+        # Save collider to json, creating the folder if it does not exist
+        if "/" in self.path_collider_file_for_configuration_as_output:
+            os.makedirs(self.path_collider_file_for_configuration_as_output, exist_ok=True)
+        collider.to_json(self.path_collider_file_for_configuration_as_output)
+
+        # Compress the collider file to zip to ease the load on afs
+        if self.compress:
+            compress_and_write(self.path_collider_file_for_configuration_as_output)
+
+    @staticmethod
+    def clean_temporary_files() -> None:
+        """
+        Remove all the temporary files created in the process of building the collider.
+
+        This function deletes the following files and directories:
+        - "mad_collider.log"
+        - "mad_b4.log"
+        - "temp" directory
+        - "errors"
+        - "acc-models-lhc"
+        """
+        # Remove all the temporaty files created in the process of building collider
+        os.remove("mad_collider.log")
+        os.remove("mad_b4.log")
+        shutil.rmtree("temp")
+        os.unlink("errors")
+        os.unlink("acc-models-lhc")
---
+++ packages/study-da/study_da-0.5.2-py3-none-any.whl/study_da/postprocess/postprocess.py
@@ -0,0 +1,298 @@
+"""
+This module provides functions to process and analyze simulation output data.
+
+Functions:
+    get_particles_data() -> List[pd.DataFrame]:
+
+    add_parameters_from_config() -> List[pd.DataFrame]:
+
+    merge_and_group_by_parameters_of_interest() -> pd.DataFrame:
+
+    aggregate_output_data() -> pd.DataFrame:
+
+    fix_LHC_version() -> pd.DataFrame:
+"""
+
+# ==================================================================================================
+# --- Imports
+# ==================================================================================================
+# Standard library imports
+import inspect
+import logging
+import os
+from typing import Any, Callable, Dict, List, Optional
+
+# Third party imports
+import numpy as np
+import pandas as pd
+
+# Local imports
+from study_da.submit.config_jobs import ConfigJobs
+from study_da.utils import load_dic_from_path, nested_get
+
+
+# ==================================================================================================
+# --- Functions to browse simulations folder and extract relevant observables
+# ==================================================================================================
+def get_particles_data(
+    dic_all_jobs: Dict[str, Dict[str, List[str]]],
+    absolute_path_study: str,
+    generation_of_interest: int = 2,
+    name_output: str = "output_particles.parquet",
+) -> List[pd.DataFrame]:
+    """
+    Retrieves particle data from simulation output files.
+
+    Args:
+        dic_all_jobs (dict): Dictionary containing all jobs and their details.
+        absolute_path_study (str): The absolute path to the study directory.
+        generation_of_interest (int, optional): The generation of interest. Defaults to 2.
+        name_output (str, optional): The name of the output file.
+            Defaults to "output_particles.parquet".
+
+    Returns:
+        list: A list of DataFrames containing the particle data.
+    """
+
+    # Loop over all jobs and extract the output data
+    l_df_output = []
+    for relative_path_job, dic_job in dic_all_jobs.items():
+        if dic_job["gen"] != generation_of_interest:
+            continue
+        absolute_path_job = os.path.join(absolute_path_study, relative_path_job)
+        absolute_folder_job = os.path.dirname(absolute_path_job)
+        try:
+            df_output = pd.read_parquet(os.path.join(absolute_folder_job, name_output))
+        except FileNotFoundError as e:
+            logging.warning(f"File not found: {e}")
+            continue
+        except Exception as e:
+            logging.warning(f"Error reading parquet file: {e}")
+            continue
+
+        # Register path of the job
+        df_output["name base collider"] = relative_path_job
+
+        # Add to the list
+        l_df_output.append(df_output)
+
+    return l_df_output
+
+
+def add_parameters_from_config(
+    l_df_output: List[pd.DataFrame],
+    dic_parameters_of_interest: Dict[str, List[str]],
+    default_path_template_parameters: bool = False,
+) -> List[pd.DataFrame]:
+    """
+    Adds parameters from the configuration to the output data.
+
+    Args:
+        l_df_output (list): List of DataFrames containing the output data.
+        dic_parameters_of_interest (dict): Dictionary of parameters of interest.
+        default_path_template_parameters (bool, optional): Flag to indicate if the default path
+            template parameters are used. If True, less caution is applied in the checking of the
+            parameters. Defaults to False.
+
+    Returns:
+        list: A list of DataFrames with added parameters.
+    """
+
+    for df_output in l_df_output:
+        # Get generation configurations as dictionnaries for parameter assignation
+        dic_configuration = df_output.attrs["configuration"]
+
+        # Select simulations parameters of interest
+        for name_param, l_path_param in dic_parameters_of_interest.items():
+            try:
+                df_output[name_param] = nested_get(dic_configuration, l_path_param)
+            except KeyError:
+                # Only be verbose if the dic_parameters_of_interest has not been provided by the user
+                if not default_path_template_parameters:
+                    logging.warning(f"Parameter {name_param} not found in the configuration file")
+
+    return l_df_output
+
+
+def merge_and_group_by_parameters_of_interest(
+    l_df_output: List[pd.DataFrame],
+    l_group_by_parameters: List[str],
+    only_keep_lost_particles: bool = True,
+    l_parameters_to_keep: Optional[List[str]] = None,
+    function_to_aggregate: Callable = min,
+) -> pd.DataFrame:
+    """
+    Merges and groups the output data by parameters of interest.
+
+    Args:
+        l_df_output (list): List of DataFrames containing the output data.
+        l_group_by_parameters (list): List of parameters to group by.
+        only_keep_lost_particles (bool, optional): Flag to indicate if only lost particles should
+            be kept. Defaults to True.
+        l_parameters_to_keep (list, optional): List of parameters to keep. Defaults to None.
+        function_to_aggregate (callable, optional): Function to aggregate the grouped data.
+            Defaults to min.
+
+    Returns:
+        pd.DataFrame: The merged and grouped DataFrame.
+    """
+    # Merge all dataframes
+    df_all_sim = pd.concat(l_df_output)
+
+    # Handle mutable default arguments
+    if l_parameters_to_keep is None:
+        logging.info("No list of parameters to keep provided, keeping all available parameters")
+        l_parameters_to_keep = list(df_all_sim.columns)
+
+    if only_keep_lost_particles:
+        # Extract the particles that were lost for DA computation
+        df_all_sim = df_all_sim[df_all_sim["state"] != 1]
+
+    # Check if the dataframe is empty
+    if df_all_sim.empty:
+        logging.warning("No unstable particles found, the output dataframe will be empty.")
+
+    # Group by parameters of interest
+    df_grouped = df_all_sim.groupby(l_group_by_parameters)
+
+    # Return the grouped dataframe, keeping only the minimum values of the parameters of interest
+    # (should not have impact except for DA, which we want to be minimal)
+    # return pd.DataFrame(
+    #     [df_grouped[parameter].min() for parameter in l_parameters_to_keep]
+    # ).transpose()
+    return pd.DataFrame(
+        [df_grouped[parameter].agg(function_to_aggregate) for parameter in l_parameters_to_keep]
+    ).transpose()
+
+
+def aggregate_output_data(
+    path_tree: str,
+    l_group_by_parameters: List[str],
+    function_to_aggregate: Callable = min,
+    generation_of_interest: int = 2,
+    name_output: str = "output_particles.parquet",
+    write_output: bool = True,
+    path_output: Optional[str] = None,
+    only_keep_lost_particles: bool = True,
+    dic_parameters_of_interest: Optional[Dict[str, List[str]]] = None,
+    l_parameters_to_keep: Optional[List[str]] = None,
+    name_template_parameters: str = "parameters_lhc.yaml",
+    path_template_parameters: Optional[str] = None,
+    force_overwrite: bool = False,
+) -> pd.DataFrame:
+    """
+    Aggregates output data from simulation files.
+
+    Args:
+        path_tree (str): The path to the tree file.
+        l_group_by_parameters (list): List of parameters to group by.
+        function_to_aggregate (callable, optional): Function to aggregate the grouped data.
+        generation_of_interest (int, optional): The generation of interest. Defaults to 2.
+        name_output (str, optional): The name of the output file. Defaults to "output_particles.parquet".
+        write_output (bool, optional): Flag to indicate if the output should be written to a file.
+            Defaults to True.
+        path_output (str, optional): The path to the output file. If not provided, the default
+            output file will be in the study folder as 'da.parquet'. Defaults to None.
+        only_keep_lost_particles (bool, optional): Flag to indicate if only lost particles should be
+            kept. Defaults to True.
+        dic_parameters_of_interest (dict, optional): Dictionary of parameters of interest. Defaults
+            to None.
+        l_parameters_to_keep (list, optional): List of parameters to keep. Defaults to None.
+        name_template_parameters (str, optional): The name of the template parameters file
+            associating each parameter to a list of keys. Defaults to "parameters_lhc.yaml", which
+            is already contained in the study-da package, and includes the main usual parameters.
+        path_template_parameters (str, optional): The path to the template parameters file. Must
+            be provided if a no template already contained in study-da is provided through the
+            argument name_template_parameters. Defaults to None.
+        force_overwrite (bool, optional): Flag to indicate if the output file should be overwritten
+            if it already exists. Defaults to False.
+
+    Returns:
+        pd.DataFrame: The final aggregated DataFrame.
+    """
+    # Check it the output doesn't already exist and ask for confirmation to overwrite
+    dic_tree, _ = load_dic_from_path(path_tree)
+    absolute_path_study = dic_tree["absolute_path"]
+    if path_output is None:
+        path_output = os.path.join(absolute_path_study, "da.parquet")
+    if os.path.exists(path_output) and not force_overwrite:
+        input_user = input(
+            f"The output file {path_output} already exists. Do you want to overwrite it? (y/n) "
+        )
+        if input_user.lower() != "y":
+            logging.warning("Output file not overwritten")
+            return pd.read_parquet(path_output)
+
+    logging.info("Analysis of output simulation files started")
+
+    dic_all_jobs = ConfigJobs(dic_tree).find_all_jobs()
+
+    l_df_sim = get_particles_data(
+        dic_all_jobs, absolute_path_study, generation_of_interest, name_output
+    )
+
+    default_path_template_parameters = False
+    if dic_parameters_of_interest is None:
+        if path_template_parameters is not None:
+            logging.info("Loading parameters of interest from the provided configuration file")
+        else:
+            if name_template_parameters is None:
+                raise ValueError(
+                    "No template configuration file provided for the parameters of interest"
+                )
+            logging.info("Loading parameters of interest from the template configuration file")
+            path_template_parameters = os.path.join(
+                os.path.dirname(inspect.getfile(aggregate_output_data)),
+                "configs",
+                name_template_parameters,
+            )
+            default_path_template_parameters = True
+        dic_parameters_of_interest, _ = load_dic_from_path(path_template_parameters)
+
+    l_df_output = add_parameters_from_config(
+        l_df_sim, dic_parameters_of_interest, default_path_template_parameters
+    )
+
+    df_final = merge_and_group_by_parameters_of_interest(
+        l_df_output,
+        l_group_by_parameters,
+        only_keep_lost_particles,
+        l_parameters_to_keep,
+        function_to_aggregate,
+    )
+
+    # Fix the LHC version type
+    df_final = fix_LHC_version(df_final)
+
+    if write_output:
+        df_final.to_parquet(path_output)
+    elif path_output is not None:
+        logging.warning("Output path provided but write_output set to False, no output saved")
+
+    logging.info("Final dataframe for current set of simulations: %s", df_final)
+    return df_final
+
+
+def fix_LHC_version(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Fixes the LHC version type in the DataFrame.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to fix.
+
+    Returns:
+        pd.DataFrame: The fixed DataFrame.
+    """
+    # Fix the LHC version type
+    if "ver_lhc_run" in df.columns and not df["ver_lhc_run"].empty:
+        if df["ver_lhc_run"].isna().sum() != 0:
+            logging.warning("Some LHC version numbers are NaN... Ignoring")
+        else:
+            df["ver_lhc_run"] = df["ver_lhc_run"].astype("int32")
+    if "ver_hllhc_optics" in df.columns and not df["ver_hllhc_optics"].empty:
+        if df["ver_hllhc_optics"].isna().sum() != 0:
+            logging.warning("Some HLLHC optics version numbers are NaN... Ignoring")
+        else:
+            df["ver_hllhc_optics"] = df["ver_hllhc_optics"].astype("float32")
+
+    return df
