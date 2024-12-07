5e9ef87b40d061d1fe9ac7c50cdc19428c582cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:08:22 +0000

    Add package open-webui/0.4.1/open_webui-0.4.1-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.4.1-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/channel.DQmuaNSb.js
@@ -0,0 +1,2 @@
+import{U as a,C as n}from"./Messages.COgOHttK.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
+//# sourceMappingURL=channel.DQmuaNSb.js.map
---
+++ packages/open-webui/open_webui-0.4.1-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.Q_NPTKAg.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.C6z1vf7O.js";export{t as start};
+//# sourceMappingURL=start.Q_NPTKAg.js.map
