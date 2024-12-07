30a2aad2f6436c6241c66406c8948de3b47f7309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:00 +0000

    Add package fine-setup-tools/0.1.0/fine_setup_tools-0.1.0.tar.gz

---
+++ packages/fine-setup-tools/fine_setup_tools-0.1.0.tar.gz/fine_setup_tools-0.1.0/cmd.sh
@@ -0,0 +1,9 @@
+
+
+python -m nice_setup_tools /Users/zhanghang/Documents/code/my/hashkey/exlink_new/nice_logger fine_logger 0.1.0     -d "nice_logger"     -a "hhczy"     -e "hhczy1003@gmail.com"     --requires-python ">=3.8" --no-test
+
+
+python -m nice_setup_tools /Users/zhanghang/Documents/code/my/hashkey/exlink_new/nice_config fine_config 0.1.0     -d "nice_logger"     -a "hhczy"     -e "hhczy1003@gmail.com"     --requires-python ">=3.8" --no-test
+
+
+python -m nice_setup_tools /Users/zhanghang/Documents/code/my/hashkey/exlink_new/nice_celery fine_celery 0.1.0     -d "nice_logger"     -a "hhczy"     -e "hhczy1003@gmail.com"     --requires-python ">=3.8" --no-test
