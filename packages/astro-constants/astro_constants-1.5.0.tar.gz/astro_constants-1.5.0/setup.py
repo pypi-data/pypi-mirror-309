9fded613762845cb2ff05094dcf5cd7890f9e5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:26 +0000

    Add package astro-constants/1.5.0/astro_constants-1.5.0.tar.gz

---
+++ packages/astro-constants/astro_constants-1.5.0.tar.gz/astro_constants-1.5.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="astro_constants",
+    version="1.5.0",
+    description="A Python library for common astronomical and physical constants",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Explicitly specify Markdown format
+    author="JoeStem",
+    author_email="JoeStem25@gmail.com",
+    url="https://github.com/RedGloveProductions/Astronomy",  # Replace with your repo URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
