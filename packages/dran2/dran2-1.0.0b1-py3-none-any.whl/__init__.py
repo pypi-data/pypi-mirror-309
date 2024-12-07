228e9f5e675a86e771c2c84de7356585a02ec266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:55 +0000

    Add package dran2/1.0.0b1/dran2-1.0.0b1-py3-none-any.whl

---
+++ packages/dran2/dran2-1.0.0b1-py3-none-any.whl/__init__.py
@@ -0,0 +1,8 @@
+# Do not delete
+logFile="logfile.txt"
+__appname__ = "dran"
+__author__ = "Pfesesani van Zyl"
+__authoremail__ = "pfesi24@gmail.com"
+__package__ ="DRAN"
+__DBNAME__='HART26DATA.db'
+
---
+++ packages/dran2/dran2-1.0.0b1-py3-none-any.whl/config.py
@@ -0,0 +1,12 @@
+# Basic configuration
+# -------------------
+
+# App configuration
+APPNAME = "DRAN"
+VERSION = "1.0.01b"
+
+# Logging configuration
+LOGFILE = "logfile.txt"
+
+# Database configuration
+DBNAME = 'HART26DATA.db'
