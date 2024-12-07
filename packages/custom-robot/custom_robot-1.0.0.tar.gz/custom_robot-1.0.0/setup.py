9067ac61baec413fc452783b73d582488db4dcb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:46 +0000

    Add package custom-robot/1.0.0/custom_robot-1.0.0.tar.gz

---
+++ packages/custom-robot/custom_robot-1.0.0.tar.gz/custom_robot-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='custom-robot',
+    version='1.0.0',
+    description='Customized version of the robot package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Rishi Varma',
+    author_email='rishi@criterionnetworks.com',
+    license='GPL',
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies here if needed
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+)
+
