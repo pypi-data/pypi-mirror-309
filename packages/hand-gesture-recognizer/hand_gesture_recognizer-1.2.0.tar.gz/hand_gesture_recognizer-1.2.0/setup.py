2618778a321c086d220931c095556ea48f846f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:42 +0000

    Add package hand-gesture-recognizer/1.2.0/hand_gesture_recognizer-1.2.0.tar.gz

---
+++ packages/hand-gesture-recognizer/hand_gesture_recognizer-1.2.0.tar.gz/hand_gesture_recognizer-1.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+with open("Readme.md","r") as f:
+    description = f.read()
+setup(
+    name="hand_gesture_recognizer",
+    version="1.2.0",
+    description="A library for recognizing hand gestures using MediaPipe",
+    author="Umesh Singh Verma",
+    packages=find_packages(),
+    install_requires=["opencv-python", "mediapipe", "numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
