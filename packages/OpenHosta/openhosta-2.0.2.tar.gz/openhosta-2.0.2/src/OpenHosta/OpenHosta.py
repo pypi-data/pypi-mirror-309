e964c11c5dde09dc9b144c66302c9292be252b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:28 +0000

    Add package OpenHosta/2.0.2/openhosta-2.0.2.tar.gz

---
+++ packages/OpenHosta/openhosta-2.0.2.tar.gz/openhosta-2.0.2/src/OpenHosta/OpenHosta.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from .exec.ask import ask
+from .utils.meta_prompt import EMULATE_PROMPT
+from .exec.thinkof import thinkof
+from .exec.thought import thought
+from .exec.example import example
+from .exec.emulate import emulate
+from .core import config
+from .core.config import Model, DefaultManager
+from .utils.meta_prompt import print_last_prompt
+
+import os
+
+DefaultManager.set_default_model(
+    Model(model="gpt-4o", base_url="https://api.openai.com/v1/chat/completions",
+          api_key=os.getenv("OPENAI_API_KEY") or None)
+)
+
+all = (
+    "config",
+    "emulate",
+    "thought",
+    "example",
+    "thinkof",
+    "ask",
+    "EMULATE_PROMPT",
+    "print_last_prompt"
+)
