f698f0387867432531240679a0fccf2af1b9a566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:57 +0000

    Add package duckdb-appender/0.2.0/duckdb_appender-0.2.0.tar.gz

---
+++ packages/duckdb-appender/duckdb_appender-0.2.0.tar.gz/duckdb_appender-0.2.0/test/test_errors.py
@@ -0,0 +1,53 @@
+import duckdb
+import pytest
+
+from appender.errors import (
+    AppendAfterCloseError,
+    AppenderDoubleCloseError,
+    TableNotFoundError,
+    ColumnCountError,
+)
+from appender.appender import Appender
+
+
+TEST_DB = "test/test.duckdb"
+
+
+class TestErrors:
+
+    def test_table_not_found_error(self):
+        conn = duckdb.connect()
+        conn.execute("CREATE TABLE users (id INTEGER, name VARCHAR);")
+        conn.commit()
+
+        with pytest.raises(TableNotFoundError):
+            appender = Appender(conn, "main", "wrong_table_name")
+
+    def test_column_count_error(self):
+        conn = duckdb.connect()
+        conn.execute("CREATE TABLE users (id INTEGER, name VARCHAR);")
+        conn.commit()
+
+        appender = Appender(conn, "main", "users")
+        with pytest.raises(ColumnCountError):
+            appender.append_row([1, "John", "john@email.com"])
+
+    def test_close_appender_twice(self):
+        conn = duckdb.connect()
+        conn.execute("CREATE TABLE users (id INTEGER, name VARCHAR);")
+        conn.commit()
+
+        appender = Appender(conn, "main", "users")
+        appender.close()
+        with pytest.raises(AppenderDoubleCloseError):
+            appender.close()
+
+    def test_append_row_after_close(self):
+        conn = duckdb.connect()
+        conn.execute("CREATE TABLE users (id INTEGER, name VARCHAR);")
+        conn.commit()
+
+        appender = Appender(conn, "main", "users")
+        appender.close()
+        with pytest.raises(AppendAfterCloseError):
+            appender.append_row([1, "John Doe"])
