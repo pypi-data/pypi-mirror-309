b87e286a7030281fec1f165e21f803d4b8a1b8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:32 +0000

    Add package ssb-arbmark-fagfunksjoner/0.0.27/ssb_arbmark_fagfunksjoner-0.0.27-py3-none-any.whl

---
+++ packages/ssb-arbmark-fagfunksjoner/ssb_arbmark_fagfunksjoner-0.0.27-py3-none-any.whl/arbmark/__init__.py
@@ -0,0 +1,80 @@
+"""SSB Arbeidsmarked og lønn Fag-fellesfunksjoner."""
+
+from arbmark.functions.aggregation import proc_sums
+from arbmark.functions.categorize_ranges import categorize_ranges
+from arbmark.functions.files import read_latest
+from arbmark.functions.interval import pinterval
+from arbmark.functions.merge import indicate_merge
+from arbmark.functions.quarter import first_last_date_quarter
+from arbmark.functions.reference import ref_day
+from arbmark.functions.reference import ref_tuesday
+from arbmark.functions.reference import ref_week
+from arbmark.functions.statbank_formats import sb_integer
+from arbmark.functions.statbank_formats import sb_percent
+from arbmark.functions.workdays import count_days
+from arbmark.functions.workdays import count_holidays
+from arbmark.functions.workdays import count_weekend_days
+from arbmark.functions.workdays import count_workdays
+from arbmark.functions.workdays import filter_holidays
+from arbmark.functions.workdays import filter_weekends
+from arbmark.functions.workdays import filter_workdays
+from arbmark.functions.workdays import get_calendar
+from arbmark.functions.workdays import get_norwegian_holidays
+from arbmark.functions.workdays import get_years
+from arbmark.functions.workdays import is_weekend
+from arbmark.functions.workdays import numpy_dates
+from arbmark.groups.age import alder_5grp
+from arbmark.groups.age import alder_grp
+from arbmark.groups.company_size import virk_str_8grp
+from arbmark.groups.country_origin import landbakgrunn_grp
+from arbmark.groups.nace import clean_nace_17_groups
+from arbmark.groups.nace import nace_sn07_47grp
+from arbmark.groups.nace import nace_to_17_groups
+from arbmark.groups.occupation import nyk08yrkeregsys1
+from arbmark.groups.region import classify_county_not_mainland
+from arbmark.groups.region import classify_mainland_not_mainland
+from arbmark.groups.region import get_regional_special_codes
+from arbmark.groups.region import get_valid_county_codes
+from arbmark.groups.sector import sektor2_grp
+from arbmark.groups.shift_work import turnuskoder
+
+__all__ = [
+    "proc_sums",
+    "read_latest",
+    "pinterval",
+    "indicate_merge",
+    "first_last_date_quarter",
+    "ref_day",
+    "ref_tuesday",
+    "ref_week",
+    "sb_integer",
+    "sb_percent",
+    "count_days",
+    "count_holidays",
+    "count_weekend_days",
+    "count_workdays",
+    "count_weekend_days",
+    "filter_holidays",
+    "filter_weekends",
+    "filter_workdays",
+    "get_calendar",
+    "get_norwegian_holidays",
+    "get_years",
+    "is_weekend",
+    "numpy_dates",
+    "alder_grp",
+    "alder_5grp",
+    "virk_str_8grp",
+    "landbakgrunn_grp",
+    "clean_nace_17_groups",
+    "nace_to_17_groups",
+    "nace_sn07_47grp",
+    "sektor2_grp",
+    "turnuskoder",
+    "get_valid_county_codes",
+    "get_regional_special_codes",
+    "classify_mainland_not_mainland",
+    "classify_county_not_mainland",
+    "nyk08yrkeregsys1",
+    "categorize_ranges",
+]
