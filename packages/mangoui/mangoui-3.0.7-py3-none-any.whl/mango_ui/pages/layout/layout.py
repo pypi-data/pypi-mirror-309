da0ca855423d60d790cf96d5941f13128cef55a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:08:41 +0000

    Add package mangoui/3.0.7/mangoui-3.0.7-py3-none-any.whl

---
+++ packages/mangoui/mangoui-3.0.7-py3-none-any.whl/mango_ui/pages/layout/layout.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2024-11-02 21:24
+# @Author : 毛鹏
+
+from mango_ui import *
+from mango_ui.init import *
+
+
+class LayoutPage(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(MangoLabel('布局展示'))
+
+
+class Layout1Page(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(MangoLabel('布局展示1'))
+
+
+class Layout2Page(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(MangoLabel('布局展示2'))
+
+
+class Layout3Page(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(MangoLabel('布局展示3'))
+
+
+class Layout4Page(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(MangoLabel('布局展示4'))
