1653f0a9a960bcfb1b0578e59ad413a4bf163718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:08 +0000

    Add package janus-ssmm-tlm-info/0.0.6/janus_ssmm_tlm_info-0.0.6-py3-none-any.whl

---
+++ packages/janus-ssmm-tlm-info/janus_ssmm_tlm_info-0.0.6-py3-none-any.whl/janus_ssmm_tlm_info/cli.py
@@ -0,0 +1,29 @@
+import sys
+
+import spiceypy
+from loguru import logger as log
+
+from janus_ssmm_tlm_info.packets import ssm_file_info
+
+try:
+    import click
+except ImportError:
+    log.error(
+        "Click not found: if you need to use the cli tool, install janus_ssmm_tlm_info with its cli extra: pip install janus_ssmm_tlm_info[cli] or install click in your environment",
+    )
+    sys.exit(0)
+
+
+@click.command(name="janus-ssmm-tlm-info")
+@click.argument("filename", type=click.Path(exists=True, dir_okay=False), nargs=-1)
+@click.option("-m", "--metakernel", type=click.Path(exists=True, dir_okay=False))
+def main(filename: list[click.Path], metakernel: click.Path) -> None:
+
+
+    spiceypy.furnsh(str(metakernel))
+
+    for item in filename:
+        info = ssm_file_info(str(item))
+        print('------------------------------')
+        for key, value in info.items():
+            click.echo(f"{key}: {value}")
