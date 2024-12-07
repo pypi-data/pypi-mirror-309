9ebc873cb72e3dda0ef6790255ac03d3d1a7e501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:14 +0000

    Add package calkit-python/0.5.0/calkit_python-0.5.0.tar.gz

---
+++ packages/calkit-python/calkit_python-0.5.0.tar.gz/calkit_python-0.5.0/calkit/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.5.0"
+
+from .core import *
+from . import git
+from . import dvc
+from . import cloud
+from . import jupyter
+from . import config
+from . import models
+from . import office
+from . import templates
