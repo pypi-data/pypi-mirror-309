bab2cb77f6875b954a535c4a1af5588397dedbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:08 +0000

    Add package study-da/0.5.1/study_da-0.5.1.tar.gz

---
+++ packages/study-da/study_da-0.5.1.tar.gz/study_da-0.5.1/README.md
@@ -0,0 +1,39 @@
+# Dynamics Aperture Study Package
+
+<p align="center">
+    <img src="https://img.shields.io/pypi/v/study-DA" alt="PyPI - Version">
+    <img src="https://img.shields.io/github/commits-since/ColasDroin/study-DA/latest" alt="GitHub commits since latest release">
+    <img src="https://img.shields.io/github/actions/workflow/status/ColasDroin/study-DA/ci_tests.yaml?label=Tests" alt="GitHub Actions Workflow Status">
+    <img src="https://img.shields.io/github/actions/workflow/status/ColasDroin/study-DA/ci_docs.yaml?label=Doc" alt="GitHub Actions Workflow Status">
+    <img src="https://img.shields.io/github/license/ColasDroin/study-DA" alt="GitHub License">
+    <img src="https://img.shields.io/pypi/pyversions/study-DA" alt="PyPI - Python Version">
+</p>
+
+This package consists of a collection of tools to study the dynamics aperture of a particle accelerator built with [Xsuite](https://github.com/xsuite/xsuite). In a sense, it is a replacement of the [DA study template](https://github.com/xsuite/DA_study_template), but much more advanced. It also allows to configure colliders and do tracking without necessarily running parametric scans.
+
+The whole project is described in details in the [full documentation](https://colasdroin.github.io/study-DA/), along with tutorials and description of the implemented functions.
+
+## Installation
+
+The package is available on PyPI and can be installed using `pip`.
+
+```bash
+pip install study-da
+```
+
+Proper installation might require additional instructions. Please refer to the [full documentation](https://colasdroin.github.io/study-DA/) for more details.
+
+## Contributing
+
+We welcome contributions to the Dynamics Aperture Study Package. If you have any ideas, suggestions, or bug reports, please open an issue or submit a pull request.
+
+1. Fork the repository
+2. Create a new branch (`git checkout -b feature/yourfeature`)
+3. Make your changes
+4. Commit your changes (`git commit -am 'Add your feature'`)
+5. Push to the branch (`git push origin feature/yourfeature`)
+6. Create a new Pull Request
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ packages/study-da/study_da-0.5.1.tar.gz/study_da-0.5.1/study_da/submit/dependency_graph.py
@@ -0,0 +1,116 @@
+"""This module contains the DependencyGraph class to manage the dependencies between jobs"""
+
+# ==================================================================================================
+# --- Imports
+# ==================================================================================================
+# Standard library imports
+# Third party imports
+# Local imports
+from study_da.utils import nested_get
+
+
+# ==================================================================================================
+# --- Class
+# ==================================================================================================
+class DependencyGraph:
+    """
+    A class to manage the dependencies between jobs.
+
+    Attributes:
+        dic_tree (dict): The dictionary representing the job tree.
+        dic_all_jobs (dict): The dictionary containing all jobs and their details.
+        dependency_graph (dict): The dictionary representing the dependency graph.
+
+    Methods:
+        __init__(dic_tree, dic_all_jobs): Initializes the DependencyGraph class.
+        build_full_dependency_graph(): Builds the full dependency graph.
+        get_unfinished_dependency(job): Gets the list of unfinished dependencies for a given job.
+    """
+
+    def __init__(self, dic_tree: dict, dic_all_jobs: dict):
+        """
+        Initializes the DependencyGraph class.
+
+        Args:
+            dic_tree (dict): The dictionary representing the job tree.
+            dic_all_jobs (dict): The dictionary containing all jobs and their details.
+        """
+        self.dic_tree = dic_tree
+        self.dic_all_jobs = dic_all_jobs
+        self.dependency_graph = {}
+
+    def build_full_dependency_graph(self) -> dict:
+        """
+        Builds the full dependency graph.
+
+        Returns:
+            dict: The full dependency graph.
+        """
+        self.set_l_keys = {
+            tuple(self.dic_all_jobs[job]["l_keys"][:-1]) for job in self.dic_all_jobs
+        }
+        for job in self.dic_all_jobs:
+            l_keys = self.dic_all_jobs[job]["l_keys"]
+            self.dependency_graph[job] = set()
+            # Add all parents to the dependency graph
+            for i in range(len(l_keys) - 1):
+                l_keys_parent = l_keys[:i]
+                if tuple(l_keys_parent) in self.set_l_keys:
+                    parent = nested_get(self.dic_tree, l_keys_parent)
+                    # Look for all the jobs in the parent (but not the generations below)
+                    for name_parent, sub_dict in parent.items():
+                        if "file" in sub_dict:
+                            self.dependency_graph[job].add(sub_dict["file"])
+        return self.dependency_graph
+
+    def get_unfinished_dependency(self, job: str) -> list:
+        """
+        Gets the list of unfinished dependencies for a given job.
+
+        Args:
+            job (str): The name of the job.
+
+        Returns:
+            list: The list of unfinished dependencies.
+        """
+        # Ensure the dependency graph is built
+        if self.dependency_graph == {}:
+            self.build_full_dependency_graph()
+
+        # Get the list of dependencies
+        l_dependencies = self.dependency_graph[job]
+
+        # Get the corresponding list of l_keys
+        ll_keys = [self.dic_all_jobs[dep]["l_keys"] for dep in l_dependencies]
+
+        return [
+            dep
+            for dep, l_keys in zip(l_dependencies, ll_keys)
+            if nested_get(self.dic_tree, l_keys + ["status"]) not in ["finished", "failed"]
+        ]
+
+    def get_failed_dependency(self, job: str) -> list:
+        """
+        Gets the list of failed dependencies for a given job.
+
+        Args:
+            job (str): The name of the job.
+
+        Returns:
+            list: The list of failed dependencies.
+        """
+        # Ensure the dependency graph is built
+        if self.dependency_graph == {}:
+            self.build_full_dependency_graph()
+
+        # Get the list of dependencies
+        l_dependencies = self.dependency_graph[job]
+
+        # Get the corresponding list of l_keys
+        ll_keys = [self.dic_all_jobs[dep]["l_keys"] for dep in l_dependencies]
+
+        return [
+            dep
+            for dep, l_keys in zip(l_dependencies, ll_keys)
+            if nested_get(self.dic_tree, l_keys + ["status"]) == "failed"
+        ]
