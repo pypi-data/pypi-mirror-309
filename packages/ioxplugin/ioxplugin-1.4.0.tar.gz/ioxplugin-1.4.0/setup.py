818c251558ff54783331e16cb204a420562deb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:37 +0000

    Add package ioxplugin/1.4.0/ioxplugin-1.4.0.tar.gz

---
+++ packages/ioxplugin/ioxplugin-1.4.0.tar.gz/ioxplugin-1.4.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ioxplugin',
+    version='1.4.0',
+    packages=find_packages(),
+    description='IoX Plugin Helper Package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Michel Kohanim',
+    author_email='support@universal-devices.com',
+    url='https://github.com/universaldevices/ioxplugin.git',
+    install_requires=[
+        'udi_interface>=3.0.57',  # Specify version ranges if needed
+        'astor'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
