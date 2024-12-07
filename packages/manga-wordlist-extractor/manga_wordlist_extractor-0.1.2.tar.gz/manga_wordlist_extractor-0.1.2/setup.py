dae2ee2f741f11b7cf10ecff53ad8417f26d33fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:55 +0000

    Add package manga-wordlist-extractor/0.1.2/manga_wordlist_extractor-0.1.2.tar.gz

---
+++ packages/manga-wordlist-extractor/manga_wordlist_extractor-0.1.2.tar.gz/manga_wordlist_extractor-0.1.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='manga-wordlist-extractor',
+    version='0.1.2',
+    description='A utility to extract vocabulary lists from manga.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Fluttrr',
+    author_email='ySZ39@proton.me',
+    url='https://github.com/Fluttrr/manga-wordlist-extractor',
+    license='GLPv3',
+    packages=find_packages(where='src/main'),
+    package_dir={'': 'src/main'},
+    install_requires=[
+        'nagisa',
+        'regex',
+        'tqdm',
+        'mokuro'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'manga-wordlist-extractor=main:main',
+        ],
+    },
+)
