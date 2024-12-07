f215c0efefc98a5b4fae8c6e0150625e6d94eb90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:12 +0000

    Add package py-own-synonyms-pkg/0.1.4/py-own-synonyms-pkg-0.1.4.tar.gz

---
+++ packages/py-own-synonyms-pkg/py-own-synonyms-pkg-0.1.4.tar.gz/py-own-synonyms-pkg-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-own-synonyms-pkg',  # Replace with your desired package name
+    version='0.1.4',  # Version of your package
+    packages=find_packages(),
+    include_package_data=True,  # Ensure the JSON file is included
+    install_requires=[],  # List dependencies here if needed
+    long_description=open('README.md').read(),  # Optional, if you have a README
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',  # Adjust for your minimum Python version
+)
