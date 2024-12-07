15273344ab4ab83412eb367e8bb29da291dc7f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:01 +0000

    Add package gitverbose/0.2/gitverbose-0.2.tar.gz

---
+++ packages/gitverbose/gitverbose-0.2.tar.gz/gitverbose-0.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from setuptools import setup
+
+setup (
+    name="gitverbose",
+    description="Easy clone github repo, Internet files.",
+    long_description="Make your life easier while cloning github repo.",
+    version="0.2",
+    author="caique9014",
+    author_email="caiqueonz777@proton.me",
+    packages=['gv'],
+    entry_points={
+        'console_scripts': ['gv=gv.entry:gv_entry_point']
+    },
+    keywords=[
+        'cli',
+        'python',
+        'github',
+        'git',
+        'pypi',
+        'fun',
+        'image',
+        'hash',
+        'table',
+        'typer',
+        '3.9',
+        'pip'
+    ],
+    install_requires=[
+        "requests",
+        "argparse",
+        "git-clone"
+    ]
+)
