c360be70691218bb40425ad077e2b3ede5dbdc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:04 +0000

    Add package mypy-boto3-builder/8.3.1/mypy_boto3_builder-8.3.1.tar.gz

---
+++ packages/mypy-boto3-builder/mypy_boto3_builder-8.3.1.tar.gz/mypy_boto3_builder-8.3.1/mypy_boto3_builder/constants.py
@@ -0,0 +1,103 @@
+"""
+Constants and paths.
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from pathlib import Path
+from typing import Final
+
+# Random region to initialize services
+DUMMY_REGION: Final = "us-west-2"
+
+# Root path for the project
+ROOT_PATH: Final = Path(__file__).parent
+
+# Jinja2 templates for boto3-stubs
+TEMPLATES_PATH: Final = ROOT_PATH / "templates"
+
+# Max line length for formatting
+LINE_LENGTH: Final = 100
+
+# Max line length for formatting docstrings
+DOCSTRING_LINE_LENGTH: Final = 80
+
+# Max docstring length extracted from method
+DOCSTRING_MAX_LENGTH: Final = 300
+
+# mypy-boto3-builder GitHub link
+BUILDER_REPO_URL: Final = "https://github.com/youtype/mypy_boto3_builder"
+
+# Main logger name
+LOGGER_NAME: Final = "mypy_boto3_builder"
+
+# builder CLI entrypoint name
+PROG_NAME: Final = "mypy_boto3_builder"
+
+# builder package name
+PACKAGE_NAME: Final = "mypy-boto3-builder"
+
+# universal mask for all resources
+ALL: Final = "*"
+DELETE: Final = "_delete"
+ATTRIBUTES: Final = "_attributes"
+SERVICE_RESOURCE: Final = "ServiceResource"
+CLIENT: Final = "Client"
+
+# python versions supported by output stubs
+SUPPORTED_PY_VERSIONS: Final = {
+    (3, 8),
+    (3, 9),
+    (3, 10),
+    (3, 11),
+    (3, 12),
+    (3, 13),
+    (3, 14),
+}
+
+# default timeout for HTTP requests
+REQUEST_TIMEOUT: Final = 120
+
+
+class TemplatePath:
+    """
+    Template paths.
+    """
+
+    master: Final = TEMPLATES_PATH / "master"
+
+    boto3_stubs: Final = TEMPLATES_PATH / "boto3-stubs"
+    boto3_stubs_docs: Final = TEMPLATES_PATH / "boto3-stubs-docs"
+    boto3_stubs_service: Final = TEMPLATES_PATH / "boto3-stubs-service"
+    boto3_stubs_service_docs: Final = TEMPLATES_PATH / "boto3-stubs-service-docs"
+    boto3_stubs_full: Final = TEMPLATES_PATH / "boto3-stubs-full"
+
+    types_aioboto3: Final = TEMPLATES_PATH / "types-aioboto3"
+    types_aioboto3_docs: Final = TEMPLATES_PATH / "types-aioboto3-docs"
+    types_aioboto3_service_docs: Final = TEMPLATES_PATH / "types-aioboto3-service-docs"
+
+    types_aiobotocore: Final = TEMPLATES_PATH / "types-aiobotocore"
+    types_aiobotocore_docs: Final = TEMPLATES_PATH / "types-aiobotocore-docs"
+    types_aiobotocore_service: Final = TEMPLATES_PATH / "types-aiobotocore-service"
+    types_aiobotocore_service_docs: Final = TEMPLATES_PATH / "types-aiobotocore-service-docs"
+    types_aiobotocore_full: Final = TEMPLATES_PATH / "types-aiobotocore-full"
+
+
+class StaticStubsPath:
+    """
+    Static *.pyi files path.
+    """
+
+    boto3_stubs: Final = ROOT_PATH / "stubs_static" / "boto3-stubs"
+    types_aiobotocore: Final = ROOT_PATH / "stubs_static" / "types-aiobotocore"
+    types_aioboto3: Final = ROOT_PATH / "stubs_static" / "types-aioboto3"
+
+
+class StaticStubsPullURL:
+    """
+    Static *.pyi files zip download URL.
+    """
+
+    boto3_stubs: Final = "https://api.github.com/repos/youtype/boto3-stubs/zipball/main"
+    types_aiobotocore: Final = "https://api.github.com/repos/youtype/types-aiobotocore/zipball/main"
+    types_aioboto3: Final = "https://api.github.com/repos/youtype/types-aioboto3/zipball/main"
---
+++ packages/mypy-boto3-builder/mypy_boto3_builder-8.3.1.tar.gz/mypy_boto3_builder-8.3.1/mypy_boto3_builder/parsers/fake_service_package.py
@@ -0,0 +1,87 @@
+"""
+Fake parser that produces `structures.ServiceModule` for master module and stubs.
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from boto3.session import Session
+
+from mypy_boto3_builder.package_data import BasePackageData
+from mypy_boto3_builder.parsers.shape_parser import ShapeParser
+from mypy_boto3_builder.service_name import ServiceName
+from mypy_boto3_builder.structures.client import Client
+from mypy_boto3_builder.structures.paginator import Paginator
+from mypy_boto3_builder.structures.service_package import ServicePackage
+from mypy_boto3_builder.structures.service_resource import ServiceResource
+from mypy_boto3_builder.structures.waiter import Waiter
+from mypy_boto3_builder.utils.boto3_utils import get_boto3_client, get_boto3_resource
+from mypy_boto3_builder.utils.strings import get_class_prefix, xform_name
+
+
+def parse_fake_service_package(
+    session: Session,
+    service_name: ServiceName,
+    package_data: type[BasePackageData],
+    version: str,
+) -> ServicePackage:
+    """
+    Create fake boto3 service module structure.
+
+    Used by stubs and master package.
+
+    Arguments:
+        session -- boto3 session.
+        service_name -- Target service name.
+        package_data -- Package data.
+        version -- Package version.
+
+    Returns:
+        ServiceModule structure.
+    """
+    shape_parser = ShapeParser(session, service_name)
+    boto3_client = get_boto3_client(session, service_name)
+    boto3_resource = get_boto3_resource(session, service_name)
+
+    result = ServicePackage(
+        data=package_data,
+        service_name=service_name,
+        client=Client(
+            name=Client.get_class_name(service_name),
+            service_name=service_name,
+            boto3_client=boto3_client,
+        ),
+        version=version,
+    )
+
+    if boto3_resource is not None:
+        result.service_resource = ServiceResource(
+            name=ServiceResource.get_class_name(service_name),
+            service_name=service_name,
+            boto3_service_resource=boto3_resource,
+        )
+
+    waiter_attribute_names: list[str] = boto3_client.waiter_names
+    for waiter_attribute_name in waiter_attribute_names:
+        real_class_name = get_class_prefix(waiter_attribute_name)
+        waiter_class_name = f"{real_class_name}Waiter"
+        result.waiters.append(
+            Waiter(
+                name=waiter_class_name,
+                waiter_name=real_class_name,
+                attribute_name=waiter_attribute_name,
+                service_name=service_name,
+            ),
+        )
+
+    for paginator_name in shape_parser.get_paginator_names():
+        operation_name = xform_name(paginator_name)
+        result.paginators.append(
+            Paginator(
+                f"{paginator_name}Paginator",
+                operation_name=operation_name,
+                service_name=service_name,
+                paginator_name=paginator_name,
+            ),
+        )
+
+    return result
