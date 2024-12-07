d3c18b6d479f17fa1d329a3c6a3e40571440ec10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:06 +0000

    Add package multiscale_spatial_image/2.0.2/multiscale_spatial_image-2.0.2.tar.gz

---
+++ packages/multiscale_spatial_image/multiscale_spatial_image-2.0.2.tar.gz/multiscale_spatial_image-2.0.2/test/_data.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+import pytest
+import pooch
+from zarr.storage import DirectoryStore
+import xarray as xr
+
+test_data_ipfs_cid = "bafybeiaskr5fxg6rbcwlxl6ibzqhubdleacenrpbnymc6oblwoi7ceqzta"
+test_data_sha256 = "507dd779cba007c46ea68a5fe8865cabd5d8a7e00816470faae9195d1f1c3cd1"
+
+
+test_dir = Path(__file__).resolve().parent
+extract_dir = "data"
+test_data_dir = test_dir / extract_dir
+test_data = pooch.create(
+    path=test_dir,
+    # base_url=f"https://{test_data_ipfs_cid}.ipfs.w3s.link/ipfs/{test_data_ipfs_cid}/",
+    base_url="https://github.com/spatial-image/multiscale-spatial-image/releases/download/v2.0.0/",
+    registry={
+        "data.tar.gz": f"sha256:{test_data_sha256}",
+    },
+    retry_if_failed=5,
+)
+
+
+@pytest.fixture
+def input_images():
+    untar = pooch.Untar(extract_dir=extract_dir)
+    test_data.fetch("data.tar.gz", processor=untar)
+    result = {}
+
+    store = DirectoryStore(
+        test_data_dir / "input" / "cthead1.zarr", dimension_separator="/"
+    )
+    image_ds = xr.open_zarr(store)
+    image_da = image_ds.cthead1
+    result["cthead1"] = image_da
+
+    store = DirectoryStore(
+        test_data_dir / "input" / "small_head.zarr", dimension_separator="/"
+    )
+    image_ds = xr.open_zarr(store)
+    image_da = image_ds.small_head
+    result["small_head"] = image_da
+
+    store = DirectoryStore(
+        test_data_dir / "input" / "2th_cthead1.zarr",
+    )
+    image_ds = xr.open_zarr(store)
+    image_da = image_ds["2th_cthead1"]
+    result["2th_cthead1"] = image_da
+
+    return result
+
+
+def verify_against_baseline(dataset_name, baseline_name, multiscale):
+    store = DirectoryStore(
+        test_data_dir / f"baseline/{dataset_name}/{baseline_name}",
+        dimension_separator="/",
+    )
+    dt = xr.open_datatree(store, engine="zarr", mode="r")
+    xr.testing.assert_equal(dt.ds, multiscale.ds)
+    for scale in multiscale.children:
+        xr.testing.assert_equal(dt[scale].ds, multiscale[scale].ds)
+
+
+def store_new_image(dataset_name, baseline_name, multiscale_image):
+    """Helper method for writing output results to disk
+    for later upload as test baseline"""
+    path = test_data_dir / f"baseline/{dataset_name}/{baseline_name}"
+    store = DirectoryStore(
+        str(path),
+        dimension_separator="/",
+    )
+    multiscale_image.to_zarr(store, mode="w")
---
+++ packages/multiscale_spatial_image/multiscale_spatial_image-2.0.2.tar.gz/multiscale_spatial_image-2.0.2/test/test_ngff_validation.py
@@ -0,0 +1,82 @@
+import json
+from typing import Dict
+import urllib3
+
+from referencing import Registry, Resource
+from jsonschema import Draft202012Validator
+
+from xarray import DataTree
+
+from multiscale_spatial_image import to_multiscale, MultiscaleSpatialImage
+from spatial_image import to_spatial_image
+import numpy as np
+import zarr
+
+http = urllib3.PoolManager()
+
+ngff_uri = "https://ngff.openmicroscopy.org"
+
+
+def load_schema(version: str = "0.4", strict: bool = False) -> Dict:
+    strict_str = ""
+    if strict:
+        strict_str = "strict_"
+    response = http.request(
+        "GET", f"{ngff_uri}/{version}/schemas/{strict_str}image.schema"
+    )
+    schema = json.loads(response.data.decode())
+    return schema
+
+
+def check_valid_ngff(multiscale: DataTree):
+    store = zarr.storage.MemoryStore(dimension_separator="/")
+    assert isinstance(multiscale.msi, MultiscaleSpatialImage)
+    multiscale.msi.to_zarr(store, compute=True)
+    zarr.convenience.consolidate_metadata(store)
+    metadata = json.loads(store.get(".zmetadata"))["metadata"]
+    ngff = metadata[".zattrs"]
+
+    image_schema = load_schema(version="0.4", strict=False)
+    # strict_image_schema = load_schema(version="0.4", strict=True)
+    registry = Registry().with_resource(
+        ngff_uri, resource=Resource.from_contents(image_schema)
+    )
+    validator = Draft202012Validator(image_schema, registry=registry)
+    # registry_strict = Registry().with_resource(ngff_uri, resource=Resource.from_contents(strict_image_schema))
+    # strict_validator = Draft202012Validator(strict_schema, registry=registry_strict)
+
+    validator.validate(ngff)
+    # Need to add NGFF metadata property
+    # strict_validator.validate(ngff)
+
+
+def test_y_x_valid_ngff():
+    array = np.random.random((32, 16))
+    image = to_spatial_image(array)
+    multiscale = to_multiscale(image, [2, 4])
+
+    check_valid_ngff(multiscale)
+
+
+def test_z_y_x_valid_ngff():
+    array = np.random.random((32, 32, 16))
+    image = to_spatial_image(array)
+    multiscale = to_multiscale(image, [2, 4])
+
+    check_valid_ngff(multiscale)
+
+
+def test_z_y_x_c_valid_ngff():
+    array = np.random.random((32, 32, 16, 3))
+    image = to_spatial_image(array)
+    multiscale = to_multiscale(image, [2, 4])
+
+    check_valid_ngff(multiscale)
+
+
+def test_t_z_y_x_c_valid_ngff():
+    array = np.random.random((2, 32, 32, 16, 3))
+    image = to_spatial_image(array)
+    multiscale = to_multiscale(image, [2, 4])
+
+    check_valid_ngff(multiscale)
