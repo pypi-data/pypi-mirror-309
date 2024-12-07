24f961b9bd8446b6450a01430b9c501ba0129341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:09 +0000

    Add package poetry-demo-plugin/0.0.1/poetry_demo_plugin-0.0.1-py3-none-any.whl

---
+++ packages/poetry-demo-plugin/poetry_demo_plugin-0.0.1-py3-none-any.whl/poetry_demo_plugin/custom_command.py
@@ -0,0 +1,24 @@
+import logging
+
+from cleo.commands.command import Command
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+logger = logging.getLogger(__name__)
+
+class CustomCommand(Command):
+
+    name = "my-command"
+
+    def handle(self) -> int:
+        self.line("My command")
+
+        return 0
+
+
+def factory():
+    return CustomCommand()
+
+
+class MyApplicationPlugin(ApplicationPlugin):
+    def activate(self, application):
+        application.command_loader.register_factory("my-command", factory)
