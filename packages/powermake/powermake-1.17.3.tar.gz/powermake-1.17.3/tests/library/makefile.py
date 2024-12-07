e3eca5912bbcbf07ceff246d2cd7c4d1d7005b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:01 +0000

    Add package powermake/1.17.3/powermake-1.17.3.tar.gz

---
+++ packages/powermake/powermake-1.17.3.tar.gz/powermake-1.17.3/tests/library/makefile.py
@@ -0,0 +1,24 @@
+import powermake
+
+def on_build(config: powermake.Config):
+    config.add_c_cpp_as_asm_flags("-Wsecurity")
+
+    if config.target_is_windows():
+        if config.target_is_mingw():
+            files = {"my_lib.c", "subtract_windows.asm", "multiply_windows.s"}
+        else:
+            files = {"my_lib.c", "subtract_windows.asm"}
+            config.add_defines("DISABLE_GNU_AS")
+    elif config.target_is_macos():
+        if config.target_simplified_architecture == "arm64":
+            files = {"my_lib.c", "multiply_macos_arm.s", "subtract_macos_arm.s"}
+        else:
+            files = {"my_lib.c", "multiply_macos.s", "subtract_macos.asm"}
+    else:
+        files = {"my_lib.c", "multiply_linux.s", "subtract_linux.asm"}
+
+    objects = powermake.compile_files(config, files)
+
+    powermake.archive_files(config, objects)
+
+powermake.run("my_lib", build_callback=on_build)
