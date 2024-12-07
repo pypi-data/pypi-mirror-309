0cdb261fd502066e4f414bf78003b1967d0aab68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:05 +0000

    Add package aind-behavior-services/0.8.9/aind_behavior_services-0.8.9-py3-none-any.whl

---
+++ packages/aind-behavior-services/aind_behavior_services-0.8.9-py3-none-any.whl/aind_behavior_services/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.8.9"
+
+from .rig import AindBehaviorRigModel  # noqa: F401
+from .session import AindBehaviorSessionModel  # noqa: F401
+from .task_logic import AindBehaviorTaskLogicModel  # noqa: F401
