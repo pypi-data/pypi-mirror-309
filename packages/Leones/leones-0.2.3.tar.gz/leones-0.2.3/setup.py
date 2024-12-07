b4f1018187362e2fcfeda82fdb83987723ad202e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:36 +0000

    Add package Leones/0.2.3/leones-0.2.3.tar.gz

---
+++ packages/Leones/leones-0.2.3.tar.gz/leones-0.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Leones",
+    version="0.2.3",
+    author="Grupo F",
+    author_email="aitor.fernandezderet@alumni.mondragon.edu",
+    description="This library consists of a casino simulator, in which you can play various casino games such as black jack.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/unaidemiguel/Casino-Montepinar.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"],
+    python_requires='>=3.6',
+    install_requires=[
+        "random",
+        "time"
+        ]
+)
