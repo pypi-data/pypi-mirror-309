afc666ff533380a9a013bb3f10f448de2743692c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:51 +0000

    Add package pyflutterflow/0.1.12/pyflutterflow-0.1.12.tar.gz

---
+++ packages/pyflutterflow/pyflutterflow-0.1.12.tar.gz/pyflutterflow-0.1.12/pyflutterflow/dashboard/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Button from 'primevue/button';
+import { computed } from 'vue';
+import { useAuthStore } from '@/stores/auth.store';
+// import { useContentStore } from "@/stores/content.store";
+
+// const contentStore = useContentStore();
+// const contentData = computed(() => contentStore.content)
+// contentStore.getContent()
+const authStore = useAuthStore();
+
+const username = computed(() => authStore.displayName || authStore.email);
+</script>
+
+<template>
+   <h1 class="text-3xl">Hi {{ username }}</h1>
+   <span class="text-surface-400">Welcome Back</span>
+</template>
