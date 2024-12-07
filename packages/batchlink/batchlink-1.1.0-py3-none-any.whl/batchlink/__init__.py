9b9354c29540e39d3deef4468ea1e2dc54b5c89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:34 +0000

    Add package batchlink/1.1.0/batchlink-1.1.0-py3-none-any.whl

---
+++ packages/batchlink/batchlink-1.1.0-py3-none-any.whl/batchlink/__init__.py
@@ -0,0 +1,14 @@
+"""Module providing batchlink main APIs."""
+
+from .core import add_underline, batch_link, formatted_paths, pretty_print, remove
+
+__version__ = "1.1.0"
+
+__all__ = [
+    "__version__",
+    "add_underline",
+    "batch_link",
+    "formatted_paths",
+    "pretty_print",
+    "remove",
+]
