5db6dbc2611b9d3b12e612ac1c19e246ffe7b3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:22 +0000

    Add package gencove/2.24.2/gencove-2.24.2.tar.gz

---
+++ packages/gencove/gencove-2.24.2.tar.gz/gencove-2.24.2/gencove/command/explorer/data/presign/main.py
@@ -0,0 +1,73 @@
+"""Configure explorer data presign subcommand."""
+import sys
+
+import click
+
+from ..common import (
+    GencoveExplorerManager,
+    request_is_from_explorer,
+    validate_explorer_user_data,
+)
+from ....base import Command
+from ....utils import user_has_aws_in_path
+
+
+class Presign(Command):
+    """Presign Explorer object command executor."""
+
+    def __init__(self, ctx, path, credentials, options):
+        super().__init__(credentials, options)
+        self.ctx = ctx
+        self.path = path
+
+        # Populated after self.login() is called
+        self.user = None
+        self.organization = None
+        self.aws_session_credentials = None
+
+        # populated after self.execute() is called
+        self.organization_users = None
+
+    def validate(self):
+        """Validate presign"""
+        validate_explorer_user_data(self.user, self.organization)
+        user_has_aws_in_path(raise_exception=True)
+
+    def initialize(self):
+        """Initialize presign subcommand."""
+        self.login()
+        self.user = self.api_client.get_user_details()
+        self.organization = self.api_client.get_organization_details()
+
+        if not request_is_from_explorer():
+            self.aws_session_credentials = (
+                self.api_client.get_explorer_data_credentials()
+            )
+
+    def execute(self):
+        """Make a request to presign Explorer object."""
+        self.echo_debug("Presign Explorer object.")
+
+        explorer_manager = GencoveExplorerManager(
+            aws_session_credentials=self.aws_session_credentials,
+            user_id=str(self.user.id),
+            organization_id=str(self.organization.id),
+            organization_users=self.api_client.get_organization_users(),
+        )
+
+        if self.path == explorer_manager.EXPLORER_SCHEME:
+            click.echo(
+                "Please provide a valid path to an Explorer object",
+                err=True,
+            )
+            sys.exit(1)
+        if (
+            self.path.rstrip("/")
+            == f"{explorer_manager.EXPLORER_SCHEME}{explorer_manager.USERS}"
+        ):
+            click.echo(
+                "Please provide a valid path to an Explorer object",
+                err=True,
+            )
+            sys.exit(1)
+        explorer_manager.execute_aws_s3_path("presign", self.path, self.ctx.args)
