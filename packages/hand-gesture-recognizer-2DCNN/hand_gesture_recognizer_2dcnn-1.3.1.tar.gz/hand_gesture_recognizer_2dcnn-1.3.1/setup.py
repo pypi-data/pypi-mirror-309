7c1e95dede8d22db404fafd23ba60527a3d829af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:08:31 +0000

    Add package hand-gesture-recognizer-2DCNN/1.3.1/hand_gesture_recognizer_2dcnn-1.3.1.tar.gz

---
+++ packages/hand-gesture-recognizer-2DCNN/hand_gesture_recognizer_2dcnn-1.3.1.tar.gz/hand_gesture_recognizer_2dcnn-1.3.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+with open("README.md", encoding="utf-8") as f:
+    description = f.read()
+
+setup(
+    name="hand_gesture_recognizer_2DCNN",
+    version="1.3.1",
+    description="A library for recognizing hand gestures using 2D CNN",
+    author="Umesh Singh Verma",
+    packages=find_packages(),
+    install_requires=["opencv-python", "mediapipe", "numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
