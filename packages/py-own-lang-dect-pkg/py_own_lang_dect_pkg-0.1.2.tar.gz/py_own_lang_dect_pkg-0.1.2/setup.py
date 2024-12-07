259c359e129c327d6f8ad00c62cd666af161e303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:38 +0000

    Add package py-own-lang-dect-pkg/0.1.2/py_own_lang_dect_pkg-0.1.2.tar.gz

---
+++ packages/py-own-lang-dect-pkg/py_own_lang_dect_pkg-0.1.2.tar.gz/py_own_lang_dect_pkg-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py_own_lang_dect_pkg",  # Replace with your package name
+    version="0.1.2",
+    description="Fine-tuned BERT model for language detection.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",  # Specify the format of README
+    author="aurionpro",
+    license="MIT",  # Replace with your license
+    packages=find_packages(),  # Automatically find all Python packages
+    include_package_data=True,  # Include non-Python files if specified in MANIFEST.in
+    package_data={
+        "py_own_lang_dect_pkg": ["model/*"],  # Include your model files
+    },
+    install_requires=[
+        "transformers>=4.0.0",  # Add necessary dependencies
+        "python-dotenv==1.0.1",
+        "torch==2.2.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",  # Specify compatible Python versions
+)
