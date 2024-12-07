28d7a579bed7ff4ded8738ed8e466825fe49e9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:23 +0000

    Add package mbdb-parsing/0.2.0/mbdb_parsing-0.2.0.tar.gz

---
+++ packages/mbdb-parsing/mbdb_parsing-0.2.0.tar.gz/mbdb_parsing-0.2.0/tests/test_base.py
@@ -0,0 +1,15 @@
+import pytest
+
+from mbdb_parsing.base import (
+    MethodFileProcessor,
+    log_exception,
+    commit_to_record,
+    has_correct_signature,
+)
+
+def test():
+    assert has_correct_signature
+
+
+
+
