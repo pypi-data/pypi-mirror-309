ad907f15c606bc3771a4bebc4197a82d32495f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:43 +0000

    Add package shoydokova/0.7.2/shoydokova-0.7.2.tar.gz

---
+++ packages/shoydokova/shoydokova-0.7.2.tar.gz/shoydokova-0.7.2/shoydokova/__init__.py
@@ -0,0 +1,4 @@
+from .core import generate_joke, generate_funny_fact
+from .periodic import start_joke_stream
+
+__all__ = ["generate_joke", "start_joke_stream"]
---
+++ packages/shoydokova/shoydokova-0.7.2.tar.gz/shoydokova-0.7.2/shoydokova/core.py
@@ -0,0 +1,14 @@
+import random
+
+jokes = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+]
+
+def generate_joke():
+    """Возвращает случайное число."""
+    return random.choice(jokes)
+
