dcf993d813a6b511a1d16f6d021ac263b03924c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:54 +0000

    Add package himpunan-team2/0.1.0/himpunan_team2-0.1.0.tar.gz

---
+++ packages/himpunan-team2/himpunan_team2-0.1.0.tar.gz/himpunan_team2-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="himpunan-team2",  # Nama package
+    version="0.1.0",  # Versi awal
+    packages=find_packages(),  # Menemukan semua package di direktori
+    description="Package Python untuk operasi himpunan dengan fitur tambahan.",
+    author="Alvin Yuga Pramana, Jason Bintang Setiawan, Javin Erasmus Clementino",
+    install_requires=[],  # Daftar dependensi package (jika ada)
+)
