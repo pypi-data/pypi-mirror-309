8798e036e40f86061475346f412c5bb967375817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:31:53 +0000

    Add package database-setup/0.0.5/database_setup-0.0.5.tar.gz

---
+++ packages/database-setup/database_setup-0.0.5.tar.gz/database_setup-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='database_setup',
+    version='0.0.5',
+    description='Database setup utilities with configuration handling',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    author='Komal Swami',
+    author_email='komal@neudeep.in',
+    packages=find_packages(),
+    install_requires=[
+        'pymysql',
+        'dbutils',
+        'PyYAML',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
