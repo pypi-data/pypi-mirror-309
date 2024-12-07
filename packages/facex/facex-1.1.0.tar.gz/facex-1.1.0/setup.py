cf8f5866163fea4e65502a3bf644039c688354fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:48 +0000

    Add package facex/1.1.0/facex-1.1.0.tar.gz

---
+++ packages/facex/facex-1.1.0.tar.gz/facex-1.1.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="facex", # Replace with your own username
+    version="1.1.0",
+    author="Hasby Fahrudin",
+    author_email="fahrudinhasby12@gmail.com",
+    description="Lightweight, High-Performance Facial Expression Classifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hfahrudin/facex",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,  # Ensure this is True
+    package_data={
+        "facex": ["assets/*.tflite", "assets/*.xml"],  # Include specific files
+    },
+    install_requires=[
+        'scikit-image>=0.16.2',
+        'tensorflow>=2.0.0',
+        'opencv-contrib-python>=4.10.0.84',
+    ],
+    python_requires='>=3.6',
+)
