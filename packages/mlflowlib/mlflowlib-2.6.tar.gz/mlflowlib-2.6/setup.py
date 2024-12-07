b9bff016a1b98b283d5b5e2a8f036e02d5f2406d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:39 +0000

    Add package mlflowlib/2.6/mlflowlib-2.6.tar.gz

---
+++ packages/mlflowlib/mlflowlib-2.6.tar.gz/mlflowlib-2.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlflowlib',
+    packages=find_packages(include=['mlflowlib']),
+    version='2.6',
+    license='MIT',    
+    description='a general-purpose python package with MLflow integration',
+    author='irem ozdemir',
+    author_email = 'iremozdemirwww3@gmail.com', 
+    keywords='mlflow',
+    install_requires=[
+        'mlflow>=1.21.0', 
+        'tensorflow>=2.5.0',  
+    ],
+    url = 'https://github.com/iremozdemr/mlflowlib', 
+    download_url = 'https://github.com/iremozdemr/mlflowlib/archive/refs/tags/2.6.tar.gz',
+)
