afef131953376eb3246b8c02735df226dc8ecd6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:22 +0000

    Add package FREEZINGEYES-STT/1.0.2/freezingeyes_stt-1.0.2.tar.gz

---
+++ packages/FREEZINGEYES-STT/freezingeyes_stt-1.0.2.tar.gz/freezingeyes_stt-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='FREEZINGEYES_STT',
+    version='1.0.2',
+    author='FreezingEYES',
+    author_email='manthanrauthan1@gmail.com',
+    description= 'Contain only one command which is > listen() > It just listenss',
+)
+packages = find_packages(),
+install_requirements = [
+    'selenium',
+    'webdriver-manager',
+]
+
+
+
