140cc625cb21416b43840389d3d0f950885efc66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:38:28 +0000

    Add package muxkt/0.3.0/muxkt-0.3.0.tar.gz

---
+++ packages/muxkt/muxkt-0.3.0.tar.gz/muxkt-0.3.0/muxkt/selection.py
@@ -0,0 +1,35 @@
+from time import sleep
+
+from iterfzf import iterfzf
+
+
+def selection(iterable: list):
+    """
+    Takes in a list and allows the user to choose among the list using fzf
+
+    Args:
+        iterable (list): The list from which user should choose an option
+
+    Return:
+        Generator Function for iterfzf
+    """
+
+    for item in iterable:
+        yield item.strip()
+        sleep(0.01)
+
+
+def fzf(iterable: list, prompt: str, choose_multiple: bool = False):
+    """
+    Takes in a list and allows the user to choose among the list using fzf
+
+    Args:
+        iterable (list): The list from which user should choose an option
+        prompt (str): Prompt to show in the fzf interface
+        choose_multiple (bool): True if user can choose multiple options; False if user cannot
+
+    Return:
+        list | str: list of chosen items if 'choose_multiple' is true; string of chosen item otherwise
+    """
+
+    return iterfzf(selection(iterable), prompt=prompt, multi=choose_multiple)
