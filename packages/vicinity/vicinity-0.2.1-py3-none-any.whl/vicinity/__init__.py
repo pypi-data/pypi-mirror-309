afae887fe00fa39824bd500d5add34f79851c05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:21 +0000

    Add package vicinity/0.2.1/vicinity-0.2.1-py3-none-any.whl

---
+++ packages/vicinity/vicinity-0.2.1-py3-none-any.whl/vicinity/__init__.py
@@ -0,0 +1,7 @@
+"""Small vector store."""
+
+from vicinity.utils import normalize
+from vicinity.version import __version__
+from vicinity.vicinity import Vicinity
+
+__all__ = ["Vicinity", "normalize", "__version__"]
