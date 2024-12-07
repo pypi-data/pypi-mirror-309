a92d97813d6b0d63dae2f566354f24b2d04012af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:39 +0000

    Add package tibber-pulse-sml-debugger/1.0.0/tibber_pulse_sml_debugger-1.0.0-py3-none-any.whl

---
+++ packages/tibber-pulse-sml-debugger/tibber_pulse_sml_debugger-1.0.0-py3-none-any.whl/tibber_pulse_sml_debugger/exceptions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  Author: wh0ami
+# License: MIT License <https://opensource.org/license/MIT>
+# Project: https://codeberg.org/wh0ami/tibber-pulse-sml-debugger
+
+
+class AuthenticationError(Exception):
+    pass
