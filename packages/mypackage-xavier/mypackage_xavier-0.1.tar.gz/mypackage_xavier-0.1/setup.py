d30af1f68bb7222d873a1a3418bb3cbaa27f39ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:37 +0000

    Add package mypackage-xavier/0.1/mypackage_xavier-0.1.tar.gz

---
+++ packages/mypackage-xavier/mypackage_xavier-0.1.tar.gz/mypackage_xavier-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mypackage_xavier',
+version='0.1',
+description='Testing installation of Package',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['mypackage_xavier'],
+zip_safe=False)
